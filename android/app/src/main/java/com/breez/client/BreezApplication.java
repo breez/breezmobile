8152a433f747c969c9387fd4760c7e84830a8eda
Roei Erez <roeierez@gmail.com>
2018-10-24 09:30:33 +0300

    remove not necessary functionality from BreezApplication

--- android/app/src/main/java/com/breez/client/BreezApplication.java
+++ android/app/src/main/java/com/breez/client/BreezApplication.java
@@ -5,16 +5,6 @@ import io.flutter.app.FlutterApplication;
 
 public class BreezApplication extends FlutterApplication {
 
-    private static boolean s_appRunning;
-
     public static boolean isBackground = false;
     public static BreezShare breezShare;
-
-    public static synchronized boolean isAppRunning(){
-        return s_appRunning;
-    }
-
-    public static synchronized void setAppRunning(){
-        s_appRunning = true;
-    }
 }
