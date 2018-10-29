2f6d16d3f0568a16756ebf37c4564143df8c1162
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2018-10-30 00:15:09 +0300

    Copy hashcode and close details dialog

--- lib/widgets/payment_details_dialog.dart
+++ lib/widgets/payment_details_dialog.dart
@@ -18,56 +18,100 @@ class PaymentDetailsDialog extends StatelessWidget {
   }
 
   Widget showPaymentDetailsDialog() {
+    final snackBar = new SnackBar(
+      content: new Text(
+        'Hashcode is copied to your clipboard.',
+        style: theme.snackBarStyle,
+      ),
+      backgroundColor: theme.snackBarBackgroundColor,
+      duration: new Duration(seconds: 4),
+    );
     return new SimpleDialog(
-      contentPadding: EdgeInsets.fromLTRB(16.0,32.0,16.0,16.0),
+      contentPadding: EdgeInsets.fromLTRB(16.0, 32.0, 16.0, 16.0),
       children: <Widget>[
         //Text((paymentInfo.type == PaymentType.SENT || paymentInfo.type == PaymentType.WITHDRAWAL ? "Recipient: " : "Sender: "),style: theme.paymentRequestTitleStyle,textAlign: TextAlign.left,),
         Center(
           child: PaymentItemAvatar(paymentInfo, radius: 32.0),
         ),
-        Padding(padding: EdgeInsets.only(top: 8.0),),
+        Padding(
+          padding: EdgeInsets.only(top: 8.0),
+        ),
         Text(
           paymentInfo.title,
           style: theme.paymentRequestAmountStyle,
           textAlign: TextAlign.center,
         ),
-        Padding(padding: EdgeInsets.only(top: 8.0),),
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-          Column(crossAxisAlignment: CrossAxisAlignment.start,children: <Widget>[
-            Text(
-            "Amount:",
-            style: theme.paymentDetailsTitleStyle,
-            textAlign: TextAlign.left,
+        Container(
+          height: 36.0,
+          child: ListTile(
+            title: Text(
+              "Amount",
+              style: theme.paymentDetailsTitleStyle,
+              textAlign: TextAlign.left,
+            ),
+            trailing: Text(
+              (paymentInfo.type == PaymentType.SENT || paymentInfo.type == PaymentType.WITHDRAWAL ? "-" : "+") +
+                  paymentInfo.currency.format(paymentInfo.amount),
+              style: theme.paymentDetailsSubtitleStyle,
+              textAlign: TextAlign.left,
+            ),
           ),
-          Text(
-            "Date:",
-            style: theme.paymentDetailsTitleStyle,
-            textAlign: TextAlign.left,
+        ),
+        Container(
+          height: 36.0,
+          child: ListTile(
+            title: Text(
+              "Date",
+              style: theme.paymentDetailsTitleStyle,
+              textAlign: TextAlign.left,
+            ),
+            trailing: Text(
+              DateUtils.formatYearMonthDayHourMinuteSecond(
+                  DateTime.fromMillisecondsSinceEpoch(paymentInfo.creationTimestamp.toInt() * 1000)),
+              style: theme.paymentDetailsSubtitleStyle,
+              textAlign: TextAlign.left,
+            ),
           ),
-          Text(
-            "Hash:",
-            style: theme.paymentDetailsTitleStyle,
-            textAlign: TextAlign.left,
-          ),],),
-          Column(crossAxisAlignment: CrossAxisAlignment.end,children: <Widget>[
-          Text(
-            (paymentInfo.type == PaymentType.SENT || paymentInfo.type == PaymentType.WITHDRAWAL ? "-" : "+") +
-                paymentInfo.currency.format(paymentInfo.amount),
-            style: theme.paymentDetailsSubtitleStyle,
-            textAlign: TextAlign.left,
+        ),
+        Container(
+          height: 36.0,
+          child: ListTile(
+            leading: Text(
+              "Hash",
+              style: theme.paymentDetailsTitleStyle,
+              textAlign: TextAlign.left,
+            ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Text(
+                  paymentInfo.hashCode.toString(),
+                  style: theme.paymentDetailsSubtitleStyle,
+                  textAlign: TextAlign.right,
+                ),
+                IconButton(
+                  alignment: Alignment.centerRight,
+                  padding: EdgeInsets.fromLTRB(16.0,8.0,0.0,8.0),
+                  tooltip: "Copy hashcode",
+                  iconSize: 24.0,
+                  color: Colors.blue,
+                  icon: Icon(
+                    IconData(0xe90b, fontFamily: 'icomoon'),
+                  ),
+                  onPressed: () {
+                    Clipboard.setData(ClipboardData(text: paymentInfo.hashCode.toString()));
+                    Navigator.pop(context);
+                    Scaffold.of(context).showSnackBar(snackBar);
+                  },
+                ),
+              ],
+            ),
           ),
-          Text(
-            DateUtils.formatYearMonthDayHourMinuteSecond(DateTime.fromMillisecondsSinceEpoch(paymentInfo.creationTimestamp.toInt() * 1000)),
-            style: theme.paymentDetailsSubtitleStyle,
-            textAlign: TextAlign.left,
-          ),
-          Text(
-            paymentInfo.hashCode.toString(),
-            style: theme.paymentDetailsSubtitleStyle,
-            textAlign: TextAlign.left,
-          ),],)
-        ]),
-        Padding(padding: EdgeInsets.only(top: 16.0),),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 32.0),
+        ),
         Row(
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.center,
