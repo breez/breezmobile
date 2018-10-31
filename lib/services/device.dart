bb1c487b1effcd3d10591a8c1a1eeb27d20ded71
Igor Cota <igor@codexapertus.com>
2018-10-31 15:42:25 +0100

    Grab clipboard data on resume and check if it's a lightning invoice

--- lib/services/device.dart
+++ lib/services/device.dart
@@ -11,10 +11,20 @@ class Device {
   final StreamController _eventsController = new StreamController<NotificationType>.broadcast();
   Stream get eventStream => _eventsController.stream;
 
+  final StreamController _deviceClipboardController = new StreamController<String>.broadcast();
+  Stream get deviceClipboardStream => _deviceClipboardController.stream;
+
   Device(){
     _notificationsChannel.receiveBroadcastStream().listen((event){
       if (event == "resume") {
         _eventsController.sink.add(NotificationType.RESUME);
+
+        Clipboard.getData("text/plain").then((clipboardData) {
+          var text = clipboardData.text;
+          if (text.startsWith("lightning:")) {
+            _deviceClipboardController.add(text.substring(10));
+          }
+        });
       }
     });
   } 
