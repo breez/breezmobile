664941dd197cafcd050813fe8995452c65144c97
Roei Erez <roeierez@gmail.com>
2018-12-03 13:36:46 +0200

    prompt backup dialog only in case of error.

--- lib/widgets/backup_disabled_indicator.dart
+++ lib/widgets/backup_disabled_indicator.dart
@@ -1,3 +1,4 @@
+import 'package:breez/bloc/backup/backup_model.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -17,50 +18,62 @@ class BackupDisabledIndicator extends StatefulWidget {
 }
 
 class BackupDisabledIndicatorState extends State<BackupDisabledIndicator> {
-  StreamSubscription<bool> _promptEnableSubscription;
+  StreamSubscription<DateTime> _promptEnableSubscription;
+  StreamSubscription<BackupSettings> _settingsSubscription;
+  BackupSettings _currentSettings;
 
   @override
   void initState() {
-    _promptEnableSubscription =
-        Observable(widget._backupBloc.promptEnableStream)
-          .delay(Duration(seconds: 1))
-          .listen((prompt) {          
+    _settingsSubscription = widget._backupBloc.backupSettingsStream.listen((settings) => _currentSettings = settings);
+
+    _promptEnableSubscription = 
+      Observable(widget._backupBloc.lastBackupTimeStream)
+      .delay(Duration(seconds: 1))
+      .listen(
+        (lastBackup){}, 
+        onError: (err){
+          if(_currentSettings.promptOnError){
+
             Navigator.popUntil(context, (route) {
               return route.settings.name == "/home" || route.settings.name == "/";          
-            }); 
+            });
+
             showDialog(
                 context: context,
                 builder: (_) => new EnableBackupDialog(context, widget._backupBloc));
-          });
+            }
+        });
+
     super.initState();
   }
 
   @override
   void dispose() {
     _promptEnableSubscription.cancel();
+    _settingsSubscription.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return new StreamBuilder<bool>(
-        stream: widget._backupBloc.backupDisabledIndicatorStream,
-        builder: (context, snapshot) {
-          if (!snapshot.hasData || !snapshot.data) {
-            return Container();
-          } else {
-            return GestureDetector(
-                onTap: () {
-                  showDialog(
-                      context: context,
-                      builder: (_) =>
-                          new EnableBackupDialog(context, widget._backupBloc));
-                },
-                child: SvgPicture.asset(
-                  "src/icon/warning.svg",
-                  color: Color.fromRGBO(0, 120, 253, 1.0),
-                ));
-          }
-        });
+    return StreamBuilder<DateTime>(
+      stream: widget._backupBloc.lastBackupTimeStream,
+      builder: (context, snapshot) {
+        if (!snapshot.hasError) {
+          return Container();
+        } else {
+          return GestureDetector(
+              onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (_) =>
+                        new EnableBackupDialog(context, widget._backupBloc));
+              },
+              child: SvgPicture.asset(
+                "src/icon/warning.svg",
+                color: Color.fromRGBO(0, 120, 253, 1.0),
+              ));
+        }
+      });
   }
 }
