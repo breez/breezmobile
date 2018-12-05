7bbf3bd53451d3e71fd1e332e0439b49984f5038
Roei Erez <roeierez@gmail.com>
2018-12-05 17:13:09 +0200

    add transparent route to general widgets

---
+++ lib/widgets/transparent_page_route.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class TransparentPageRoute<T> extends ModalRoute<T> {  
+
+  final Widget Function(BuildContext context) builder;
+
+  TransparentPageRoute( this.builder);
+
+  @override
+  bool get opaque => false;
+
+  @override  
+  Color get barrierColor => null;
+
+  @override
+  bool get barrierDismissible => false;
+
+  @override  
+  String get barrierLabel => null;
+
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
+    return builder(context);
+  }
+
+  @override  
+  bool get maintainState => false;
+
+  @override  
+  Duration get transitionDuration => Duration(seconds: 0);
+}
