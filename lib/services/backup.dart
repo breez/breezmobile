243bf8c79dbd641d9a0936ac96c0c0f1ff3c96b4
Roei Erez <roeierez@gmail.com>
2018-12-10 15:47:58 +0200

    implement API for changes in backup service

--- lib/services/backup.dart
+++ lib/services/backup.dart
@@ -1,9 +1,12 @@
+import 'dart:collection';
+
 import 'package:flutter/services.dart';
 import 'dart:async';
 
 class BackupService {
   static const _methodChannel = const MethodChannel("com.breez.client/backup");
   static const String _signInFaileCode = "SIGN_IN_FAILED";
+  static const _backupChangesChannel = const EventChannel('com.breez.client/breez_backup_changes_stream');
 
   Future<dynamic> backup(List<String> paths, String nodeId, {bool silent = false}) {
     return _methodChannel.invokeMethod("backup", {"paths": paths, "nodeId": nodeId, "silent": silent});
@@ -19,6 +22,10 @@ class BackupService {
 
   Future<dynamic> signOut(){
      return _invokeMethod("signOut");
+  }
+
+  Stream<dynamic> getBackupChangesStream(String nodeID, String deviceID){    
+    return _backupChangesChannel.receiveBroadcastStream(HashMap.from({"nodeID": nodeID, "deviceID": deviceID}));
   }
 
   Future<dynamic> _invokeMethod(String method, [dynamic arguments]) {
