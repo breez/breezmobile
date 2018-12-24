c5074a0e83a6d065797bddb74d8dbde3b9a36091
Roei Erez <roeierez@gmail.com>
2018-12-24 19:41:49 +0200

    implement  some usefull getters on the PaymentInfo model

--- lib/bloc/account/account_model.dart
+++ lib/bloc/account/account_model.dart
@@ -140,15 +140,22 @@ class PaymentInfo {
   String get destination => _paymentResponse.destination;
   String get redeemTxID => _paymentResponse.redeemTxID;
   String get paymentHash => _paymentResponse.paymentHash;
-  bool get isUnknown {
-    String remoteName = (type == PaymentType.SENT ? _paymentResponse.invoiceMemo?.payeeName : _paymentResponse.invoiceMemo?.payerName);
-    return remoteName == null || remoteName.isEmpty;
+
+  bool get containsPaymentInfo {
+    String remoteName = (type == PaymentType.SENT ? _paymentResponse.invoiceMemo?.payeeName : _paymentResponse.invoiceMemo?.payerName);  
+    String decription = _paymentResponse.invoiceMemo?.description;  
+    return remoteName?.isNotEmpty  == true|| decription.isNotEmpty == true;     
   }
+
+  bool get isTransferRequest => _paymentResponse?.invoiceMemo?.transferRequest == true;
+
   String get description => type == PaymentType.DEPOSIT || type == PaymentType.WITHDRAWAL ? "Bitcoin Transfer" : _paymentResponse.invoiceMemo?.description;
+  
   String get imageURL {    
     String url = (type == PaymentType.SENT ? _paymentResponse.invoiceMemo?.payeeImageURL : _paymentResponse.invoiceMemo?.payerImageURL);
     return (url == null || url.isEmpty) ? null : url;      
   }
+  
   String get title {
     if (type == PaymentType.DEPOSIT || type == PaymentType.WITHDRAWAL){
       return "Bitcoin Transfer";
@@ -159,9 +166,11 @@ class PaymentInfo {
     }
     return (result == null || result.isEmpty) ? "Unknown" : result;
   }
+  
   Currency get currency => _currency;
 
   PaymentInfo(this._paymentResponse, this._currency);
+  
   PaymentInfo copyWith(Currency currency) {
     return PaymentInfo(this._paymentResponse, currency);
   }
@@ -214,74 +223,4 @@ class BroadcastRefundResponseModel {
   final String txID;
 
   BroadcastRefundResponseModel(this.request, this.txID);
-}
-
-class MockPaymentInfo implements PaymentInfo {
-  static bool isMockData = false;
-  MockPaymentInfo(this.amount, this.fee, this.hash, this.type, this.creationTimestamp, this.mockTitle, this.imageURL, [this.invoiceNote]);
-  static final miliPerDay = 1000 * 3600 * 24;
-  static List<PaymentInfo> createMockData() {
-    int now = DateTime.now().millisecondsSinceEpoch;
-    return new List<PaymentInfo>.unmodifiable([
-      new MockPaymentInfo(Int64(75300), Int64(9950), "123", PaymentType.DEPOSIT, Int64( ((now - miliPerDay * 7)  / 1000).ceil()), "Bitcoin Transfer", null,"Thanks for using our services"),
-      new MockPaymentInfo(Int64(34500), Int64(9950), "123", PaymentType.SENT, Int64( ((now - miliPerDay * 7)  / 1000).ceil()), "Hotel payment", "https://api.adorable.io/avatars/100/hotel@gmail.com.png","Hope you enjoyed your visit! Please visit our lorem ipsum. Feel free to contact use for further lorem ipsum."),
-      new MockPaymentInfo(Int64(452300), Int64(9950), "123", PaymentType.SENT, Int64( ((now - miliPerDay * 6)  / 1000).ceil()), "Kindergarden Payment", "https://api.adorable.io/avatars/100/kindergarden@gmail.com.png"),
-      new MockPaymentInfo(Int64(4400), Int64(9950), "123", PaymentType.RECEIVED, Int64( ((now - miliPerDay * 4)  / 1000).ceil()), "Order Refund", "https://api.adorable.io/avatars/100/order@gmail.com.png"),
-      new MockPaymentInfo(Int64(1000000), Int64(9950), "123", PaymentType.RECEIVED, Int64( ((now - miliPerDay * 3) / 1000).ceil()), "Trip Refund", "https://api.adorable.io/avatars/100/myfriendr@gmail.com.png"),
-      new MockPaymentInfo(Int64(11200), Int64(9950), "123", PaymentType.WITHDRAWAL, Int64( ((now - miliPerDay *2) / 1000).ceil()), "Electricity Bill", "https://api.adorable.io/avatars/100/electricity@gmail.com.png"),
-      new MockPaymentInfo(Int64(2000000), Int64(9950), "123", PaymentType.SENT, Int64( ((now - miliPerDay)  / 1000).ceil()), "Barber payment", "https://api.adorable.io/avatars/100/barber@gmail.com.png"),
-      new MockPaymentInfo(Int64(532000), Int64(9950), "123", PaymentType.RECEIVED, Int64( ((now - miliPerDay) / 1000).ceil()), "Bet Winner", "https://api.adorable.io/avatars/100/looser@gmail.com.png", "You are in luck!"),
-    ]);
-  }
-  
-  Payment get _paymentResponse => null;
-  final Int64 amount;
-  final Int64 fee;
-  final String hash;
-  final Int64 creationTimestamp;
-  final PaymentType type;
-  final String mockTitle;
-  final String invoiceNote;
-  final String imageURL;
-  Map _typeMap;
-
-  // TODO: implement _currency
-  @override
-  Currency get _currency => Currency.BTC;
-
-  // @override
-  // String get formattedAmount => CurrencyFormatter.format(amount, _currency);
-
-  @override
-  PaymentInfo copyWith(Currency currency) {
-    return null;
-  }
-
-  // TODO: implement currency
-  @override
-  Currency get currency => Currency.BTC;
-
-  // TODO: implement title
-  @override
-  String get title => mockTitle;
-
-  // TODO: implement description
-  @override
-  String get description => invoiceNote;
-
-  // TODO: implement destination
-  @override
-  String get destination => "02f6725f9c1c40333b67faea92fd211c183050f28df32cac3f9d69685fe9665432";
-
-  // TODO: implement paymentHash
-  @override
-  String get paymentHash => "c794bc55ec111256e41ba7c53f6aafadf39975a47abf52d496b662cdc4d5a90e";
-
-  // TODO: implement redeemTxID
-  @override
-  String get redeemTxID => null;
-
-  @override
-  // TODO: implement isUnknown
-  bool get isUnknown => false;
 }
