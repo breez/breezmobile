655be40d0c136307b26011eda233655f483d0827
Roei Erez <roeierez@gmail.com>
2018-12-30 16:38:24 +0200

    use textLoading component in accoutn page

--- lib/routes/user/home/status_text.dart
+++ lib/routes/user/home/status_text.dart
@@ -1,3 +1,4 @@
+import 'package:breez/widgets/loading_animated_text.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -36,8 +37,8 @@ class _StatusTextState extends State<StatusText> {
   @override
   Widget build(BuildContext context) {
     return widget._statusMessage == null
-        ? new Text("Start using Breez\nby adding funds to your balance.", textAlign: TextAlign.center)
-        : new Text(widget._statusMessage + "\n" + _animatedDots, textAlign: TextAlign.center);
+        ? LoadingAnimatedText("Start using Breez\nby adding funds to your balance.")
+        : LoadingAnimatedText(widget._statusMessage);        
   }
 
   String get _animatedDots =>
