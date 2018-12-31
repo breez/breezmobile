d9313f745aa7f8f76951d3f3c5fc2b48fea8194b
Roei Erez <roeierez@gmail.com>
2018-12-31 14:28:47 +0200

    implement createLoaderRoute function to show a transparent loader on top

--- lib/widgets/loader.dart
+++ lib/widgets/loader.dart
@@ -1,3 +1,4 @@
+import 'package:breez/widgets/transparent_page_route.dart';
 import 'package:flutter/material.dart';
 
 class Loader extends StatelessWidget {
@@ -11,3 +12,27 @@ class Loader extends StatelessWidget {
     ]);
   }
 }
+
+TransparentPageRoute createLoaderRoute(BuildContext context, {String message, double opacity = 0.3}){    
+    return TransparentPageRoute((context){
+        return Material(       
+          type: MaterialType.transparency, 
+          child: Container(
+            color: Colors.black.withOpacity(opacity),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Loader(),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: message != null ? Text(message,textAlign: TextAlign.center) : SizedBox(),
+                )
+              ],                    
+            )
+          ),
+        );
+      });
+  }
