90840b324e17887010218175443c3b0b87fe0823
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2019-01-17 22:20:12 +0300

    Added WebView and passed data between views

--- lib/routes/user/embedded_widgets/bitrefill_widget.dart
+++ lib/routes/user/embedded_widgets/bitrefill_widget.dart
@@ -1,8 +1,9 @@
+import 'dart:async';
 import 'package:breez/theme_data.dart' as theme;
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class BitrefillPage extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     return new BitrefillPageState();
@@ -10,31 +11,61 @@ class BitrefillPage extends StatefulWidget {
 }
 
 class BitrefillPageState extends State<BitrefillPage> {
-  
+  final Completer<WebViewController> _webViewController =
+      Completer<WebViewController>();
+  String _bgColor = 'yellow';
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          iconTheme: theme.appBarIconTheme,
-          textTheme: theme.appBarTextTheme,
-          backgroundColor: Color.fromRGBO(5, 93, 235, 1.0),
-          title: new Text(
-            "Hey",
-            style: theme.appBarTextStyle,
-          ),
-          elevation: 0.0,
+      appBar: new AppBar(
+        iconTheme: theme.appBarIconTheme,
+        textTheme: theme.appBarTextTheme,
+        backgroundColor: Color.fromRGBO(5, 93, 235, 1.0),
+        title: new Text(
+          "Bitrefill Widget",
+          style: theme.appBarTextStyle,
         ),
-        body: Stack(children: <Widget>[Center(child: Container(child: Text("Hello"),),)],));
+        elevation: 0.0,
+      ),
+      body: Center(
+        child: WebView(
+          initialUrl: "https://cdn.bitrefill.com/refill-widget.html",
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _webViewController.complete(webViewController);
+          },
+        ),
+      ),
+      floatingActionButton: getCurrentURL(),
+    );
   }
-}
 
-class SessionErrorWidget extends StatelessWidget {
-  final Object _error;
-
-  const SessionErrorWidget(this._error);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text("Failed connecting to session: ${_error.toString()}");
+  Widget getCurrentURL() {
+    return FutureBuilder<WebViewController>(
+        future: _webViewController.future,
+        builder: (BuildContext context,
+            AsyncSnapshot<WebViewController> controller) {
+          if (controller.hasData) {
+            return FloatingActionButton(
+              onPressed: () async {
+                final String url = await controller.data.currentUrl();
+                final String result = await controller.data.evaluateJavascript(
+                    "document.body.style.backgroundColor = '$_bgColor'");
+                Scaffold.of(context).showSnackBar(
+                  SnackBar(
+                      content: Text(
+                          "Current Page URL: $url and Background is set to $result")),
+                );
+              },
+              child: const Icon(
+                Icons.link,
+                color: Colors.white,
+              ),
+              backgroundColor: Color.fromRGBO(5, 93, 235, 1.0),
+            );
+          }
+          return Container();
+        });
   }
 }
