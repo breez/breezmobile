d6679b3cdf925cb80270b7d9b03c18d859d99ba7
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2019-01-26 22:05:56 +0300

    Set listened post messages to local variables

--- lib/routes/user/embedded_widgets/bitrefill_widget.dart
+++ lib/routes/user/embedded_widgets/bitrefill_widget.dart
@@ -6,6 +6,7 @@ import 'package:breez/bloc/account/account_bloc.dart';
 import 'package:breez/bloc/blocs_provider.dart';
 import 'package:breez/widgets/back_button.dart' as backBtn;
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'dart:convert' as JSON;
 import 'package:flutter/services.dart';
 
 class BitrefillPage extends StatefulWidget {
@@ -18,28 +19,30 @@ class BitrefillPage extends StatefulWidget {
 }
 
 class BitrefillPageState extends State<BitrefillPage> {
-  //static const _platform = const MethodChannel('flutter_webview_plugin');
   final widgetWebview = new FlutterWebviewPlugin();
-
+  String _orderId;
+  String _uri;
 
   @override
   void initState() {
     super.initState();
-    /*
-    _platform.setMethodCallHandler((call) {
-      if (call.method == "FlutterJSInterface.parseStringToJSONObject") {
-        print("Callback works!");
-      }
-      if (call.method == "parseStringToJSONObject") {
-        print("Callback works!!");
-      }
-    });
-    */
 
+    widgetWebview.testStream.listen((value) {
+      final order = JSON.jsonDecode(value);
+      print("Listened value: $order");
+      print("orderId: ${order['orderId']}");
+      print("uri:  ${order['uri']}");
+
+      setState(() {
+        _orderId = order['orderId'];
+        _uri = order['uri'];
+        widgetWebview.hide();
+      });
+    });
     widgetWebview.onStateChanged.listen((state) async {
       if (state.type == WebViewState.finishLoad) {
         String script = 'window.addEventListener("message", receiveMessage, false);' +
-            'function receiveMessage(event) {FlutterJSInterface.parseStringToJSONObject(event.data);}';
+            'function receiveMessage(event) {FlutterJsInterface.parseStringToJSONObject(event.data);}';
         widgetWebview.evalJavascript(script);
       }
     });
@@ -68,8 +71,41 @@ class BitrefillPageState extends State<BitrefillPage> {
           if (!snapshot.hasData) {
             return StaticLoader();
           }
+
           var account = snapshot.data;
           print('Account balance: ${account.currency.format(account.balance)}');
+
+          if (_orderId != null && _uri != null) {
+            return new Scaffold(
+                appBar: new AppBar(
+                  iconTheme: theme.appBarIconTheme,
+                  textTheme: theme.appBarTextTheme,
+                  backgroundColor: Color.fromRGBO(5, 93, 235, 1.0),
+                  leading: backBtn.BackButton(),
+                  title: new Text(
+                    widget._title,
+                    style: theme.appBarTextStyle,
+                  ),
+                  elevation: 0.0,
+                ),
+                body: new Container(
+                    child: Material(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                          child: Padding(
+                        padding:
+                            EdgeInsets.only(top: 50.0, left: 30.0, right: 30.0),
+                        child: Text("order id: $_orderId\n uri: $_uri",
+                            textAlign: TextAlign.center),
+                      ))
+                    ],
+                  ),
+                )));
+          }
+
           // set widgets userAccountBalance option to account.balance
           return new WebviewScaffold(
             appBar: new AppBar(
