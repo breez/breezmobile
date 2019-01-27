62e58f651781fbb5f37ce81715e667d3f5089f80
Roei Erez <roeierez@gmail.com>
2019-01-27 16:47:31 +0200

    implement JobManager for spinning one time work requests

---
+++ android/app/src/main/java/com/breez/client/job/JobManager.java
@@ -0,0 +1,37 @@
+package com.breez.client.job;
+
+import com.breez.client.plugins.breez.breezlib.ChainSync;
+import androidx.work.Constraints;
+import androidx.work.ExistingWorkPolicy;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+
+import static com.breez.client.plugins.breez.breezlib.ChainSync.UNIQUE_WORK_NAME;
+
+public class JobManager {
+
+    public static JobManager instance = new JobManager();
+
+    private JobManager(){}
+
+    public void enqueJob(String workName) throws Exception {
+        switch (workName) {
+            case UNIQUE_WORK_NAME:
+                startSyncJob();
+                return;
+            default:
+                throw new Exception("There is no associated work with " + workName);
+        }
+    }
+
+    private void startSyncJob(){
+        OneTimeWorkRequest oneTime = new OneTimeWorkRequest.Builder(ChainSync.class)
+                .setConstraints(
+                        new Constraints.Builder()
+                                .setRequiresBatteryNotLow(true)
+                                .build())
+                .build();
+
+        WorkManager.getInstance().enqueue(oneTime);        
+    }
+}
