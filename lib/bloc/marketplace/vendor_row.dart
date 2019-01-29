59f46b78c84755fc660def50f75805fb3f7058f5
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2019-01-30 00:48:34 +0300

    Created vendor widget

---
+++ lib/bloc/marketplace/vendor_row.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:breez/bloc/marketplace/vendor_model.dart';
+import 'package:breez/theme_data.dart' as theme;
+
+class VendorRow extends StatelessWidget {
+  final VendorInfo _vendor;
+
+  VendorRow(this._vendor);
+
+  @override
+  Widget build(BuildContext context) {
+    final _vendorLogo = _vendor.logo != null
+        ? Image(
+            image: AssetImage(_vendor.logo),
+            height: 55.0,
+            width: 55.0,
+          )
+        : Container();
+
+    final _vendorCard = Container(
+      margin: EdgeInsets.fromLTRB(32.0, 16.0, 32.0, 16.0),
+      constraints: BoxConstraints.expand(),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          _vendorLogo,
+          Text(_vendor.name, style: theme.vendorTitleStyle),
+        ],
+      ),
+    );
+
+    return _vendorCard;
+  }
+}
