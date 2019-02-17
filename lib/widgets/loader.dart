8aa4027917b34c8f86b006df8d7354384886e723
Roei Erez <roeierez@gmail.com>
2019-02-17 15:40:05 +0200

    add loader widget with transaprent black background.

--- lib/widgets/loader.dart
+++ lib/widgets/loader.dart
@@ -6,33 +6,69 @@ class Loader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(alignment: FractionalOffset.center, children: <Widget>[
       new CircularProgressIndicator(
-        valueColor: new AlwaysStoppedAnimation<Color>(Color.fromRGBO(51, 255, 255, 0.7),),
+        valueColor: new AlwaysStoppedAnimation<Color>(
+          Color.fromRGBO(51, 255, 255, 0.7),
+        ),
         backgroundColor: Colors.red,
       ),
     ]);
   }
 }
 
-TransparentPageRoute createLoaderRoute(BuildContext context, {String message, double opacity = 0.3}){    
-    return TransparentPageRoute((context){
-        return Material(       
-          type: MaterialType.transparency, 
-          child: Container(
-            color: Colors.black.withOpacity(opacity),
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-              child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Loader(),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  child: message != null ? Text(message,textAlign: TextAlign.center) : SizedBox(),
-                )
-              ],                    
-            )
-          ),
-        );
-      });
+TransparentPageRoute createLoaderRoute(BuildContext context,
+    {String message, double opacity = 0.5}) {
+  return TransparentPageRoute((context) {
+    return Material(
+      type: MaterialType.transparency,
+      child: Container(
+          color: Colors.black.withOpacity(opacity),
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Loader(),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: message != null
+                    ? Text(message, textAlign: TextAlign.center)
+                    : SizedBox(),
+              )
+            ],
+          )),
+    );
+  });
+}
+
+class FullScreenLoader extends StatelessWidget {
+  final String message;
+  final double opacity;
+
+  const FullScreenLoader({Key key, this.message, this.opacity = 0.3})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      type: MaterialType.transparency,
+      child: Container(
+          color: Colors.black.withOpacity(0.5),
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Loader(),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: message != null
+                    ? Text(message, textAlign: TextAlign.center)
+                    : SizedBox(),
+              )
+            ],
+          )),
+    );
   }
+}
