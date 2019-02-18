3bd10e03d4d73beecae8604638e7e6ae20d358be
Roei Erez <roeierez@gmail.com>
2019-02-18 13:19:49 +0200

    add fastbitcoins confirmation dialog

---
+++ lib/routes/user/add_funds/fastbitcoins_confirm.dart
@@ -0,0 +1,64 @@
+import 'package:breez/bloc/fastbitcoins/fastbitcoins_model.dart';
+import 'package:breez/bloc/user_profile/breez_user_model.dart';
+import 'package:fixnum/fixnum.dart';
+import 'package:flutter/material.dart';
+import 'package:breez/theme_data.dart' as theme;
+
+class FastBitcoinsConfirmWidget extends StatelessWidget {
+  final ValidateRequestModel request;
+  final ValidateResponseModel response;
+  final BreezUserModel user;
+
+  const FastBitcoinsConfirmWidget(
+      {Key key, this.user, this.request, this.response})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String btcReceived = user.currency.format(Int64(response.satoshiAmount));
+    String comissionRate =
+        (response.commissionTotal / response.value * 100).toString();
+
+    return Column(
+      children: <Widget>[
+        ConfirmationItem(
+            title: "Voucher value",
+            details: '${response.value} ${request.currency}'),
+        ConfirmationItem(
+            title: "Exchange rate",
+            details: '${response.exchangeRate} ${request.currency}'),
+        ConfirmationItem(title: "Comission rate", details: comissionRate),
+        ConfirmationItem(
+            title: "Comission total",
+            details: '${response.commissionTotal} ${request.currency}'),
+        ConfirmationItem(title: "Bitcoins received", details: btcReceived),
+      ],
+    );
+  }
+}
+
+class ConfirmationItem extends StatelessWidget {
+  final String title;
+  final String details;
+
+  const ConfirmationItem({Key key, this.title, this.details}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 48.0,
+      child: ListTile(
+        title: Text(
+          title,
+          style: theme.paymentDetailsTitleStyle,
+          textAlign: TextAlign.left,
+        ),
+        trailing: Text(
+          details,
+          style: theme.paymentDetailsSubtitleStyle,
+          textAlign: TextAlign.left,
+        ),
+      ),
+    );
+  }
+}
