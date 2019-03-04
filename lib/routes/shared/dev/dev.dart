b123844cff4243b2c191af01951e35d0af6a97d0
Roei Erez <roeierez@gmail.com>
2019-03-04 19:07:13 +0200

    dev: Add enable/disable account action to dev menu

--- lib/routes/shared/dev/dev.dart
+++ lib/routes/shared/dev/dev.dart
@@ -27,8 +27,7 @@ class LinkTextSpan extends TextSpan {
             recognizer: new TapGestureRecognizer()
               ..onTap = () {
                 _cliInputController.text = command + " ";
-                FocusScope
-                    .of(_scaffoldKey.currentState.context)
+                FocusScope.of(_scaffoldKey.currentState.context)
                     .requestFocus(_cliEntryFocusNode);
               });
 }
@@ -104,174 +103,197 @@ class DevViewState extends State<DevView> {
   Widget build(BuildContext context) {
     AccountBloc accBloc = AppBlocsProvider.of<AccountBloc>(context);
     return StreamBuilder(
-      stream: accBloc.accountSettingsStream,
-      builder: (context, settingsSnapshot) {
-        return new Scaffold(
-          key: _scaffoldKey,
-          appBar: new AppBar(
-            iconTheme: theme.appBarIconTheme,
-            textTheme: theme.appBarTextTheme,
-            backgroundColor: theme.BreezColors.blue[500],
-            leading: backBtn.BackButton(),
-            elevation: 0.0,
-            actions: <Widget>[
-              PopupMenuButton<Choice>(
-                onSelected: widget._select,
-                itemBuilder: (BuildContext context) {
-                  return getChoices(accBloc, settingsSnapshot.data).map((Choice choice) {
-                    return PopupMenuItem<Choice>(
-                      value: choice,
-                      child: Text(choice.title),
-                    );
-                  }).toList();
-                },
-              ),
-            ],
-            title: new Text(
-              "Developers",
-              style: theme.appBarTextStyle,
-            ),
-          ),
-          body: new Column(mainAxisSize: MainAxisSize.max, children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(left: 10.0),
-              child: new Row(
-                children: <Widget>[
-                  new Flexible(
-                      child: new TextField(
-                    focusNode: _cliEntryFocusNode,
-                    controller: _cliInputController,
-                    decoration: InputDecoration(
-                        hintText: 'Enter a command or use the links below'),
-                    onSubmitted: (command) {
-                      _sendCommand(command);
-                    },
-                  )),
-                  new IconButton(
-                    icon: new Icon(Icons.play_arrow),
-                    tooltip: 'Run',
-                    onPressed: () {
-                      _sendCommand(_cliInputController.text);
-                    },
-                  ),
-                  new IconButton(
-                    icon: new Icon(Icons.clear),
-                    tooltip: 'Clear',
-                    onPressed: () {
-                      setState(() {
-                        _cliInputController.clear();
-                        _showDefaultCommands = true;
-                        _cliText = "";
-                        _richCliText = defaultCliCommandsText;
-                      });
-                    },
-                  ),
-                ],
-              ),
-            ),
-            new Expanded(
-                flex: 1,
-                child: new Container(
-                  padding: new EdgeInsets.all(10.0),
-                  child: new Container(
-                    padding: _showDefaultCommands
-                        ? new EdgeInsets.all(0.0)
-                        : new EdgeInsets.all(2.0),
-                    decoration: new BoxDecoration(
-                        border: _showDefaultCommands ? null : new Border.all(
-                            width: 1.0,
-                            color: Color(0x80FFFFFF))),
-                    child: new Column(
-                      mainAxisSize: MainAxisSize.max,
-                      children: <Widget>[
-                        _showDefaultCommands
-                            ? new Container()
-                            : new Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: <Widget>[
-                                  new IconButton(
-                                    icon: new Icon(Icons.content_copy),
-                                    tooltip: 'Copy to Clipboard',
-                                    iconSize: 19.0,
-                                    onPressed: () {
-                                      Clipboard.setData(
-                                          new ClipboardData(text: _cliText));
-                                      _scaffoldKey.currentState
-                                          .showSnackBar(new SnackBar(
-                                        content: new Text(
-                                          'Copied to clipboard.',
-                                          style: theme.snackBarStyle,
-                                        ),
-                                        backgroundColor:
-                                            theme.snackBarBackgroundColor,
-                                        duration: new Duration(seconds: 2),
-                                      ));
-                                    },
-                                  ),
-                                  new IconButton(
-                                    icon: new Icon(Icons.share),
-                                    iconSize: 19.0,
-                                    tooltip: 'Share',
-                                    onPressed: () {
-                                      Share.share(_cliText);
-                                    },
-                                  )
-                                ],
-                              ),
-                        new Expanded(
-                            child: new SingleChildScrollView(
-                                child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: new Row(
-                            children: <Widget>[
-                              new Expanded(
-                                  child: new RichText(
-                                      text: new TextSpan(
-                                          style: _cliTextStyle,
-                                          children: _richCliText)))
-                            ],
-                          ),
-                        )))
-                      ],
+        stream: accBloc.accountStream,
+        builder: (accCtx, accSnapshot) {
+          AccountModel account = accSnapshot.data;
+          return StreamBuilder(
+              stream: accBloc.accountSettingsStream,
+              builder: (context, settingsSnapshot) {
+                return new Scaffold(
+                  key: _scaffoldKey,
+                  appBar: new AppBar(
+                    iconTheme: theme.appBarIconTheme,
+                    textTheme: theme.appBarTextTheme,
+                    backgroundColor: theme.BreezColors.blue[500],
+                    leading: backBtn.BackButton(),
+                    elevation: 0.0,
+                    actions: <Widget>[
+                      PopupMenuButton<Choice>(
+                        onSelected: widget._select,
+                        itemBuilder: (BuildContext context) {
+                          return getChoices(
+                                  accBloc, settingsSnapshot.data, account)
+                              .map((Choice choice) {
+                            return PopupMenuItem<Choice>(
+                              value: choice,
+                              child: Text(choice.title),
+                            );
+                          }).toList();
+                        },
+                      ),
+                    ],
+                    title: new Text(
+                      "Developers",
+                      style: theme.appBarTextStyle,
                     ),
                   ),
-                )),
-            new LNDBootstrapProgress(),
-          ]),
-        );
-      }
-    );
+                  body: new Column(mainAxisSize: MainAxisSize.max, children: <
+                      Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10.0),
+                      child: new Row(
+                        children: <Widget>[
+                          new Flexible(
+                              child: new TextField(
+                            focusNode: _cliEntryFocusNode,
+                            controller: _cliInputController,
+                            decoration: InputDecoration(
+                                hintText:
+                                    'Enter a command or use the links below'),
+                            onSubmitted: (command) {
+                              _sendCommand(command);
+                            },
+                          )),
+                          new IconButton(
+                            icon: new Icon(Icons.play_arrow),
+                            tooltip: 'Run',
+                            onPressed: () {
+                              _sendCommand(_cliInputController.text);
+                            },
+                          ),
+                          new IconButton(
+                            icon: new Icon(Icons.clear),
+                            tooltip: 'Clear',
+                            onPressed: () {
+                              setState(() {
+                                _cliInputController.clear();
+                                _showDefaultCommands = true;
+                                _cliText = "";
+                                _richCliText = defaultCliCommandsText;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    new Expanded(
+                        flex: 1,
+                        child: new Container(
+                          padding: new EdgeInsets.all(10.0),
+                          child: new Container(
+                            padding: _showDefaultCommands
+                                ? new EdgeInsets.all(0.0)
+                                : new EdgeInsets.all(2.0),
+                            decoration: new BoxDecoration(
+                                border: _showDefaultCommands
+                                    ? null
+                                    : new Border.all(
+                                        width: 1.0, color: Color(0x80FFFFFF))),
+                            child: new Column(
+                              mainAxisSize: MainAxisSize.max,
+                              children: <Widget>[
+                                _showDefaultCommands
+                                    ? new Container()
+                                    : new Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.end,
+                                        children: <Widget>[
+                                          new IconButton(
+                                            icon: new Icon(Icons.content_copy),
+                                            tooltip: 'Copy to Clipboard',
+                                            iconSize: 19.0,
+                                            onPressed: () {
+                                              Clipboard.setData(
+                                                  new ClipboardData(
+                                                      text: _cliText));
+                                              _scaffoldKey.currentState
+                                                  .showSnackBar(new SnackBar(
+                                                content: new Text(
+                                                  'Copied to clipboard.',
+                                                  style: theme.snackBarStyle,
+                                                ),
+                                                backgroundColor: theme
+                                                    .snackBarBackgroundColor,
+                                                duration:
+                                                    new Duration(seconds: 2),
+                                              ));
+                                            },
+                                          ),
+                                          new IconButton(
+                                            icon: new Icon(Icons.share),
+                                            iconSize: 19.0,
+                                            tooltip: 'Share',
+                                            onPressed: () {
+                                              Share.share(_cliText);
+                                            },
+                                          )
+                                        ],
+                                      ),
+                                new Expanded(
+                                    child: new SingleChildScrollView(
+                                        child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: new Row(
+                                    children: <Widget>[
+                                      new Expanded(
+                                          child: new RichText(
+                                              text: new TextSpan(
+                                                  style: _cliTextStyle,
+                                                  children: _richCliText)))
+                                    ],
+                                  ),
+                                )))
+                              ],
+                            ),
+                          ),
+                        )),
+                    new LNDBootstrapProgress(),
+                  ]),
+                );
+              });
+        });
   }
 
-  List<Choice> getChoices(AccountBloc accBloc, AccountSettings settings){
-    List<Choice> choices = <Choice>[
+  List<Choice> getChoices(
+      AccountBloc accBloc, AccountSettings settings, AccountModel account) {
+    List<Choice> choices = new List<Choice>();
+    choices.addAll([
+      Choice(title: 'Share Logs', icon: Icons.share, function: shareLog),
       Choice(
-        title: 'Share Logs', 
-        icon: Icons.share, 
-        function: shareLog),
+          title: 'Show Initial Screen',
+          icon: Icons.phone_android,
+          function: _gotoInitialScreen),
       Choice(
-        title: 'Show Initial Screen',
-        icon: Icons.phone_android,
-        function: _gotoInitialScreen),
+          title: 'Battery Optimization',
+          icon: Icons.phone_android,
+          function: _showOptimizationsSettings),
       Choice(
-        title: 'Battery Optimization',
-        icon: Icons.phone_android,
-        function: _showOptimizationsSettings),
-      Choice(
-        title: '${settings.showConnectProgress ? "Hide" : "Show"} Connect Progress',
-        icon: Icons.phone_android,
-        function: (){
-          toggleConnectProgress(accBloc, settings);
-        }),
-    ];
+          title:
+              '${settings.showConnectProgress ? "Hide" : "Show"} Connect Progress',
+          icon: Icons.phone_android,
+          function: () {
+            toggleConnectProgress(accBloc, settings);
+          })
+    ]);
+
+    if (!account.enableInProgress) {
+      choices.add(Choice(
+          title: account.enabled
+              ? "Disable Breez Account"
+              : "Enable Breez Account",
+          icon: Icons.phone_android,
+          function: () {
+            accBloc.accountEnableSink.add(!account.enabled);
+          }));
+    }
+
     return choices;
-  }  
+  }
 
   void _gotoInitialScreen() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setBool('isFirstRun', true);
-    Navigator
-        .of(_scaffoldKey.currentState.context)
+    Navigator.of(_scaffoldKey.currentState.context)
         .pushReplacementNamed("/splash");
   }
 
@@ -279,7 +301,8 @@ class DevViewState extends State<DevView> {
     widget._permissionsService.requestOptimizationSettings();
   }
 
-  void toggleConnectProgress(AccountBloc bloc, AccountSettings settings){
-    bloc.accountSettingsSink.add(settings.copyWith(showConnectProgress: !settings.showConnectProgress));
+  void toggleConnectProgress(AccountBloc bloc, AccountSettings settings) {
+    bloc.accountSettingsSink.add(
+        settings.copyWith(showConnectProgress: !settings.showConnectProgress));
   }
 }
