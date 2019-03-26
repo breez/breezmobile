59787c3af65738e2971640fba40c5e00bf323a6e
Roei Erez <roeierez@gmail.com>
2019-03-26 15:41:34 +0200

    upgrade flashbar, fixes dismiss bug.

--- lib/widgets/flushbar.dart
+++ lib/widgets/flushbar.dart
@@ -6,25 +6,19 @@ import 'package:breez/theme_data.dart' as theme;
 
 Flushbar showFlushbar(BuildContext context, {String title = "", String message = "", Duration duration = const Duration(seconds: 8)}) {
   Flushbar flush;
-  flush = Flushbar() // <bool> is the type of the result passed to dismiss() and collected by show().then((result){})
-    ..titleText = new Text(title, style: TextStyle(height: 0.0))
-    ..messageText = new Text(message, style: theme.snackBarStyle, textAlign: TextAlign.left)    
-    ..backgroundColor = theme.snackBarBackgroundColor
-    ..mainButton = FlatButton(
+  flush = Flushbar(
+    titleText: Text(title, style: TextStyle(height: 0.0)),
+    duration: duration,
+    messageText: new Text(message, style: theme.snackBarStyle, textAlign: TextAlign.left),
+    backgroundColor: theme.snackBarBackgroundColor,
+    mainButton: FlatButton(
       onPressed: () {
         flush.dismiss(true); // result = true
       },
       child: Text("OK", style: theme.validatorStyle),
     )
-    ..show(context);
-  
-  if (duration != null) {    
-    Timer(duration, (){  
-      if (!flush.isDismissed()) {          
-        flush.dismiss();
-      }
-    });
-  }
+  )..show(context);
+
   return flush;
 }
 
