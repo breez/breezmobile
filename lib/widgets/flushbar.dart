1fa53a3d32ae6f5e1f48b9681c1ef30525c90b01
Roei Erez <roeierez@gmail.com>
2019-03-26 15:30:43 +0200

    flushbar: fix flushbar dismiss bug.
    This commit fixes the dismiss flashbar to only dismiss the current
    instance.
    In case the instance is not on top of the stack it just removes the
    route without poping other routes.

--- lib/widgets/flushbar.dart
+++ lib/widgets/flushbar.dart
@@ -1,13 +1,14 @@
+import 'dart:async';
+
 import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:breez/theme_data.dart' as theme;
 
-showFlushbar(BuildContext context, {String title = "", String message = "", int seconds = 8}) {
-  var flush;
+Flushbar showFlushbar(BuildContext context, {String title = "", String message = "", Duration duration = const Duration(seconds: 8)}) {
+  Flushbar flush;
   flush = Flushbar() // <bool> is the type of the result passed to dismiss() and collected by show().then((result){})
     ..titleText = new Text(title, style: TextStyle(height: 0.0))
-    ..messageText = new Text(message, style: theme.snackBarStyle, textAlign: TextAlign.left)
-    ..duration = Duration(seconds: seconds)
+    ..messageText = new Text(message, style: theme.snackBarStyle, textAlign: TextAlign.left)    
     ..backgroundColor = theme.snackBarBackgroundColor
     ..mainButton = FlatButton(
       onPressed: () {
@@ -16,6 +17,15 @@ showFlushbar(BuildContext context, {String title = "", String message = "", int 
       child: Text("OK", style: theme.validatorStyle),
     )
     ..show(context);
+  
+  if (duration != null) {    
+    Timer(duration, (){  
+      if (!flush.isDismissed()) {          
+        flush.dismiss();
+      }
+    });
+  }
+  return flush;
 }
 
 void popFlushbars(BuildContext context){
