c5496a1bd6d786354822fa7c0178327dc5a7a030
Roei Erez <roeierez@gmail.com>
2019-04-09 21:54:31 +0300

    impement compact qr image widget. This widget uses this table https://www.qrcode.com/en/about/version.html to determinte the qr version in order to use the minimum rendering space needed.

---
+++ lib/widgets/compact_qr_image.dart
@@ -0,0 +1,36 @@
+
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+var _versionsToMaxCharacters = [
+  17, 32, 53, 78, 106, 134, 154, 192, 230, 271, 
+  321, 367, 425, 458, 520, 586, 644, 718, 792, 858,
+  929, 1003, 1091, 1171, 1273, 1367, 1465, 1528, 1628, 1732,
+  1840, 1952, 2068, 2188, 2303, 2431, 2563, 2699, 2809, 2953
+];
+
+class CompactQRImage extends StatelessWidget {
+  final String data;
+  final double size;
+
+  CompactQRImage({Key key, this.data, this.size}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return QrImage(
+      version: _calculateVersion(),
+      data: data,
+      size: this.size,
+    );
+  }
+
+  int _calculateVersion() {
+    int index;
+    for (index = 0; index < _versionsToMaxCharacters.length; ++index) {
+      if (_versionsToMaxCharacters[index] > data.length) {
+        break;
+      }
+    }    
+    return index+1;
+  }  
+}
