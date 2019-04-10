d344ffe08942dcd6608e21a44dfdc2941f4b9432
Roei Erez <roeierez@gmail.com>
2019-04-10 17:45:44 +0300

    add Breez plugin file

---
+++ ios/Plugins/Breez/BreezLib/Breez.swift
@@ -0,0 +1,13 @@
+//
+//  Breez.swift
+//  Runner
+//
+//  Created by Roei Erez on 4/10/19.
+//  Copyright © 2019 The Chromium Authors. All rights reserved.
+//
+
+import Foundation
+
+class Breez {
+    
+}
