378d5ed01567726c3239bdc696294bf4ac93e75b
Roei Erez <roeierez@gmail.com>
2019-04-11 13:22:54 +0300

    implement breez plugin with native methods

--- ios/Plugins/Breez/BreezLib/Breez.swift
+++ ios/Plugins/Breez/BreezLib/Breez.swift
@@ -7,7 +7,96 @@
 //
 
 import Foundation
+import Flutter
+import Bindings
 
-class Breez {
+
+class Breez : NSObject, FlutterPlugin, BindingsAppServicesProtocol, FlutterStreamHandler {
     
+    var eventSink : FlutterEventSink?;
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let BREEZ_CHANNEL_NAME = "com.breez.client/breez_lib";
+        let BREEZ_STREAM_NAME = "com.breez.client/breez_lib_notifications";
+        
+        let instance = Breez();
+        let channel = FlutterMethodChannel(name: BREEZ_CHANNEL_NAME, binaryMessenger: registrar.messenger());
+        registrar.addMethodCallDelegate(instance, channel: channel);
+        
+        let eventChannel = FlutterEventChannel(name: BREEZ_STREAM_NAME, binaryMessenger: registrar.messenger());
+        eventChannel.setStreamHandler(instance);
+        
+    }
+    
+    func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if (call.method == "init") {
+            initBreez(call: call, result: result);
+            return;
+        }
+       
+        if (call.method == "log") {
+            log(call: call, result: result);
+        }
+        
+        let executor = NativeMethods.getExecutor(forMethod: call.method);
+        if executor != nil {
+            executor?.execute(call: call, result: result);
+        }
+    }
+    
+    func initBreez(call: FlutterMethodCall, result: @escaping FlutterResult){
+        if let args = call.arguments as? Dictionary<String,Any> {
+            let tempDir : String = args["tempDir"] as! String;
+            let workingDir : String = args["workingDir"] as! String;
+            var err : NSError?
+            if (!BindingsInit(tempDir, workingDir, self, &err)){
+                result(true)
+            } else {
+                result(err);
+            }
+        }
+        result(FlutterError(code: "Missing Argument", message: "Expecting a dictionary", details: nil));
+    }
+    
+    func log(call: FlutterMethodCall, result: @escaping FlutterResult){
+        if let args = call.arguments as? Dictionary<String,Any> {
+            let msg : String = args["msg"] as! String;
+            let lvl : String = args["lvl"] as! String;
+            BindingsLog(msg, lvl);
+            result(true);
+        }
+        result(FlutterError(code: "Missing Argument", message: "Expecting a dictionary", details: nil));
+    }
+    
+    // FlutterStreamHandler protocol
+    func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        eventSink = events;
+        return nil;
+    }
+    
+    func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        eventSink = nil;
+        return nil;
+    }
+    
+    // BindingsAppServicesProtocol protocol
+    func backupProviderName() -> String! {
+        return "gdrive"
+    }
+    
+    func backupProviderSignIn() throws -> String {
+        return "";
+    }
+    
+    func notify(_ notificationEvent: Data!) {
+        if let sink = eventSink {
+            var data = Data(count:0);
+            if let wrapped = notificationEvent {
+                data = wrapped;
+            }
+            sink(FlutterStandardTypedData(bytes: data));
+        }
+    }
 }
+
+
---
+++ ios/Plugins/Breez/BreezLib/NativeMethods.swift
@@ -0,0 +1,148 @@
+//
+//  NativeMethods.swift
+//  Runner
+//
+//  Created by Roei Erez on 4/11/19.
+//  Copyright © 2019 The Chromium Authors. All rights reserved.
+//
+
+import Foundation
+import Flutter
+import Bindings
+
+public class NativeMethods {
+    static func getExecutor(forMethod: String) -> BindingExecutor? {
+        return calls[forMethod];
+    }
+}
+
+
+fileprivate let calls : Dictionary<String, BindingExecutor> = [
+    "start": EmptyArgsBindingExecutor(f: BindingsStart),
+    "stop": VoidBindingExecutor(f: BindingsStop),
+    
+    "addFundsInit": SingleArgBindingExecutor(f: BindingsAddFundsInit),
+    "addInvoice": SingleArgBindingExecutor(f: BindingsAddInvoice),
+    "availableSnapshots": SingleArgBindingExecutor(f: BindingsAvailableSnapshots),
+    "bootstrapFiles": SingleArgBindingExecutor(f: BindingsBootstrapFiles),
+    "connectAccount": EmptyArgsBindingExecutor(f: BindingsConnectAccount),
+    
+    "decodePaymentRequest": SingleArgBindingExecutor(f: BindingsDecodePaymentRequest),
+    "enableAccount": SingleArgBindingExecutor(f: BindingsEnableAccount),
+    "getAccountInfo": EmptyArgsBindingExecutor(f: BindingsGetAccountInfo),
+    
+    "getFundStatus": SingleArgBindingExecutor(f: BindingsGetFundStatus),
+    "getPayments": EmptyArgsBindingExecutor(f: BindingsGetPayments),
+    "getRefundableSwapAddresses": EmptyArgsBindingExecutor(f: BindingsGetRefundableSwapAddresses),
+    
+    "getRelatedInvoice": SingleArgBindingExecutor(f: BindingsGetRelatedInvoice),
+    "getLogger": SingleArgBindingExecutor(f: BindingsGetLogger),
+    "createRatchetSession": SingleArgBindingExecutor(f: BindingsCreateRatchetSession),
+    "ratchetDecrypt": SingleArgBindingExecutor(f: BindingsRatchetDecrypt),
+    "ratchetEncrypt": SingleArgBindingExecutor(f: BindingsRatchetEncrypt),
+    "ratchetSessionInfo": SingleArgBindingExecutor(f: BindingsRatchetSessionInfo),
+    "ratchetSessionSetInfo": SingleArgBindingExecutor(f: BindingsRatchetSessionSetInfo),
+    "refund": SingleArgBindingExecutor(f: BindingsRefund),
+    "registerChannelOpenedNotification": SingleArgBindingExecutor(f: BindingsRegisterChannelOpenedNotification),
+    "registerPeriodicSync": SingleArgBindingExecutor(f: BindingsRegisterPeriodicSync),
+    "registerReceivePaymentReadyNotification": SingleArgBindingExecutor(f: BindingsRegisterReceivePaymentReadyNotification),
+    "restoreBackup": SingleArgBindingExecutor(f: BindingsRestoreBackup),
+    "sendCommand": SingleArgBindingExecutor(f: BindingsSendCommand),
+    "sendPaymentFailureBugReport": SingleArgBindingExecutor(f: BindingsSendPaymentFailureBugReport),
+    "sendPaymentForRequest": SingleArgBindingExecutor(f: BindingsSendPaymentForRequest),
+    "sendWalletCoins": SingleArgBindingExecutor(f: BindingsSendWalletCoins),
+    "validateAddress": SingleArgBindingExecutor(f: BindingsValidateAddress),
+    "removeFund": SingleArgBindingExecutor(f: BindingsRemoveFund),
+    
+    "daemonReady": VoidBindingExecutor(f: BindingsDaemonReady),
+    "getDefaultOnChainFeeRate": VoidBindingExecutor(f: BindingsGetDefaultOnChainFeeRate),
+    "isConnectedToRoutingNode": VoidBindingExecutor(f: BindingsIsConnectedToRoutingNode),
+    "onResume": VoidBindingExecutor(f: BindingsOnResume),
+    "requestBackup": VoidBindingExecutor(f: BindingsRequestBackup),
+    "waitDaemonShutdown": VoidBindingExecutor(f: BindingsWaitDaemonShutdown),
+    "getLogPath": VoidBindingExecutor(f: BindingsGetLogPath)
+    
+    //jobs
+    //    FOUNDATION_EXPORT id<BindingsJobController> BindingsNewClosedChannelsJob(NSString* workingDir, NSError** error);
+    //    FOUNDATION_EXPORT id<BindingsJobController> BindingsNewSyncJob(NSString* workingDir, NSError** error);
+    
+];
+
+
+
+protocol BindingExecutor {
+    func execute(call : FlutterMethodCall, result : @escaping FlutterResult);
+}
+
+fileprivate extension BindingExecutor {    
+    
+    func unwrapInputType(arg: Any?) -> Any? {
+        if let data = arg as? FlutterStandardTypedData {
+            return data.data;
+        }
+        return arg;
+    }
+    
+    func wrapOutputType(arg: Any?) -> Any? {
+        if let data = arg as? Data {
+            return FlutterStandardTypedData(bytes: data);
+        }
+        return arg;
+    }
+}
+
+fileprivate class SingleArgBindingExecutor<T,O> : BindingExecutor {
+    var bindingFunc : (T, NSErrorPointer) -> O;
+    
+    init(f: @escaping (T, NSErrorPointer) -> O) {
+        self.bindingFunc = f;
+    }
+    
+    func execute(call : FlutterMethodCall, result : @escaping FlutterResult){
+        DispatchQueue.global().async {
+            let args = call.arguments as! Dictionary<String,Any>
+            let arg = self.unwrapInputType(arg: args["argument"]);
+            var error : NSError?;
+            let res = self.bindingFunc(arg as! T, &error);
+            if let err = error {
+                result(err);
+            } else {
+                result(self.wrapOutputType(arg: res))
+            }
+        }
+    }
+}
+
+fileprivate class EmptyArgsBindingExecutor<O> : BindingExecutor {
+    var bindingFunc : (NSErrorPointer) -> O;
+    
+    init(f: @escaping (NSErrorPointer) -> O) {
+        self.bindingFunc = f;
+    }
+    
+    func execute(call : FlutterMethodCall, result : @escaping FlutterResult){
+         DispatchQueue.global().async {
+            var error : NSError?;
+            let res = self.bindingFunc(&error);
+            if let err = error {
+                result(err);
+            } else {
+                result(self.wrapOutputType(arg: res))
+            }
+        }
+    }
+}
+
+fileprivate class VoidBindingExecutor<O> : BindingExecutor {
+    var bindingFunc : () -> O;
+    
+    init(f: @escaping () -> O) {
+        self.bindingFunc = f;
+    }
+    
+    func execute(call : FlutterMethodCall, result : @escaping FlutterResult){
+        DispatchQueue.global().async {
+            result(self.wrapOutputType(arg: self.bindingFunc()));
+        }
+    }
+}
