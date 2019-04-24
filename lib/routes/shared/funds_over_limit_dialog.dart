fd4087a115fde7ec18ca4a024be94f331ed68aad
Roei Erez <roeierez@gmail.com>
2019-04-24 09:23:32 +0300

    show loader when fetching swap status in add funds page.

--- lib/routes/shared/funds_over_limit_dialog.dart
+++ lib/routes/shared/funds_over_limit_dialog.dart
@@ -18,10 +18,14 @@ class OverLimitFundsDialog extends StatefulWidget {
 }
 
 class OverLimitFundsDialogState extends State<OverLimitFundsDialog> {
+  Future _fetchFuture;
+
   @override
   void initState() {
     super.initState();
-    widget.accountBloc.userActionsSink.add(FetchSwapFundStatus());
+    var fetchAction = FetchSwapFundStatus();
+    _fetchFuture = fetchAction.future;
+    widget.accountBloc.userActionsSink.add(fetchAction);
   }
 
   @override
@@ -33,51 +37,60 @@ class OverLimitFundsDialogState extends State<OverLimitFundsDialog> {
           style: theme.alertTitleStyle,
         ),
         contentPadding: EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 24.0),
-        content: StreamBuilder<AccountModel>(
-            stream: widget.accountBloc.accountStream,
-            builder: (ctx, snapshot) {
-              var swapStatus = snapshot?.data?.swapFundsStatus;
-              if (swapStatus == null) {
+        content: FutureBuilder(
+            future: this._fetchFuture,
+            initialData: "loading",
+            builder: (ctx, loadingSnapshot) {
+              if (loadingSnapshot.data == "loading") {
                 return Loader();
               }
 
-              String hoursToUnlock = swapStatus.hoursToUnlock > 1
-                  ? "~${swapStatus.hoursToUnlock.toInt().toString()} hours"
-                  : "in about an hour";
-              List<TextSpan> redeemText = List<TextSpan>();
-              if (swapStatus.hoursToUnlock > 0) {
-                redeemText.add(TextSpan(
-                    text:
-                        "You will be able to redeem your funds in block ${swapStatus.lockHeight} ($hoursToUnlock).",
-                    style: theme.dialogGrayStyle));
-              } else {
-                redeemText.addAll([
-                  TextSpan(text: "You can ", style: theme.dialogGrayStyle),
-                  TextSpan(
-                      text: "get a refund ",
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () async {
-                          Navigator.pop(context);
-                          Navigator.pushNamed(context, "/get_refund");
-                        },
-                      style: theme.blueLinkStyle),
-                  TextSpan(text: "now.", style: theme.dialogGrayStyle),
-                ]);
-              }
+              return StreamBuilder<AccountModel>(
+                  stream: widget.accountBloc.accountStream,
+                  builder: (ctx, snapshot) {
+                    var swapStatus = snapshot?.data?.swapFundsStatus;
+                    if (swapStatus == null) {
+                      return Loader();
+                    }
 
-              return RichText(
-                  text: TextSpan(
-                      style: theme.dialogGrayStyle,
-                      text:
-                          "Breez was not able to transfer the funds to your balance since the executed transaction was above the specified limit.\n",
-                      children: redeemText));
+                    String hoursToUnlock = swapStatus.hoursToUnlock > 1
+                        ? "~${swapStatus.hoursToUnlock.toInt().toString()} hours"
+                        : "in about an hour";
+                    List<TextSpan> redeemText = List<TextSpan>();
+                    if (swapStatus.hoursToUnlock > 0) {
+                      redeemText.add(TextSpan(
+                          text:
+                              "You will be able to redeem your funds in block ${swapStatus.lockHeight} ($hoursToUnlock).",
+                          style: theme.dialogGrayStyle));
+                    } else {
+                      redeemText.addAll([
+                        TextSpan(
+                            text: "You can ", style: theme.dialogGrayStyle),
+                        TextSpan(
+                            text: "get a refund ",
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () async {
+                                Navigator.pop(context);
+                                Navigator.pushNamed(context, "/get_refund");
+                              },
+                            style: theme.blueLinkStyle),
+                        TextSpan(text: "now.", style: theme.dialogGrayStyle),
+                      ]);
+                    }
+
+                    return RichText(
+                        text: TextSpan(
+                            style: theme.dialogGrayStyle,
+                            text:
+                                "Breez was not able to transfer the funds to your balance since the executed transaction was above the specified limit.\n",
+                            children: redeemText));
+                  });
             }),
         actions: [
-            new SimpleDialogOption(
-              onPressed: () => Navigator.pop(context),
-              child: new Text("OK", style: theme.buttonStyle),
-            )            
-          ]  
-      );
+          new SimpleDialogOption(
+            onPressed: () => Navigator.pop(context),
+            child: new Text("OK", style: theme.buttonStyle),
+          )
+        ]);
   }
 }
