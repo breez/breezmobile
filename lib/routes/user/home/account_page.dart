ee0557bf9533102b423153e902664d2305a0e1d7
Roei Erez <roeierez@gmail.com>
2019-05-01 15:51:18 +0300

    increase payment max time to 2 minutes

--- lib/routes/user/home/account_page.dart
+++ lib/routes/user/home/account_page.dart
@@ -73,7 +73,7 @@ class AccountPageState extends State<AccountPage> {
             textAlign: TextAlign.left,
           ),
           backgroundColor: theme.snackBarBackgroundColor,
-          duration: new Duration(seconds: 80),
+          duration: new Duration(seconds: 120),
         );
         Scaffold.of(context).showSnackBar(_snackBar);
       }
