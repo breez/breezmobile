c2f776ab546ff6baa0e36400e5aa18c45229d629
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2019-05-24 05:33:31 +0300

    [Needs Rework] Change VendorRow

--- lib/routes/user/marketplace/vendor_row.dart
+++ lib/routes/user/marketplace/vendor_row.dart
@@ -11,25 +11,41 @@ class VendorRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color _vendorFgColor = Colors.transparent;
+    if (_vendor.name.toLowerCase() == "bitrefill") {
+      _vendorFgColor = theme.bitrefill.iconFgColor != null ? theme.bitrefill.iconFgColor : Colors.transparent;
+    } else if (_vendor.name.toLowerCase() == "ln.pizza") {
+      _vendorFgColor = theme.lnpizza.iconFgColor != null ? theme.lnpizza.iconFgColor : Colors.transparent;
+    }
+
+    Color _vendorBgColor = Colors.transparent;
+    if (_vendor.name.toLowerCase() == "bitrefill") {
+      _vendorBgColor = theme.bitrefill.iconBgColor != null ? theme.bitrefill.iconBgColor : Colors.transparent;
+    } else if (_vendor.name.toLowerCase() == "ln.pizza") {
+      _vendorBgColor = theme.lnpizza.iconBgColor != null ? theme.lnpizza.iconBgColor : Colors.transparent;
+    }
+
+    Color _vendorTextColor = Colors.black;
+    if (_vendor.name.toLowerCase() == "bitrefill") {
+      _vendorTextColor = theme.bitrefill.textColor != null ? theme.bitrefill.textColor : Colors.black;
+    } else if (_vendor.name.toLowerCase() == "ln.pizza") {
+      _vendorTextColor = theme.lnpizza.textColor != null ? theme.lnpizza.textColor : Colors.black;
+    }
+
     final _vendorLogo = _vendor.logo != null
         ? Image(
             image: AssetImage(_vendor.logo),
             height: 48.0,
-            width: 48.0,
+            color: _vendorFgColor,
           )
         : Container();
-
-    final _vendorBgColor = theme.bitrefill.iconBgColor != null
-        ? theme.bitrefill.iconBgColor
-        : Colors.transparent;
 
     final _vendorCard = new GestureDetector(
         onTap: () {
           Navigator.push(
               context,
               FadeInRoute(
-                builder: (_) =>
-                    new VendorWebViewPage(_vendor.url, _vendor.name),
+                builder: (_) => new VendorWebViewPage(_vendor.url, _vendor.name),
               ));
         },
         child: Container(
@@ -37,20 +53,34 @@ class VendorRow extends StatelessWidget {
           constraints: BoxConstraints.expand(),
           decoration: BoxDecoration(
               color: _vendorBgColor,
+              boxShadow: [
+                BoxShadow(
+                  color: theme.BreezColors.grey[600],
+                  blurRadius: 8.0,
+                )
+              ],
               border: Border.all(
-                  color: Colors.white, style: BorderStyle.solid, width: 1.0),
+                  color: _vendorBgColor == Colors.white ? theme.BreezColors.blue[200] : Colors.white, style: BorderStyle.solid, width: 1.0),
               borderRadius: BorderRadius.circular(14.0)),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              _vendorLogo,
-              Padding(padding: EdgeInsets.only(left: 4.0)),
-              Text(_vendor.name, style: theme.vendorTitleStyle),
-            ],
+            children: _buildLogo(_vendorLogo, _vendorTextColor),
           ),
         ));
 
     return _vendorCard;
+  }
+
+  List<Widget> _buildLogo(Widget _vendorLogo, _vendorTextColor) {
+    if (_vendor.onlyShowLogo) {
+      return <Widget>[_vendorLogo];
+    } else {
+      return <Widget>[
+        _vendorLogo,
+        Padding(padding: EdgeInsets.only(left: 4.0)),
+        Text(_vendor.name, style: theme.vendorTitleStyle.copyWith(color: _vendorTextColor)),
+      ];
+    }
   }
 }
