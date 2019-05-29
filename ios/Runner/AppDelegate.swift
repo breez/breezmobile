aefeb2fa65ed479b04ac7e94243d093c8adc29c9
Roei Erez <roeierez@gmail.com>
2019-05-29 14:44:51 +0300

    eliminate background fetch and use breez logger.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,15 +8,21 @@
 
 import UIKit
 import Flutter
+import Bindings
 
 @UIApplicationMain
 class AppDelegate : FlutterAppDelegate {
     
+    var logger : BindingsLoggerProtocol = EmptyLogger();
+    
     override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
         GeneratedPluginRegistrant.register(with: self);
         registerBreezPlugins();
-        application.setMinimumBackgroundFetchInterval(3600);
+        //application.setMinimumBackgroundFetchInterval(3600);
         Notifier.scheduleSyncRequiredNotification();
+        let workingDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        var error : NSError?;
+        logger = BindingsGetLogger(workingDir, &error)!;
         return super.application(application, didFinishLaunchingWithOptions: launchOptions);
     }
     
@@ -28,8 +34,10 @@ class AppDelegate : FlutterAppDelegate {
     override func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         let jobName = userInfo["_job"] as? String?;
         if (jobName == "chainSync") {
+            logger.log("chainSync notification received!", lvl: "INFO")
             ChainSync.run(app: application, completionHandler: {
                 Notifier.scheduleSyncRequiredNotification();
+                self.logger.log("Job completion handler was called!", lvl: "INFO");
                 completionHandler(UIBackgroundFetchResult.newData);
             });
             return;
@@ -39,10 +47,18 @@ class AppDelegate : FlutterAppDelegate {
     }
     
     override func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) -> Void {
-        print("background fetch started...");
-        ChainSync.run(app: application, completionHandler: {
-            Notifier.scheduleSyncRequiredNotification();
-            completionHandler(UIBackgroundFetchResult.newData);
-        });
+        logger.log("background fetch started...", lvl: "INFO");
+        completionHandler(UIBackgroundFetchResult.noData);
+        
+//        ChainSync.run(app: application, completionHandler: {
+//            Notifier.scheduleSyncRequiredNotification();
+//            completionHandler(UIBackgroundFetchResult.newData);
+//        });
+    }
+}
+
+class EmptyLogger : NSObject, BindingsLoggerProtocol {
+    func log(_ msg: String?, lvl: String?) {
+        print(msg);
     }
 }
