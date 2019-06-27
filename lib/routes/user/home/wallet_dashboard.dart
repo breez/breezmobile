2883639504965c82789217fed5cbfd54b05ae1b1
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2019-06-27 21:06:33 +0300

    Show balances fiat currency equivalent on long press to wallet dashboard

--- lib/routes/user/home/wallet_dashboard.dart
+++ lib/routes/user/home/wallet_dashboard.dart
@@ -1,15 +1,16 @@
 import 'package:breez/bloc/account/account_model.dart';
 import 'package:breez/bloc/user_profile/currency.dart';
+import 'package:breez/bloc/user_profile/fiat_currency.dart';
 import 'package:breez/widgets/fade_in_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:breez/theme_data.dart' as theme;
 import 'package:breez/routes/user/home/status_indicator.dart';
 import 'dart:math';
+import 'package:breez/services/breezlib/breez_bridge.dart';
+import 'package:breez/services/breezlib/data/rpc.pb.dart';
+import 'package:breez/services/injector.dart';
 
-class WalletDashboard extends StatelessWidget {
-  static const CHART_MAX_VERTICAL_OFFSET = 70.0;
-  static const CHART_MAX_HORIZONTAL_OFFSET = 30.0;
-  static const BALANCE_OFFSET_TRANSITION = 30.0;
+class WalletDashboard extends StatefulWidget {
   final AccountModel _accountModel;
   final AccountSettings _accSettings;
   final double _height;
@@ -19,57 +20,115 @@ class WalletDashboard extends StatelessWidget {
   WalletDashboard(this._accSettings, this._accountModel, this._height, this._offsetFactor, this._onCurrencyChange);
 
   @override
+  State<StatefulWidget> createState() {
+    return new WalletDashboardState();
+  }
+}
+
+class WalletDashboardState extends State<WalletDashboard> {
+  static const CHART_MAX_VERTICAL_OFFSET = 70.0;
+  static const CHART_MAX_HORIZONTAL_OFFSET = 30.0;
+  static const BALANCE_OFFSET_TRANSITION = 30.0;
+  bool _showFiatCurrency = false;
+  bool _isPressed = false;
+
+  BreezBridge _breezLib;
+  double _convertedBalance = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _breezLib = new ServiceInjector().breezBridge;
+  }
+
+  void _getExchangeRates() async {
+    Rates _rate = await _breezLib.rate();
+    // get conversion rate for _currency
+    _rate.rates.forEach((rate) {
+      if (rate.coin == widget._accountModel.fiatCurrency.shortName) {
+        setState(() {
+          _convertedBalance = widget._accountModel.balance.toDouble() / rate.value;
+        });
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     double startHeaderSize = theme.headline.fontSize;
     double endHeaderFontSize = theme.headline.fontSize - 8.0;
-    bool showProgressBar = (_accSettings?.showConnectProgress == true && !_accountModel.initial) || _accountModel?.isInitialBootstrap == true;
+    bool showProgressBar = (widget._accSettings?.showConnectProgress == true && !widget._accountModel.initial) ||
+        widget._accountModel?.isInitialBootstrap == true;
 
-    return Stack(
-      alignment: AlignmentDirectional.topCenter,
-      children: <Widget>[
-        Container(
-            width: MediaQuery.of(context).size.width,
-            height: _height,
-            decoration: BoxDecoration(color: Colors.white)
-        ),
-        Positioned(
-            width: MediaQuery.of(context).size.width ,
-            left: CHART_MAX_HORIZONTAL_OFFSET * _offsetFactor,
-            height: this._height + CHART_MAX_VERTICAL_OFFSET * _offsetFactor,
-            bottom: -CHART_MAX_VERTICAL_OFFSET * _offsetFactor,
-            child:Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  image: DecorationImage(
-                    image: AssetImage("src/images/chart_graph.png"),
-                    fit: BoxFit.fitWidth,
-                    alignment: FractionalOffset.bottomCenter,
-                  ))),
+    return Listener(
+        child: Stack(
+          alignment: AlignmentDirectional.topCenter,
+          children: <Widget>[
+            Container(width: MediaQuery.of(context).size.width, height: widget._height, decoration: BoxDecoration(color: Colors.white)),
+            Positioned(
+              width: MediaQuery.of(context).size.width,
+              left: CHART_MAX_HORIZONTAL_OFFSET * widget._offsetFactor,
+              height: this.widget._height + CHART_MAX_VERTICAL_OFFSET * widget._offsetFactor,
+              bottom: -CHART_MAX_VERTICAL_OFFSET * widget._offsetFactor,
+              child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      image: DecorationImage(
+                        image: AssetImage("src/images/chart_graph.png"),
+                        fit: BoxFit.fitWidth,
+                        alignment: FractionalOffset.bottomCenter,
+                      ))),
             ),
-        showProgressBar ? 
-          Positioned(top: 0.0, child: StatusIndicator(_accountModel)) : SizedBox(), 
-        Positioned(
-            top: 10.0,
-            child: Center(
-              child: _accountModel != null && !_accountModel.initial
-                  ? Text("Balance", style: theme.subtitle.copyWith(color: theme.subtitle.color.withOpacity( pow(1 - _offsetFactor, 8))))
-                  : SizedBox(),
-            )),
-        Positioned(
-            top: 30 - BALANCE_OFFSET_TRANSITION * _offsetFactor,
-            child: Center(
-              child: _accountModel != null && !_accountModel.initial
-                  ? FlatButton(
-                  onPressed: () {
-                    var nextCurrencyIndex = (Currency.currencies.indexOf(_accountModel.currency) + 1) % Currency.currencies.length;
-                    _onCurrencyChange(Currency.currencies[nextCurrencyIndex]);
-                  },
-                  child: Text(_accountModel.currency.format(_accountModel.balance, fixedDecimals: false),
-                      style: theme.headline.copyWith(fontSize: startHeaderSize - (startHeaderSize - endHeaderFontSize) * _offsetFactor)))
-                  : SizedBox(),
+            showProgressBar ? Positioned(top: 0.0, child: StatusIndicator(widget._accountModel)) : SizedBox(),
+            Positioned(
+                top: 10.0,
+                child: Center(
+                  child: widget._accountModel != null && !widget._accountModel.initial
+                      ? Text("Balance",
+                          style: theme.subtitle.copyWith(color: theme.subtitle.color.withOpacity(pow(1 - widget._offsetFactor, 8))))
+                      : SizedBox(),
+                )),
+            Positioned(
+              top: 30 - BALANCE_OFFSET_TRANSITION * widget._offsetFactor,
+              child: Center(
+                child: widget._accountModel != null && !widget._accountModel.initial
+                    ? FlatButton(
+                        onPressed: () {
+                          var nextCurrencyIndex =
+                              (Currency.currencies.indexOf(widget._accountModel.currency) + 1) % Currency.currencies.length;
+                          widget._onCurrencyChange(Currency.currencies[nextCurrencyIndex]);
+                        },
+                        child: _showFiatCurrency
+                            ? Text("${widget._accountModel.fiatCurrency.symbol}${_convertedBalance.toStringAsFixed(2)}",
+                                style: theme.headline
+                                    .copyWith(fontSize: startHeaderSize - (startHeaderSize - endHeaderFontSize) * widget._offsetFactor))
+                            : Text(widget._accountModel.currency.format(widget._accountModel.balance, fixedDecimals: false),
+                                style: theme.headline
+                                    .copyWith(fontSize: startHeaderSize - (startHeaderSize - endHeaderFontSize) * widget._offsetFactor)))
+                    : SizedBox(),
               ),
             )
-      ],
-    );
+          ],
+        ),
+        behavior: HitTestBehavior.translucent,
+        onPointerDown: (details) {
+          _getExchangeRates();
+          setState(() {
+            _isPressed = true;
+          });
+          Future.delayed(Duration(milliseconds: 600), () {
+            if (_isPressed) {
+              setState(() {
+                _showFiatCurrency = true;
+              });
+            }
+          });
+        },
+        onPointerUp: (details) {
+          setState(() {
+            _isPressed = false;
+            _showFiatCurrency = false;
+          });
+        });
   }
 }
