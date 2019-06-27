70de11803f6c5d6a4e89d3dbc881c53499f8a43d
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2019-06-27 21:01:11 +0300

    Created fiat currency

---
+++ lib/bloc/user_profile/fiat_currency.dart
@@ -0,0 +1,28 @@
+enum FiatCurrencyID { USD, EUR, GBP, JPY }
+
+class FiatCurrency extends Object {
+  final String symbol;
+  static const FiatCurrency USD = FiatCurrency._internal("\$");
+  static const FiatCurrency EUR = FiatCurrency._internal("€");
+  static const FiatCurrency GBP = FiatCurrency._internal("£");
+  static const FiatCurrency JPY = FiatCurrency._internal("¥");
+  static final List<FiatCurrency> currencies = List.unmodifiable([USD, EUR, GBP, JPY]);
+
+  const FiatCurrency._internal(this.symbol);
+
+  factory FiatCurrency.fromSymbol(String symbol) {
+    return currencies.firstWhere((c) => c.shortName == symbol);
+  }
+
+  String get shortName {
+    if (symbol == "€") {
+      return "EUR";
+    } else if (symbol == "£") {
+      return "GBP";
+    } else if (symbol == "¥") {
+      return "JPY";
+    } else {
+      return "USD";
+    }
+  }
+}
