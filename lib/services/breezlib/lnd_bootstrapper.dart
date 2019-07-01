4049eee06e84a714c5a1daa6a112cc7ccca77913
Roei Erez <roeierez@gmail.com>
2019-07-01 23:58:12 +0300

    trigger build

--- lib/services/breezlib/lnd_bootstrapper.dart
+++ lib/services/breezlib/lnd_bootstrapper.dart
@@ -8,6 +8,7 @@ import 'package:breez/logger.dart';
 import 'package:ini/ini.dart';
 int retruNum = 0;
 class LNDBootstrapper {
+  
   final StreamController<DownloadFileInfo> _bootstrapFilesProgress = new StreamController.broadcast();
   Stream<DownloadFileInfo> get bootstrapProgressStreams => _bootstrapFilesProgress.stream;    
 
