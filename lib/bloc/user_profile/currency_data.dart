b837fb3aa132c61c51446b7a94ca6598ce466ded
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2019-07-01 09:49:22 +0300

    Add shortName field to CurrencyData

--- lib/bloc/user_profile/currency_data.dart
+++ lib/bloc/user_profile/currency_data.dart
@@ -1,21 +1,24 @@
 import 'dart:convert';
 
 Map<String, CurrencyData> currencyDataFromJson(String str) =>
-    new Map.from(json.decode(str)).map((k, v) => new MapEntry<String, CurrencyData>(k, CurrencyData.fromJson(v)));
+    new Map.from(json.decode(str)).map((k, v) => new MapEntry<String, CurrencyData>(k, CurrencyData.fromJson(k, v)));
 
 class CurrencyData {
   String name;
+  String shortName;
   int fractionSize;
   Symbol symbol;
 
   CurrencyData({
     this.name,
+    this.shortName,
     this.fractionSize,
     this.symbol,
   });
 
-  factory CurrencyData.fromJson(Map<String, dynamic> json) => new CurrencyData(
+  factory CurrencyData.fromJson(String shortName, Map<String, dynamic> json) => new CurrencyData(
         name: json["name"],
+        shortName: shortName,
         fractionSize: json["fractionSize"],
         symbol: json["symbol"] == null ? null : Symbol.fromJson(json["symbol"]),
       );
