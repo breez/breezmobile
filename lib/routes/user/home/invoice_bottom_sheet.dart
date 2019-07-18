4c7de275e394d89c44bea6181edd0ea1cd87c52e
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2019-07-18 18:17:58 +0300

    Create InvoiceBottomSheet and add to home page

---
+++ lib/routes/user/home/invoice_bottom_sheet.dart
@@ -0,0 +1,88 @@
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:breez/bloc/invoice/invoice_bloc.dart';
+import 'package:breez/theme_data.dart' as theme;
+import 'package:flutter/material.dart';
+
+class InvoiceBottomSheet extends StatefulWidget {
+  final InvoiceBloc invoiceBloc;
+  final bool isSmallView;
+
+  InvoiceBottomSheet(this.invoiceBloc, this.isSmallView);
+
+  @override
+  State createState() => InvoiceBottomSheetState();
+}
+
+class InvoiceBottomSheetState extends State<InvoiceBottomSheet> with TickerProviderStateMixin {
+  bool isExpanded;
+
+  @override
+  void initState() {
+    super.initState();
+    isExpanded = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+        transform: isExpanded ? Matrix4.translationValues(0, 112.0, 0) : Matrix4.translationValues(0, 0, 0),
+        duration: Duration(milliseconds: 150),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              _buildInvoiceMenuItem(widget.isSmallView, "INVOICE", "src/icon/invoice.png", () {
+                setState(() {
+                  isExpanded = !isExpanded;
+                });
+              }, isFirst: true),
+              _buildInvoiceMenuItem(widget.isSmallView, "PAY", "src/icon/qr_scan.png", () async {
+                String decodedQr = await BarcodeScanner.scan();
+                widget.invoiceBloc.decodeInvoiceSink.add(decodedQr);
+              }),
+              _buildInvoiceMenuItem(
+                  widget.isSmallView, "CREATE", "src/icon/paste.png", () => Navigator.of(context).pushNamed('/create_invoice')),
+            ]));
+  }
+
+  Widget _buildInvoiceMenuItem(bool isSmallView, String title, String iconPath, Function function, {bool isFirst = false}) {
+    return AnimatedContainer(
+      width: isSmallView ? 56.0 : 126.0,
+      height: isFirst ? 50.0 : 56.0,
+      duration: Duration(milliseconds: 150),
+      child: RaisedButton(
+        onPressed: function,
+        color: isFirst ? Colors.white : Color.fromRGBO(0, 133, 251, 1.0),
+        padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 12.0),
+        shape: isFirst
+            ? RoundedRectangleBorder(borderRadius: new BorderRadius.only(topLeft: Radius.circular(20.0)))
+            : Border(top: BorderSide(color: Color.fromRGBO(255, 255, 255, 0.12), width: 1, style: BorderStyle.solid)),
+        child: Row(
+            mainAxisSize: MainAxisSize.max,
+            children: isSmallView
+                ? <Widget>[
+                    ImageIcon(
+                      AssetImage(iconPath),
+                      color: isFirst ? Color.fromRGBO(0, 133, 251, 1.0) : Colors.white,
+                      size: 24.0,
+                    )
+                  ]
+                : <Widget>[
+                    ImageIcon(
+                      AssetImage(iconPath),
+                      color: isFirst ? Color.fromRGBO(0, 133, 251, 1.0) : Colors.white,
+                      size: 24.0,
+                    ),
+                    Padding(padding: EdgeInsets.only(left: 8.0)),
+                    Text(
+                      title.toUpperCase(),
+                      style: theme.bottomSheetMenuItemStyle.copyWith(
+                        color: isFirst ? Color.fromRGBO(0, 133, 251, 1.0) : Colors.white,
+                      ),
+                    ),
+                  ]),
+      ),
+    );
+  }
+}
