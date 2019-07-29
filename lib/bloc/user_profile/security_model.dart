dca5e7a43aa93a34bed11a4dcc693dd7892414ba
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2019-07-29 15:55:11 +0300

    Add secureBackupWithPin property and fromJson/toJson functions to SecurityModel

--- lib/bloc/user_profile/security_model.dart
+++ lib/bloc/user_profile/security_model.dart
@@ -1,9 +1,18 @@
 class SecurityModel {
   final String pinCode;
+  final bool secureBackupWithPin;
 
-  SecurityModel({this.pinCode});
+  SecurityModel({this.pinCode, this.secureBackupWithPin});
 
-  SecurityModel copyWith({String pinCode}) {
-    return new SecurityModel(pinCode: pinCode ?? this.pinCode);
+  SecurityModel copyWith({String pinCode, bool secureBackupWithPin}) {
+    return new SecurityModel(pinCode: pinCode ?? this.pinCode, secureBackupWithPin: secureBackupWithPin ?? this.secureBackupWithPin);
   }
+
+  SecurityModel.fromJson(Map<String, dynamic> json)
+      : pinCode = null,
+        secureBackupWithPin = json['secureBackupWithPin'];
+
+  Map<String, dynamic> toJson() => {
+    'secureBackupWithPin': secureBackupWithPin,
+  };
 }
