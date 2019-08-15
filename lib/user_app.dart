4ca4bfacd117b73960094ed85f297a155ffca37d
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2019-08-15 18:03:14 +0300

    Remove textScaleFactor restriction

--- lib/user_app.dart
+++ lib/user_app.dart
@@ -55,14 +55,6 @@ class UserApp extends StatelessWidget {
           return MaterialApp(
             navigatorKey: _navigatorKey,
             title: 'Breez',
-            builder: (BuildContext context, Widget child) {
-              final MediaQueryData data = MediaQuery.of(context);
-              return MediaQuery(
-                  data: data.copyWith(
-                    textScaleFactor: (data.textScaleFactor >= 1.2) ? 1.2 : data.textScaleFactor,
-                  ),
-                  child: child);
-            },
             theme: ThemeData(
               backgroundColor: theme.BreezColors.blue[500],
               brightness: Brightness.dark,
