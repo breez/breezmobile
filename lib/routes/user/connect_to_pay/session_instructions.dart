1df357fb276825870dbbbfe1720e42dd1a3729d0
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2019-08-15 22:05:24 +0300

    Fix multiple scrollbar on c2p

--- lib/routes/user/connect_to_pay/session_instructions.dart
+++ lib/routes/user/connect_to_pay/session_instructions.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:breez/theme_data.dart' as theme;
+import 'package:flutter/material.dart';
 
 class SessionInstructions extends StatelessWidget {
   final Widget _child;
@@ -19,39 +19,38 @@ class SessionInstructions extends StatelessWidget {
       alignment: AlignmentDirectional.bottomCenter,
       children: <Widget>[
         Container(
-          height: 100.0,           
+          height: 100.0,
           color: theme.massageBackgroundColor,
           padding: new EdgeInsets.only(left: 16.0, top: 8.0, right: 16.0, bottom: hasActions ? 0.0 : 0.0),
-          child: Scrollbar(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: hasActions ? const EdgeInsets.only(bottom: 36.0) : const EdgeInsets.only(),
-                        child: Center(child: _child),
-                      ),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: hasActions ? const EdgeInsets.only(bottom: 36.0) : const EdgeInsets.only(),
+                      child: Center(child: _child),
                     ),
-                  ],
-                ),
-              ]),
-            ),
+                  ),
+                ],
+              ),
+            ]),
           ),
         ),
         Positioned(
-              bottom: 10.0,
-                child: Container(                  
-                  height: 36.0,                  
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: (_actions ?? []).map((action) {
-                      return FlatButton(child: Text(action.toUpperCase()), onPressed: _disabledActions.contains(action) ? null : () => _onAction(action));
-                    }).toList(),
-            ),
-                ))
+            bottom: 10.0,
+            child: Container(
+              height: 36.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: (_actions ?? []).map((action) {
+                  return FlatButton(
+                      child: Text(action.toUpperCase()), onPressed: _disabledActions.contains(action) ? null : () => _onAction(action));
+                }).toList(),
+              ),
+            ))
       ],
     );
   }
