c9eb8acf9f5d15c7be4fb84bdfe7b7a125c5b25f
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2019-08-21 17:30:32 +0300

    Remove line break in c2p instruction message

--- lib/routes/user/connect_to_pay/payer_session_widget.dart
+++ lib/routes/user/connect_to_pay/payer_session_widget.dart
@@ -74,7 +74,7 @@ class _PayerInstructions extends StatelessWidget {
       if (sessionState.payeeData.status.online) {
         message = '${sessionState.payeeData.userName} joined the session.\nPlease enter an amount and tap Pay to proceed.';
       } else if (!sessionState.invitationSent && sessionState.payeeData.userName == null) {
-        message = "Tap the Share button to share a link with a person that you want to pay.\nThen, please wait while this person clicks the link and joins the session.";
+        message = "Tap the Share button to share a link with a person that you want to pay. Then, please wait while this person clicks the link and joins the session.";
       } else {
         return LoadingAnimatedText('Waiting for ${sessionState.payeeData.userName ?? "someone"} to join this session', textStyle: theme.sessionNotificationStyle);
       }
