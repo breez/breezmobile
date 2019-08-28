172b0ac0f2d32094612a39820436f4f7d5d47369
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2019-08-28 09:35:30 +0300

    Do not display error messages on deposit to btc address page

--- lib/routes/user/add_funds/deposit_to_btc_address_page.dart
+++ lib/routes/user/add_funds/deposit_to_btc_address_page.dart
@@ -8,11 +8,8 @@ import 'package:breez/bloc/user_profile/breez_user_model.dart';
 import 'package:breez/routes/user/add_funds/address_widget.dart';
 import 'package:breez/theme_data.dart' as theme;
 import 'package:breez/widgets/back_button.dart' as backBtn;
-import 'package:breez/widgets/flushbar.dart';
-import 'package:breez/widgets/link_launcher.dart';
 import 'package:breez/widgets/single_button_bottom_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class DepositToBTCAddressPage extends StatefulWidget {
   final BreezUserModel _user;
@@ -88,54 +85,6 @@ class DepositToBTCAddressPageState extends State<DepositToBTCAddressPage> {
   }
 
   Widget getBody(BuildContext context, AccountModel account, AddFundResponse response, String error) {
-    var unconfirmedTxID = account?.swapFundsStatus?.unconfirmedTxID;
-    bool waitingDepositConfirmation = unconfirmedTxID?.isNotEmpty == true;
-
-    String errorMessage;
-    if (error != null) {
-      errorMessage = error;
-    } else if (account == null || account.bootstraping) {
-      errorMessage = 'You\'d be able to add funds after Breez is finished bootstrapping.';
-    } else if (unconfirmedTxID?.isNotEmpty == true || account.processingWithdrawal) {
-      errorMessage =
-          'Breez is processing your previous ${waitingDepositConfirmation || account.processingBreezConnection ? "deposit" : "withdrawal"}. You will be able to add more funds once this operation is completed.';
-    } else if (response != null && response.errorMessage.isNotEmpty) {
-      errorMessage = response.errorMessage;
-    }
-
-    if (errorMessage != null) {
-      if (!errorMessage.endsWith('.')) {
-        errorMessage += '.';
-      }
-      return Column(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: 50.0, left: 30.0, right: 30.0),
-            child: Text(errorMessage, textAlign: TextAlign.center),
-          ),
-          waitingDepositConfirmation
-              ? Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                  Padding(
-                    padding: EdgeInsets.only(top: 30.0, left: 30.0, right: 30.0),
-                    child: Text("Transaction ID:", textAlign: TextAlign.start),
-                  ),
-                  Padding(
-                      padding: EdgeInsets.only(top: 10.0, left: 30.0, right: 22.0),
-                      child: LinkLauncher(
-                        linkName: unconfirmedTxID,
-                        linkAddress: "https://blockstream.info/tx/$unconfirmedTxID",
-                        onCopy: () {
-                          Clipboard.setData(ClipboardData(text: unconfirmedTxID));
-                          showFlushbar(context, message: "Transaction ID was copied to your clipboard.", duration: Duration(seconds: 3));
-                        },
-                      ))
-                ])
-              : SizedBox()
-        ],
-      );
-    }
     return Column(children: <Widget>[
       AddressWidget(response?.address, response?.backupJson),
       response == null
