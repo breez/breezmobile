2c95697004ad429b7857223898348aa9ab1ec3c0
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2019-08-28 14:47:22 +0300

    Initialize moonpayUrl from add funds bloc

--- lib/bloc/account/add_funds_bloc.dart
+++ lib/bloc/account/add_funds_bloc.dart
@@ -3,38 +3,64 @@ import 'dart:async';
 import 'package:breez/bloc/account/account_model.dart';
 import 'package:breez/services/breezlib/breez_bridge.dart';
 import 'package:breez/services/injector.dart';
+import 'package:flutter/services.dart';
+import "package:ini/ini.dart";
 import 'package:rxdart/rxdart.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
 import 'moonpay_order.dart';
 
 class AddFundsBloc {
+  String _moonPayURL;
   final _addFundRequestController = new StreamController<void>();
+
   Sink<void> get addFundRequestSink => _addFundRequestController.sink;
 
   final _addFundResponseController = new StreamController<AddFundResponse>();
+
   Stream<AddFundResponse> get addFundResponseStream => _addFundResponseController.stream;
 
   final _moonPayOrderController = new StreamController<MoonpayOrder>();
+
   Sink<MoonpayOrder> get moonPayOrderSink => _moonPayOrderController.sink;
+
   Stream<MoonpayOrder> get moonPayOrderStream => _moonPayOrderController.stream;
 
   final _orderController = new BehaviorSubject<MoonpayOrder>();
+
   Sink<MoonpayOrder> get orderSink => _orderController.sink;
 
-  AddFundsBloc(String userID){
+  AddFundsBloc(String userID) {
     ServiceInjector injector = ServiceInjector();
     BreezBridge breezLib = injector.breezBridge;
-    _addFundRequestController.stream.listen((request){
+    _addFundRequestController.stream.listen((request) {
       _addFundResponseController.add(null);
-        breezLib.addFundsInit(userID)
+      breezLib
+          .addFundsInit(userID)
           .then((reply) => _addFundResponseController.add(new AddFundResponse(reply)))
           .catchError(_addFundResponseController.addError);
-      })
-      .onDone(_dispose);
-
+    }).onDone(_dispose);
+    _createMoonpayUrl();
     _initializeWithMoonpayOrder(injector);
     _listenToMoonpayOrder(injector);
   }
+
+  Future _createMoonpayUrl() async {
+    Config config = await _readConfig();
+    String baseUrl = config.get("MoonPay Parameters", 'baseUrl');
+    String apiKey = config.get("MoonPay Parameters", 'apiKey');
+    String currencyCode = config.get("MoonPay Parameters", 'currencyCode');
+    String colorCode = config.get("MoonPay Parameters", 'colorCode');
+    String redirectURL = config.get("MoonPay Parameters", 'redirectURL');
+    _moonPayURL = "$baseUrl?apiKey=$apiKey&currencyCode=$currencyCode&colorCode=$colorCode&redirectURL=${Uri.encodeFull(redirectURL)}";
+  }
+
+  Future<Config> _readConfig() async {
+    String lines = await rootBundle.loadString('conf/moonpay.conf');
+    return Config.fromString(lines);
+  }
+
+  String get moonPayURL => _moonPayURL;
 
   _initializeWithMoonpayOrder(ServiceInjector injector) {
     injector.sharedPreferences.then((preferences) {
