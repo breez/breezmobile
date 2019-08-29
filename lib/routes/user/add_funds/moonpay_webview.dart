ccdbf2529bc44f9e3407948c5541c3b398133350
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2019-08-29 15:46:50 +0300

    Hide webview when prompting pin

--- lib/routes/user/add_funds/moonpay_webview.dart
+++ lib/routes/user/add_funds/moonpay_webview.dart
@@ -7,8 +7,10 @@ import 'package:breez/bloc/account/account_model.dart';
 import 'package:breez/bloc/account/add_funds_bloc.dart';
 import 'package:breez/bloc/account/moonpay_order.dart';
 import 'package:breez/bloc/backup/backup_bloc.dart';
+import 'package:breez/bloc/blocs_provider.dart';
 import 'package:breez/bloc/user_profile/breez_user_model.dart';
 import 'package:breez/bloc/user_profile/currency.dart';
+import 'package:breez/bloc/user_profile/user_profile_bloc.dart';
 import 'package:breez/theme_data.dart' as theme;
 import 'package:breez/widgets/loader.dart';
 import 'package:flutter/material.dart';
@@ -34,6 +36,7 @@ class MoonpayWebViewState extends State<MoonpayWebView> {
   StreamSubscription<AccountModel> _accountSubscription;
   StreamSubscription<String> _urlSubscription;
   StreamSubscription<bool> _backupPromptVisibilitySubscription;
+  StreamSubscription<BreezUserModel> _userSubscription;
   AddFundsBloc _addFundsBloc;
 
   String walletAddress = "";
@@ -46,16 +49,6 @@ class MoonpayWebViewState extends State<MoonpayWebView> {
   void initState() {
     super.initState();
     _addFundsBloc = new AddFundsBloc(widget._user.userID);
-    _backupPromptVisibilitySubscription = widget._backupBloc.backupPromptVisibleStream.listen((isVisible) {
-      if (isVisible) {
-        onBeforeCallHandler();
-      } else {
-        _widgetWebview.show();
-        setState(() {
-          _screenshotData = null;
-        });
-      }
-    });
     _accountSubscription = widget._accountBloc.accountStream.listen((acc) {
       if (!acc.bootstraping) {
         _addFundsBloc.addFundRequestSink.add(null);
@@ -67,6 +60,9 @@ class MoonpayWebViewState extends State<MoonpayWebView> {
         _moonPayURL = moonPayURL;
         _urlSubscription.cancel();
       });
+    });
+    _backupPromptVisibilitySubscription = widget._backupBloc.backupPromptVisibleStream.listen((isVisible) {
+      _hideWebview(isVisible);
     });
     _widgetWebview.onDestroy.listen((_) {
       if (Navigator.canPop(context)) {
@@ -91,6 +87,10 @@ class MoonpayWebViewState extends State<MoonpayWebView> {
           }
         }
       });
+      UserProfileBloc userBloc = AppBlocsProvider.of<UserProfileBloc>(context);
+      _userSubscription = userBloc.userStream.listen((user) {
+        _hideWebview(user.locked);
+      });
       _isInit = true;
     }
     super.didChangeDependencies();
@@ -101,12 +101,24 @@ class MoonpayWebViewState extends State<MoonpayWebView> {
     _postMessageListener?.cancel();
     _accountSubscription.cancel();
     _urlSubscription.cancel();
+    _userSubscription.cancel();
     _backupPromptVisibilitySubscription.cancel();
     _widgetWebview.dispose();
     super.dispose();
   }
 
-  Future onBeforeCallHandler() {
+  _hideWebview(bool hide) {
+    if (hide) {
+      _moveWebviewToBackground();
+    } else {
+      _widgetWebview.show();
+      setState(() {
+        _screenshotData = null;
+      });
+    }
+  }
+
+  Future _moveWebviewToBackground() {
     if (_screenshotData != null) {
       return Future.value(null);
     }
@@ -140,22 +152,33 @@ class MoonpayWebViewState extends State<MoonpayWebView> {
         if (!response.hasData) {
           return Material(
             child: Scaffold(
-              appBar: AppBar(
-                actions: <Widget>[IconButton(icon: Icon(Icons.close), onPressed: () => Navigator.pop(context))],
-                automaticallyImplyLeading: false,
-                iconTheme: theme.appBarIconTheme,
-                textTheme: theme.appBarTextTheme,
-                backgroundColor: theme.BreezColors.blue[500],
-                title: Text(
-                  "MoonPay",
-                  style: theme.appBarTextStyle,
+                appBar: AppBar(
+                  actions: <Widget>[IconButton(icon: Icon(Icons.close), onPressed: () => Navigator.pop(context))],
+                  automaticallyImplyLeading: false,
+                  iconTheme: theme.appBarIconTheme,
+                  textTheme: theme.appBarTextTheme,
+                  backgroundColor: theme.BreezColors.blue[500],
+                  title: Text(
+                    "MoonPay",
+                    style: theme.appBarTextStyle,
+                  ),
+                  elevation: 0.0,
                 ),
-                elevation: 0.0,
-              ),
-              body: Center(
-                child: Loader(color: theme.BreezColors.white[400]),
-              ),
-            ),
+                body: response.hasError
+                    ? Column(
+                        mainAxisSize: MainAxisSize.max,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(top: 50.0, left: 30.0, right: 30.0),
+                            child: Text(
+                              "Failed to retrieve an address from Breez server\nPlease check your internet connection.",
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      )
+                    : Center(child: Loader(color: theme.BreezColors.white[400]))),
           );
         }
         if (response.data != null) {
