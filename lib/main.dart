884a1304c4307271e82d8cd97b647b7d79cbd523
Roei Erez <roeierez@gmail.com>
2019-09-05 21:31:51 +0300

    update dependencies.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'dart:io' show Platform;
 import 'package:flutter/cupertino.dart';
-import 'package:intl/date_symbol_data_local.dart';
+//import 'package:intl/date_symbol_data_local.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() { 
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  initializeDateFormatting(Platform.localeName, null);  
+  //initializeDateFormatting(Platform.localeName, null);  
   runApp(Container());
 }
