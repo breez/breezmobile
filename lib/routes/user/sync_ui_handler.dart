62184a1f081c727d1cc4f2eb6e2f05875532855a
Roei Erez <roeierez@gmail.com>
2019-09-08 11:17:10 +0300

    fix strings.

--- lib/routes/user/sync_ui_handler.dart
+++ lib/routes/user/sync_ui_handler.dart
@@ -47,7 +47,7 @@ ModalRoute _createSyncRoute(AccountBloc accBloc){
           var account = snapshot.data;
           double progress = account?.syncProgress ?? 0;
           return TransparentRouteLoader(
-            message: "Please wait while Breez is initializing", 
+            message: "Please wait while Breez is synchronizing", 
             value: progress, 
             opacity: 0.9,
             onClose: () => accBloc.userActionsSink.add(ChangeSyncUIState(SyncUIState.COLLAPSED)),
