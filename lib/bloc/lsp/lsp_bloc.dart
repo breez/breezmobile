59e87f24e1adbf46491dbc14482634e193ef6cff
Roei Erez <roeierez@gmail.com>
2019-10-15 16:56:49 +0300

    changes to lspBloc and lspModel to support connection status.

--- lib/bloc/lsp/lsp_bloc.dart
+++ lib/bloc/lsp/lsp_bloc.dart
@@ -2,16 +2,20 @@ import 'dart:async';
 
 import 'package:breez/bloc/lsp/lsp_model.dart';
 import 'package:breez/services/breezlib/breez_bridge.dart';
+import 'package:breez/services/breezlib/data/rpc.pb.dart';
 import 'package:breez/services/injector.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../async_actions_handler.dart';
 import 'lsp_actions.dart';
 
 class LSPBloc with AsyncActionsHandler {
-  final StreamController _lspsStreamController =
-      BehaviorSubject<List<LSPInfo>>();
-  Stream<List<LSPInfo>> get lspsStream => _lspsStreamController.stream;
+  static const LSP_DONT_PROMPT_PREFERENCES_KEY = "LSP_DONT_PROMPT_PREFERENCES_KEY";
+
+  final _lspsStatusController =
+      BehaviorSubject<LSPStatus>();
+  Stream<LSPStatus> get lspStatusStream => _lspsStatusController.stream;
 
   BreezBridge _breezLib;
 
@@ -25,6 +29,8 @@ class LSPBloc with AsyncActionsHandler {
     });
 
     listenActions();
+
+    ServiceInjector().sharedPreferences.then((sp) => _listenLSPStatus(sp));    
   }
 
   Future _fetchLSPList(FetchLSPList action) async {
@@ -34,20 +40,66 @@ class LSPBloc with AsyncActionsHandler {
       return LSPInfo(entry.value, entry.key);
     }).toList();
 
-    _lspsStreamController.add(lspInfoList);
+    _lspsStatusController.add(
+      _lspsStatusController.value.copyWith(availableLSPs: lspInfoList)
+    );
     action.resolve(lspInfoList);
   }
 
   Future _connectLSP(ConnectLSP action) async {
-    if (action.lnurl?.isNotEmpty == true) {
-      action.resolve(await _breezLib.connectToLnurl(action.lnurl));  
-    } else {
-      action.resolve(await _breezLib.connectToLSP(action.lspID));
+    _lspsStatusController.add(
+      _lspsStatusController.value.copyWith(
+        connectionStatus: LSPConnectionStatus.InProgress, lastConnectionError: null)
+    );
+
+    try {      
+      if (action.lnurl?.isNotEmpty == true) {
+        action.resolve(await _breezLib.connectToLnurl(action.lnurl));  
+      } else {
+        action.resolve(await _breezLib.connectToLSP(action.lspID));
+      }
+      _lspsStatusController.add(
+        _lspsStatusController.value.copyWith(
+          connectionStatus: LSPConnectionStatus.Active, lastConnectionError: null)
+      );
+    } catch (err) {
+      _lspsStatusController.add(
+        _lspsStatusController.value.copyWith(
+          connectionStatus: LSPConnectionStatus.NotSelected,
+          lastConnectionError: err.toString()
+        )
+      );
+      rethrow;
     }
+  }
+
+  void _listenLSPStatus(SharedPreferences sp){
+
+    var dontPrompt = sp.getBool(LSP_DONT_PROMPT_PREFERENCES_KEY);
+    _lspsStatusController.add(LSPStatus(null, null, null, dontPrompt ?? false));
+
+    List<Account_AccountStatus> lspSelectedStatuses = [
+      Account_AccountStatus.CONNECTED, Account_AccountStatus.PROCESSING_CONNECTION
+    ];
+
+    _breezLib.notificationStream.listen((_){
+      _breezLib.getAccount().then((acc){
+        bool lspSelected =  lspSelectedStatuses.contains(acc.status);
+        _lspsStatusController.add(
+          _lspsStatusController.value.copyWith(
+            connectionStatus: lspSelected ? LSPConnectionStatus.Active : LSPConnectionStatus.NotSelected,
+            dontPromptToConnect: _lspsStatusController.value.dontPromptToConnect || lspSelected)
+        );
+      });
+    });
+
+    _lspsStatusController.stream.listen((status){
+      sp.setBool(LSP_DONT_PROMPT_PREFERENCES_KEY, status.dontPromptToConnect);
+    });
   }
 
   void close() async {
     await dispose();
-    await _lspsStreamController.close();
+    await _lspsStatusController.close();
   }
 }
--- lib/bloc/lsp/lsp_model.dart
+++ lib/bloc/lsp/lsp_model.dart
@@ -1,5 +1,34 @@
 import 'package:breez/services/breezlib/data/rpc.pb.dart';
 
+enum LSPConnectionStatus {
+  InProgress,
+  Active,
+  NotSelected,
+}
+
+class LSPStatus {
+  final List<LSPInfo> availableLSPs;
+  final LSPConnectionStatus connectionStatus;
+  final String lastConnectionError;
+  final bool dontPromptToConnect;
+
+  LSPStatus(this.availableLSPs, this.connectionStatus, this.lastConnectionError,
+      this.dontPromptToConnect);  
+  LSPStatus copyWith(
+      {List<LSPInfo> availableLSPs,
+      LSPConnectionStatus connectionStatus,
+      String lastConnectionError,
+      bool dontPromptToConnect}) {
+    return LSPStatus(
+        availableLSPs ?? this.availableLSPs,
+        connectionStatus ?? this.connectionStatus,
+        lastConnectionError ?? this.lastConnectionError,
+        dontPromptToConnect ?? this.dontPromptToConnect);
+  }
+
+  bool get selectionRequired => connectionStatus == LSPConnectionStatus.NotSelected;
+}
+
 class LSPInfo {
   final String lspID;
   final LSPInformation _lspInformation;
