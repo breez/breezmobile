7652ba41cadc61a16c9dd316ca2a6fbd041ae391
Roei Erez <roeierez@gmail.com>
2019-10-15 21:58:45 +0300

    fix calendar dialog.

--- lib/widgets/calendar_dialog.dart
+++ lib/widgets/calendar_dialog.dart
@@ -1,9 +1,8 @@
 import 'dart:async';
+import 'dart:math';
 
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:breez/theme_data.dart' as theme;
 import 'package:breez/utils/date.dart';
-import 'package:breez/utils/min_font_size.dart';
 import 'package:breez/widgets/breez_date_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -20,15 +19,14 @@ class CalendarDialog extends StatefulWidget {
 
 class _CalendarDialogState extends State<CalendarDialog> {
   TextEditingController _startDateController = new TextEditingController();
-  TextEditingController _endDateController = new TextEditingController();  
+  TextEditingController _endDateController = new TextEditingController();
   DateTime _endDate = DateTime.now();
   DateTime _startDate;
-  AutoSizeGroup _autoSizeGroup = AutoSizeGroup();
 
   @override
   void initState() {
-    super.initState();   
-    _startDate = widget.firstDate;     
+    super.initState();
+    _startDate = widget.firstDate;
     _startDateController.text = DateUtils.formatYearMonthDay(widget.firstDate);
     _endDateController.text = DateUtils.formatYearMonthDay(_endDate);
   }
@@ -45,18 +43,29 @@ class _CalendarDialogState extends State<CalendarDialog> {
         style: Theme.of(context).dialogTheme.titleTextStyle,
       ),
       content: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          _selectDateButton("Start", _startDateController, true),
-          _selectDateButton("End", _endDateController, false),
+          Flexible(
+              //flex: 0,
+              child: _selectDateButton("Start", _startDateController, true)),
+          Flexible(
+              //flex: 0,
+              child: _selectDateButton("End", _endDateController, false)),
         ],
       ),
       actions: <Widget>[
         FlatButton(
-          child: Text("Clear", style: theme.cancelButtonStyle.copyWith(color: theme.themeId == "BLUE" ? Colors.red : Theme.of(context).errorColor)),
+          child: Text("Clear",
+              style: theme.cancelButtonStyle.copyWith(
+                  color: theme.themeId == "BLUE"
+                      ? Colors.red
+                      : Theme.of(context).errorColor)),
           onPressed: _clearFilter,
         ),
         FlatButton(
-          child: Text("Apply Filter", style: Theme.of(context).primaryTextTheme.button),
+          child: Text("Apply Filter",
+              style: Theme.of(context).primaryTextTheme.button),
           onPressed: () {
             _applyFilter();
           },
@@ -68,47 +77,40 @@ class _CalendarDialogState extends State<CalendarDialog> {
   void _applyFilter() {
     // Check if filter is unchanged
     if (_startDate != widget.firstDate || _endDate.day != DateTime.now().day) {
-      DateTime startDate = DateTime(_startDate.year, _startDate.month, _startDate.day, 0, 0, 0);
-      DateTime endDate = DateTime(_endDate.year, _endDate.month, _endDate.day, 23, 59, 59, 999);
+      DateTime startDate =
+          DateTime(_startDate.year, _startDate.month, _startDate.day, 0, 0, 0);
+      DateTime endDate = DateTime(
+          _endDate.year, _endDate.month, _endDate.day, 23, 59, 59, 999);
       Navigator.of(context).pop([startDate, endDate]);
     } else {
       Navigator.of(context).pop([null, null]);
     }
   }
 
-  Widget _selectDateButton(String label, TextEditingController textEditingController, bool isStartBtn) {
-    return Expanded(
-      child: GestureDetector(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Column(mainAxisSize: MainAxisSize.min, crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-              AutoSizeText(
-                label,
-                style: Theme.of(context).dialogTheme.contentTextStyle.copyWith(fontSize: 12, fontWeight: FontWeight.w300),
-                maxLines: 1,
-                minFontSize: MinFontSize(context).minFontSize,
-                stepGranularity: 0.1,
-              ),
-              AutoSizeText(
-                textEditingController.text,
-                style: Theme.of(context).dialogTheme.contentTextStyle,
-                maxLines: 1,
-                minFontSize: MinFontSize(context).minFontSize,
-                stepGranularity: 0.1,
-                group: _autoSizeGroup,
-              )
-            ]),
-          ],
+  Widget _selectDateButton(String label,
+      TextEditingController textEditingController, bool isStartBtn) {
+    return GestureDetector(
+      child: Theme(
+        data: theme.themeId == "BLUE"
+            ? Theme.of(context)
+            : Theme.of(context)
+                .copyWith(disabledColor: Theme.of(context).backgroundColor),
+        child: TextField(
+          decoration: InputDecoration(
+            labelText: label,
+            labelStyle: Theme.of(context).dialogTheme.contentTextStyle,
+          ),
+          controller: textEditingController,
+          enabled: false,
+          style: Theme.of(context).dialogTheme.contentTextStyle,
         ),
-        onTap: () {
-          setState(() {
-            _selectDate(context, isStartBtn);
-          });
-        },
-        behavior: HitTestBehavior.translucent,
       ),
+      onTap: () {
+        setState(() {
+          _selectDate(context, isStartBtn);
+        });
+      },
+      behavior: HitTestBehavior.translucent,
     );
   }
 
@@ -120,7 +122,9 @@ class _CalendarDialogState extends State<CalendarDialog> {
       lastDate: DateTime.now(),
     );
     if (selectedDate != null) {
-      Duration difference = isStartBtn ? selectedDate.difference(_endDate) : selectedDate.difference(_startDate);
+      Duration difference = isStartBtn
+          ? selectedDate.difference(_endDate)
+          : selectedDate.difference(_startDate);
       if (difference.inDays < 0) {
         setState(() {
           isStartBtn ? _startDate = selectedDate : _endDate = selectedDate;
