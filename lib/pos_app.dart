23013759fb3828c7258746dc5183124b31cd39ed
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2019-10-31 20:28:56 +0300

    Initialize AddFundsBloc

--- lib/pos_app.dart
+++ lib/pos_app.dart
@@ -16,6 +16,7 @@ import 'package:breez/routes/pos/settings/pos_settings_page.dart';
 import 'package:breez/routes/user/withdraw_funds/withdraw_funds_page.dart';
 import 'package:breez/routes/pos/transactions/pos_transactions_page.dart';
 import 'package:breez/theme_data.dart' as theme;
+import 'package:breez/bloc/account/add_funds_bloc.dart';
 
 class PosApp extends StatelessWidget {
   const PosApp({Key key}) : super(key: key);
@@ -34,53 +35,56 @@ class PosApp extends StatelessWidget {
           }
 
           BreezUserModel user = snapshot.data;
-          return MaterialApp(
-            title: 'Breez POS',
-            initialRoute: user.registered ? null : '/intro',
-            home: PosHome(accountBloc, backupBloc, userProfileBloc),
-            theme: theme.themeMap[user.themeId],
-            onGenerateRoute: (RouteSettings settings) {
-              switch (settings.name) {
-                case '/home':
-                  return new FadeInRoute(
-                    builder: (_) =>
-                        new PosHome(accountBloc, backupBloc, userProfileBloc),
-                    settings: settings,
-                  );
-                case '/intro':
-                  return new FadeInRoute(
-                    builder: (_) => new InitialWalkthroughPage(user,
-                        userProfileBloc, backupBloc, true),
-                    settings: settings,
-                  );
-                case '/transactions':
-                  return new FadeInRoute(
-                    builder: (_) => new PosTransactionsPage(),
-                    settings: settings,
-                  );
-                case '/withdraw_funds':
-                  return new FadeInRoute(
-                    builder: (_) => new WithdrawFundsPage(),
-                    settings: settings,
-                  );
-                case '/settings':
-                  return new FadeInRoute(
-                    builder: (_) => PosSettingsPage(),
-                    settings: settings,
-                  );
-                case '/network':
-                  return new FadeInRoute(
-                    builder: (_) => new NetworkPage(),
-                    settings: settings,
-                  );
-                case '/developers':
-                  return new FadeInRoute(
-                    builder: (_) => new DevView(),
-                    settings: settings,
-                  );
-              }
-              assert(false);
-            },
+          return  BlocProvider(
+            creator: () => new AddFundsBloc(userProfileBloc.userStream, accountBloc.accountStream),
+            builder: (ctx) => MaterialApp(
+              title: 'Breez POS',
+              initialRoute: user.registered ? null : '/intro',
+              home: PosHome(accountBloc, backupBloc, userProfileBloc),
+              theme: theme.themeMap[user.themeId],
+              onGenerateRoute: (RouteSettings settings) {
+                switch (settings.name) {
+                  case '/home':
+                    return new FadeInRoute(
+                      builder: (_) =>
+                          new PosHome(accountBloc, backupBloc, userProfileBloc),
+                      settings: settings,
+                    );
+                  case '/intro':
+                    return new FadeInRoute(
+                      builder: (_) => new InitialWalkthroughPage(user,
+                          userProfileBloc, backupBloc, true),
+                      settings: settings,
+                    );
+                  case '/transactions':
+                    return new FadeInRoute(
+                      builder: (_) => new PosTransactionsPage(),
+                      settings: settings,
+                    );
+                  case '/withdraw_funds':
+                    return new FadeInRoute(
+                      builder: (_) => new WithdrawFundsPage(),
+                      settings: settings,
+                    );
+                  case '/settings':
+                    return new FadeInRoute(
+                      builder: (_) => PosSettingsPage(),
+                      settings: settings,
+                    );
+                  case '/network':
+                    return new FadeInRoute(
+                      builder: (_) => new NetworkPage(),
+                      settings: settings,
+                    );
+                  case '/developers':
+                    return new FadeInRoute(
+                      builder: (_) => new DevView(),
+                      settings: settings,
+                    );
+                }
+                assert(false);
+              },
+            ),
           );
         });
   }
