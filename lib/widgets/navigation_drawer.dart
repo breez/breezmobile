bd3b25fa17eb5766f00fb9b756f0ea0e7409deaa
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2019-11-04 12:25:11 +0300

    Reduce duplicate code

--- lib/widgets/navigation_drawer.dart
+++ lib/widgets/navigation_drawer.dart
@@ -32,10 +32,9 @@ class DrawerItemConfigGroup {
 class NavigationDrawer extends StatelessWidget {
   final bool _avatar;
   final List<DrawerItemConfigGroup> _drawerGroupedItems;
-  final void Function(String screenName) _onItemSelected;  
+  final void Function(String screenName) _onItemSelected;
 
-  NavigationDrawer(this._avatar, this._drawerGroupedItems,
-      this._onItemSelected);
+  NavigationDrawer(this._avatar, this._drawerGroupedItems, this._onItemSelected);
 
   final ScrollController _scrollController = ScrollController();
 
@@ -45,8 +44,7 @@ class NavigationDrawer extends StatelessWidget {
 
     List<Widget> children = List<Widget>();
     _drawerGroupedItems.forEach((gropuItems) {
-      children.addAll(_createDrawerGroupWidgets(gropuItems, context,
-          withDivider: children.length > 0));
+      children.addAll(_createDrawerGroupWidgets(gropuItems, context, withDivider: children.length > 0));
     });
 
     children.insert(0, _breezDrawerHeader(userProfileBloc, _avatar));
@@ -59,19 +57,12 @@ class NavigationDrawer extends StatelessWidget {
             children: children));
   }
 
-  List<Widget> _createDrawerGroupWidgets(
-      DrawerItemConfigGroup group, BuildContext context,
-      {bool withDivider = false}) {
-    List<Widget> groupItems = group.items
-        .map((action) => _actionTile(action, context, action.onItemSelected ?? _onItemSelected))
-        .toList();
+  List<Widget> _createDrawerGroupWidgets(DrawerItemConfigGroup group, BuildContext context, {bool withDivider = false}) {
+    List<Widget> groupItems = group.items.map((action) => _actionTile(action, context, action.onItemSelected ?? _onItemSelected)).toList();
     if (group.groupTitle != null && groupItems.length > 0) {
       groupItems = List<Widget>()
         ..add(_ExpansionTile(
-            items: groupItems,
-            title: group.groupTitle,
-            icon: AssetImage(group.groupAssetImage),
-            controller: _scrollController));
+            items: groupItems, title: group.groupTitle, icon: AssetImage(group.groupAssetImage), controller: _scrollController));
     }
 
     if (groupItems.length > 0 && withDivider) {
@@ -84,181 +75,148 @@ class NavigationDrawer extends StatelessWidget {
 class _ListDivider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.only(left: 8.0, right: 8.0), child: Divider());
+    return Padding(padding: EdgeInsets.only(left: 8.0, right: 8.0), child: Divider());
   }
 }
 
 Widget _breezDrawerHeader(UserProfileBloc user, bool drawAvatar) {
-  return new BreezDrawerHeader(
+  return BreezDrawerHeader(
     padding: EdgeInsets.only(left: 16.0),
-    child: !drawAvatar
-        ? new StreamBuilder<BreezUserModel>(
-            stream: user.userStream,
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return new Container();
-              } else {
-                return new Column(children: <Widget>[
-                  GestureDetector(
-                    onTap: () => _changeTheme(snapshot.data.themeId == "BLUE" ? "DARK" : "BLUE", user, context),
-                    child: new Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        new Padding(
-                          padding: EdgeInsets.only(
-                            top: 10,
-                            right: 16.0,
-                          ),
-                          child: Container(
-                            width: 64,
-                            padding: EdgeInsets.all(4),
-                            decoration: ShapeDecoration(shape: StadiumBorder(), color: theme.marketplaceButtonColor),
-                            child: Row(
-                              children: <Widget>[
-                                Image.asset(
-                                  "src/icon/ic_lightmode.png",
-                                  height: 24,
-                                  width: 24,
-                                  color: snapshot.data.themeId == "BLUE" ? Colors.white : Colors.white30,
-                                ),
-                                Container(
-                                  height: 20,
-                                  width: 8,
-                                  child: VerticalDivider(
-                                    color: Colors.white30,
-                                  ),
-                                ),
-                                ImageIcon(AssetImage("src/icon/ic_darkmode.png"),
-                                    color: snapshot.data.themeId == "DARK" ? Colors.white : Colors.white30, size: 24.0),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ]);
-              }
-            })
-        : new StreamBuilder<BreezUserModel>(
-            stream: user.userStream,
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return new Container();
-              } else {
-                return new GestureDetector(
-                  onTap: () {
-                    showDialog<bool>(
-                      context: context,
-                      barrierDismissible: false,
-                      builder: (context) => breezAvatarDialog(context, user),
-                    );
-                  },
-                  child: new Column(children: <Widget>[
-                    GestureDetector(
-                      onTap: () => _changeTheme(snapshot.data.themeId == "BLUE" ? "DARK" : "BLUE", user, context),
-                      child: new Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          new Padding(
-                            padding: EdgeInsets.only(
-                              top: 10,
-                              right: 16.0,
-                            ),
-                            child: Container(
-                              width: 64,
-                              padding: EdgeInsets.all(4),
-                              decoration: ShapeDecoration(shape: StadiumBorder(), color: theme.marketplaceButtonColor),
-                              child: Row(
-                                children: <Widget>[
-                                  Image.asset(
-                                    "src/icon/ic_lightmode.png",
-                                    height: 24,
-                                    width: 24,
-                                    color: snapshot.data.themeId == "BLUE"
-                                        ? Colors.white
-                                        : Colors.white30,
-                                  ),
-                                  Container(
-                                    height: 20,
-                                    width: 8,
-                                    child: VerticalDivider(
-                                      color: Colors.white30,
-                                    ),
-                                  ),
-                                  ImageIcon(AssetImage("src/icon/ic_darkmode.png"),
-                                      color: snapshot.data.themeId == "DARK"
-                                          ? Colors.white
-                                          : Colors.white30,
-                                      size: 24.0),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    new Row(
-                      children: <Widget>[
-                        BreezAvatar(snapshot.data.avatarURL, radius: 24.0),
-                      ],
-                    ),
-                    new Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        new Padding(
-                          padding: EdgeInsets.only(top: 8.0),
-                          child: AutoSizeText(
-                            snapshot.data.name ?? "No Name",
-                            style: theme.navigationDrawerHandleStyle,
-                          ),
-                        ),
-                        new Spacer(),
-                        new Padding(
-                          padding: EdgeInsets.only(
-                            top: 4.0,
-                          ),
-                          child: new RawMaterialButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                              Navigator.of(context).pushNamed("/marketplace");
-                            },
-                            child: ImageIcon(AssetImage("src/icon/ic_market.png"), color: Colors.white, size: 24.0),
-                            padding: const EdgeInsets.all(12.0),
-                            fillColor: theme.marketplaceButtonColor,
-                            shape: new CircleBorder(),
-                            elevation: 0.0,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ]),
-                );
-              }
-            }),
-    decoration: new BoxDecoration(
+    child: _buildDrawerHeaderContent(user, drawAvatar),
+    decoration: BoxDecoration(
       image: DecorationImage(image: AssetImage("src/images/waves-drawer.png"), fit: BoxFit.scaleDown, alignment: Alignment(0, 0)),
     ),
   );
 }
 
-Future _changeTheme(String themeId, UserProfileBloc userProfileBloc, BuildContext context) async {
-    var action = ChangeTheme(themeId);
-    userProfileBloc.userActionsSink.add(action);
-    action.future.then((_) {
-    }).catchError((err){
-      promptError(context, "Internal Error", Text(err.toString(), style: Theme.of(context).dialogTheme.contentTextStyle,));
-    });
+StreamBuilder<BreezUserModel> _buildDrawerHeaderContent(UserProfileBloc user, bool drawAvatar) {
+  return StreamBuilder<BreezUserModel>(
+      stream: user.userStream,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Container();
+        } else {
+          List<Widget> drawerHeaderContent = new List<Widget>();
+          drawerHeaderContent.add(_buildThemeSwitch(snapshot, user, context));
+          if (drawAvatar) {
+            drawerHeaderContent..add(_buildAvatarButton(snapshot))..add(_buildBottomRow(snapshot, context));
+          }
+          return GestureDetector(
+            onTap: drawAvatar
+                ? () {
+                    showDialog<bool>(
+                      context: context,
+                      barrierDismissible: false,
+                      builder: (context) => breezAvatarDialog(context, user),
+                    );
+                  }
+                : null,
+            child: Column(children: drawerHeaderContent),
+          );
+        }
+      });
 }
 
-Widget _actionTile(
-    DrawerItemConfig action, BuildContext context, Function onItemSelected,
-    [bool subTile]) {
+GestureDetector _buildThemeSwitch(AsyncSnapshot<BreezUserModel> snapshot, UserProfileBloc user, BuildContext context) {
+  return GestureDetector(
+    onTap: () => _changeTheme(snapshot.data.themeId == "BLUE" ? "DARK" : "BLUE", user, context),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(
+            top: 10,
+            right: 16.0,
+          ),
+          child: Container(
+            width: 64,
+            padding: EdgeInsets.all(4),
+            decoration: ShapeDecoration(shape: StadiumBorder(), color: theme.marketplaceButtonColor),
+            child: Row(
+              children: <Widget>[
+                Image.asset(
+                  "src/icon/ic_lightmode.png",
+                  height: 24,
+                  width: 24,
+                  color: snapshot.data.themeId == "BLUE" ? Colors.white : Colors.white30,
+                ),
+                Container(
+                  height: 20,
+                  width: 8,
+                  child: VerticalDivider(
+                    color: Colors.white30,
+                  ),
+                ),
+                ImageIcon(AssetImage("src/icon/ic_darkmode.png"),
+                    color: snapshot.data.themeId == "DARK" ? Colors.white : Colors.white30, size: 24.0),
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Future _changeTheme(String themeId, UserProfileBloc userProfileBloc, BuildContext context) async {
+  var action = ChangeTheme(themeId);
+  userProfileBloc.userActionsSink.add(action);
+  action.future.then((_) {}).catchError((err) {
+    promptError(context, "Internal Error", Text(err.toString(), style: Theme.of(context).dialogTheme.contentTextStyle,));
+  });
+}
+
+Row _buildAvatarButton(AsyncSnapshot<BreezUserModel> snapshot) {
+  return Row(
+    children: <Widget>[
+      BreezAvatar(snapshot.data.avatarURL, radius: 24.0),
+    ],
+  );
+}
+
+Row _buildBottomRow(AsyncSnapshot<BreezUserModel> snapshot, BuildContext context) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: <Widget>[
+      _buildUsername(snapshot),
+      Spacer(),
+      buildMarketplaceButton(context),
+    ],
+  );
+}
+
+Padding _buildUsername(AsyncSnapshot<BreezUserModel> snapshot) {
+  return Padding(
+    padding: EdgeInsets.only(top: 8.0),
+    child: AutoSizeText(
+      snapshot.data.name ?? "No Name",
+      style: theme.navigationDrawerHandleStyle,
+    ),
+  );
+}
+
+Padding buildMarketplaceButton(BuildContext context) {
+  return Padding(
+    padding: EdgeInsets.only(
+      top: 4.0,
+    ),
+    child: RawMaterialButton(
+      onPressed: () {
+        Navigator.pop(context);
+        Navigator.of(context).pushNamed("/marketplace");
+      },
+      child: ImageIcon(AssetImage("src/icon/ic_market.png"), color: Colors.white, size: 24.0),
+      padding: const EdgeInsets.all(12.0),
+      fillColor: theme.marketplaceButtonColor,
+      shape: CircleBorder(),
+      elevation: 0.0,
+    ),
+  );
+}
+
+Widget _actionTile(DrawerItemConfig action, BuildContext context, Function onItemSelected, [bool subTile]) {
   return new Padding(
-    padding: subTile != null
-        ? EdgeInsets.only(left: 36.0, right: 8.0)
-        : EdgeInsets.only(left: 8.0, right: 8.0),
+    padding: subTile != null ? EdgeInsets.only(left: 36.0, right: 8.0) : EdgeInsets.only(left: 8.0, right: 8.0),
     child: new ListTile(
       leading: ImageIcon(
         AssetImage(action.icon),
@@ -280,13 +238,11 @@ class _ExpansionTile extends StatelessWidget {
   final AssetImage icon;
   final ScrollController controller;
 
-  const _ExpansionTile({Key key, this.items, this.title, this.icon, this.controller})
-      : super(key: key);
+  const _ExpansionTile({Key key, this.items, this.title, this.icon, this.controller}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final _expansionTileTheme =
-        Theme.of(context).copyWith(dividerColor: Theme.of(context).canvasColor);
+    final _expansionTileTheme = Theme.of(context).copyWith(dividerColor: Theme.of(context).canvasColor);
     return Theme(
         data: _expansionTileTheme,
         child: ExpansionTile(
@@ -307,9 +263,11 @@ class _ExpansionTile extends StatelessWidget {
           ),
           children: items.map((item) => Padding(padding: EdgeInsets.only(left: 28.0), child: item)).toList(),
           onExpansionChanged: (isExpanded) {
-            if (isExpanded) Timer(Duration(milliseconds: 200), () =>
-                controller.animateTo(
-                    controller.position.maxScrollExtent + 28.0, duration: Duration(milliseconds: 400), curve: Curves.ease));
+            if (isExpanded)
+              Timer(
+                  Duration(milliseconds: 200),
+                  () => controller.animateTo(controller.position.maxScrollExtent + 28.0,
+                      duration: Duration(milliseconds: 400), curve: Curves.ease));
             // 28 = bottom padding of list + intrinsic bottom padding
           },
         ));
