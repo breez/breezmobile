3738fecc1c358793412d7bd197bb9ec73a2a39a2
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2019-11-21 02:42:25 +0300

    Add enrolledBiometricIds to SecurityModel

--- lib/bloc/user_profile/security_model.dart
+++ lib/bloc/user_profile/security_model.dart
@@ -5,30 +5,34 @@ class SecurityModel {
   final bool requiresPin;
   final bool isFingerprintEnabled;
   final String enrolledBiometrics;
+  final String enrolledBiometricIds;
   final int automaticallyLockInterval;
 
-  SecurityModel._({this.requiresPin, this.isFingerprintEnabled, this.enrolledBiometrics, this.automaticallyLockInterval});
+  SecurityModel._({this.requiresPin, this.isFingerprintEnabled, this.enrolledBiometrics, this.enrolledBiometricIds, this.automaticallyLockInterval});
 
-  SecurityModel copyWith({bool requiresPin, bool isFingerprintEnabled, String enrolledBiometrics, int automaticallyLockInterval}) {
+  SecurityModel copyWith({bool requiresPin, bool isFingerprintEnabled, String enrolledBiometrics, String enrolledBiometricIds, int automaticallyLockInterval}) {
     return new SecurityModel._(
         requiresPin: requiresPin ?? this.requiresPin,
         isFingerprintEnabled: isFingerprintEnabled ?? this.isFingerprintEnabled,
         enrolledBiometrics: enrolledBiometrics ?? this.enrolledBiometrics,
+        enrolledBiometricIds: enrolledBiometricIds ?? this.enrolledBiometricIds,
         automaticallyLockInterval: automaticallyLockInterval ?? this.automaticallyLockInterval);
   }
 
-  SecurityModel.initial() : this._(requiresPin: false, isFingerprintEnabled: false, enrolledBiometrics: "", automaticallyLockInterval: _defaultLockInterval);
+  SecurityModel.initial() : this._(requiresPin: false, isFingerprintEnabled: false, enrolledBiometrics: "", enrolledBiometricIds: "", automaticallyLockInterval: _defaultLockInterval);
 
   SecurityModel.fromJson(Map<String, dynamic> json)
       : requiresPin = json['requiresPin'] ?? false,
         isFingerprintEnabled = json['isFingerprintEnabled'] ?? false,
         enrolledBiometrics = json['enrolledBiometrics'] ?? "",
+        enrolledBiometricIds = json['enrolledBiometricIds'] ?? "",
         automaticallyLockInterval = json['automaticallyLockInterval'] ?? _defaultLockInterval;
 
   Map<String, dynamic> toJson() => {
         'requiresPin': requiresPin,
         'isFingerprintEnabled': isFingerprintEnabled,
         'enrolledBiometrics': enrolledBiometrics,
+        'enrolledBiometricIds': enrolledBiometricIds,
         'automaticallyLockInterval': automaticallyLockInterval,
       };
 }
