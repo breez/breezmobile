b49384f1a28be58177975f071b4d00a58c6e26f6
Roei Erez <roeierez@gmail.com>
2019-11-28 16:08:51 +0200

    polish withdraw dialog.

--- lib/routes/user/create_invoice/lnurl_withdraw_dialog.dart
+++ lib/routes/user/create_invoice/lnurl_withdraw_dialog.dart
@@ -1,16 +1,16 @@
 import 'package:breez/bloc/invoice/invoice_bloc.dart';
 import 'package:breez/bloc/lnurl/lnurl_actions.dart';
 import 'package:breez/bloc/lnurl/lnurl_bloc.dart';
+import 'package:breez/widgets/loading_animated_text.dart';
 import 'package:flutter/material.dart';
 import 'package:breez/theme_data.dart' as theme;
 
 class LNURlWidthrawDialog extends StatefulWidget {
-  final String lnurl;
   final InvoiceBloc invoiceBloc;
   final LNUrlBloc lnurlBloc;
 
-  const LNURlWidthrawDialog(this.lnurl, this.invoiceBloc, this.lnurlBloc);
-  
+  const LNURlWidthrawDialog(this.invoiceBloc, this.lnurlBloc);
+
   @override
   State<StatefulWidget> createState() {
     return LNUrlWithdrawDialogState();
@@ -24,7 +24,7 @@ class LNUrlWithdrawDialogState extends State<LNURlWidthrawDialog> {
   void initState() {
     super.initState();
     widget.invoiceBloc.readyInvoicesStream.first.then((bolt11) {
-      Withdraw withdrawAction = Withdraw(widget.lnurl, bolt11);
+      Withdraw withdrawAction = Withdraw(bolt11);
       widget.lnurlBloc.actionsSink.add(withdrawAction);
       return withdrawAction.future;
     }).catchError((err) {
@@ -51,31 +51,49 @@ class LNUrlWithdrawDialogState extends State<LNURlWidthrawDialog> {
           }));
     }
     return AlertDialog(
-      title: Text(
-        "Withdraw Funds",
-        style: Theme.of(context).dialogTheme.titleTextStyle,
-        textAlign: TextAlign.center,
-      ),
-      content: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
+      titlePadding: EdgeInsets.zero,
+      contentPadding: EdgeInsets.fromLTRB(24.0, 20.0, 24.0, _error != null ? 24.0 : 0.0),
+      title: Row(
         children: <Widget>[
-          new Text(
-            _error ?? "Please wait while Breez is withdrawing your funds.",
-            style: Theme.of(context).dialogTheme.contentTextStyle,
-            textAlign: TextAlign.center,
-          ),
-          _error != null
-              ? SizedBox()
-              : Padding(
-                  padding: EdgeInsets.only(top: 8.0),
-                  child: new Image.asset(
-                    theme.customData[theme.themeId].loaderAssetPath,
-                    gaplessPlayback: true,
-                  ))
+          
+          Container(width: 50.0),
+          Expanded(flex: 1, child: Text("Receive Funds",
+                    style: Theme.of(context).dialogTheme.titleTextStyle,
+                    textAlign: TextAlign.center)),
+          Container(
+            width: 50.0,
+            child: IconButton(    
+              iconSize: 24.0,          
+                onPressed: () => Navigator.pop(context),
+                icon: Icon(Icons.close,
+                    color: Theme.of(context).dialogTheme.titleTextStyle.color)
+            ),
+          )
         ],
       ),
-      actions: actions,
+      content: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,            
+              children: <Widget>[                
+                _error != null
+                    ? new Text("Failed to receive funds: $_error",
+                        style: Theme.of(context).dialogTheme.contentTextStyle,
+                        textAlign: TextAlign.center)
+                    : LoadingAnimatedText(
+                        'Please wait while your payment is being processed',
+                        textStyle: Theme.of(context).dialogTheme.contentTextStyle,
+                        textAlign: TextAlign.center,
+                      ),
+                _error != null
+                    ? SizedBox()
+                    : Padding(
+                        padding: EdgeInsets.only(top: 8.0),
+                        child: new Image.asset(
+                          theme.customData[theme.themeId].loaderAssetPath,
+                          gaplessPlayback: true,
+                        ))
+              ],
+            ),      
     );
   }
 }
