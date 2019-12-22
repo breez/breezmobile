65566cb93917ac5ece0129458a1d8754dcc0d7fe
Roei Erez <roeierez@gmail.com>
2019-12-22 12:14:59 +0200

    add package info to payment failure report.

--- lib/services/device.dart
+++ lib/services/device.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:package_info/package_info.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:share_extend/share_extend.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -63,5 +64,10 @@ class Device {
         }
       }
     });
+  }
+
+  Future<String> appVersion() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    return "${packageInfo.version}.${packageInfo.buildNumber}";
   }
 }
