b94765693e1d30c1122a414acd2fc15bfb1bbc9f
Roei Erez <roeierez@gmail.com>
2020-02-03 19:36:21 +0200

    add some customizations to the flushbar.

--- lib/widgets/flushbar.dart
+++ lib/widgets/flushbar.dart
@@ -6,19 +6,26 @@ Flushbar showFlushbar(BuildContext context,
     {String title = "",
     String message = "",
     Widget messageWidget,
+    String buttonText = "OK",
+    FlushbarPosition position = FlushbarPosition.BOTTOM,
+    bool Function() onDismiss,
     Duration duration = const Duration(seconds: 8)}) {
   Flushbar flush;
   flush = Flushbar(
+      flushbarPosition: position,
       titleText: Text(title, style: TextStyle(height: 0.0)),
-      duration: duration,
+      duration: duration == Duration.zero ? null : duration,
       messageText: messageWidget ??
           Text(message, style: theme.snackBarStyle, textAlign: TextAlign.left),
-      backgroundColor: theme.snackBarBackgroundColor,
+      backgroundColor: theme.snackBarBackgroundColor,      
       mainButton: FlatButton(
-        onPressed: () {
-          flush.dismiss(true); // result = true
+        onPressed: () {          
+          bool dismiss = onDismiss != null ? onDismiss() : true;          
+          if (dismiss) {
+            flush.dismiss(true);
+          }
         },
-        child: Text("OK", style: theme.validatorStyle),
+        child: Text(buttonText, style: theme.validatorStyle),
       ))
     ..show(context);
 
