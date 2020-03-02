de2b8562248a53339eccd4adf55cb117866e9819
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2020-03-02 19:05:14 +0300

    Create ItemsList

---
+++ lib/routes/charge/items/items_list.dart
@@ -0,0 +1,22 @@
+import 'package:breez/bloc/pos_catalog/model.dart';
+import 'package:flutter/material.dart';
+
+import 'catalog_item.dart';
+
+class ItemsList extends StatelessWidget {
+  final List<Item> _items;
+
+  ItemsList(
+    this._items,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverFixedExtentList(
+      itemExtent: 72.0,
+      delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+        return CatalogItem(_items[index], _items.length - 1 == index);
+      }, childCount: _items.length),
+    );
+  }
+}
