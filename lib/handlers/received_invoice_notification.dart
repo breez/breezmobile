f7ac5585aa576db3c9f0ab85c5efc1a3f0e70455
Roei Erez <roeierez@gmail.com>
2020-03-04 12:42:14 +0200

    protect actions and route with admin password.

--- lib/handlers/received_invoice_notification.dart
+++ lib/handlers/received_invoice_notification.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:breez/bloc/account/account_bloc.dart';
 import 'package:breez/bloc/invoice/invoice_model.dart';
+import 'package:breez/bloc/user_profile/user_profile_bloc.dart';
+import 'package:breez/routes/admin_login_dialog.dart';
 import 'package:breez/widgets/flushbar.dart';
 import 'package:breez/widgets/loader.dart';
 import 'package:breez/widgets/payment_request_dialog.dart' as paymentRequest;
@@ -9,6 +11,7 @@ import 'package:flutter/material.dart';
 
 class InvoiceNotificationsHandler {
   final BuildContext _context;
+  final UserProfileBloc _userProfileBloc;
   final AccountBloc _accountBloc;
   final Stream<PaymentRequestModel> _receivedInvoicesStream;
   final GlobalKey firstPaymentItemKey;
@@ -20,6 +23,7 @@ class InvoiceNotificationsHandler {
 
   InvoiceNotificationsHandler(
       this._context,
+      this._userProfileBloc,
       this._accountBloc,
       this._receivedInvoicesStream,
       this.firstPaymentItemKey,
@@ -42,12 +46,13 @@ class InvoiceNotificationsHandler {
       // show payment request dialog for decoded requests
       _receivedInvoicesStream
           .where((payreq) => payreq != null && !_handlingRequest)
-          .listen((payreq) {
+          .listen((payreq) async {
         if (!payreq.loaded) {
           _setLoading(true);
           return;
         }
-
+        var user =
+            await _userProfileBloc.userStream.firstWhere((u) => u != null);
         _setLoading(false);
         _handlingRequest = true;
 
@@ -55,12 +60,15 @@ class InvoiceNotificationsHandler {
         if (scaffoldController.currentState.isDrawerOpen) {
           Navigator.pop(_context);
         }
-        showDialog(
-            useRootNavigator: false,
-            context: _context,
-            barrierDismissible: false,
-            builder: (_) => paymentRequest.PaymentRequestDialog(_context,
-                _accountBloc, payreq, firstPaymentItemKey, scrollController));
+
+        protectAdminAction(_context, user, () {
+          return showDialog(
+              useRootNavigator: false,
+              context: _context,
+              barrierDismissible: false,
+              builder: (_) => paymentRequest.PaymentRequestDialog(_context,
+                  _accountBloc, payreq, firstPaymentItemKey, scrollController));
+        });
       }).onError((error) {
         _setLoading(false);
         _handlingRequest = false;
