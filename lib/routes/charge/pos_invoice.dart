90ec10f659e57cba350f75a30e5d673babc33ec3
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2020-03-10 02:21:51 +0300

    Center align empty catalog text

--- lib/routes/charge/pos_invoice.dart
+++ lib/routes/charge/pos_invoice.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:breez/bloc/account/account_bloc.dart';
 import 'package:breez/bloc/account/account_model.dart';
 import 'package:breez/bloc/account/fiat_conversion.dart';
@@ -13,6 +14,7 @@ import 'package:breez/bloc/user_profile/breez_user_model.dart';
 import 'package:breez/bloc/user_profile/currency.dart';
 import 'package:breez/bloc/user_profile/user_profile_bloc.dart';
 import 'package:breez/theme_data.dart' as theme;
+import 'package:breez/utils/min_font_size.dart';
 import 'package:breez/widgets/breez_dropdown.dart';
 import 'package:breez/widgets/error_dialog.dart';
 import 'package:breez/widgets/flushbar.dart';
@@ -445,12 +447,16 @@ class POSInvoiceState extends State<POSInvoice> {
                 border: UnderlineInputBorder()),
           ),*/
           catalogItems?.length == 0
-              ? Center(
-                  child: Padding(
-                  padding: const EdgeInsets.only(top: 160.0),
-                  child: Text(
-                      "No items to display. Add items to this view using the '+' button."),
-                ))
+              ? Padding(
+                  padding: const EdgeInsets.only(
+                      top: 180.0, left: 40.0, right: 40.0),
+                  child: AutoSizeText(
+                    "No items to display. Add items to this view using the '+' button.",
+                    textAlign: TextAlign.center,
+                    minFontSize: MinFontSize(context).minFontSize,
+                    stepGranularity: 0.1,
+                  ),
+                )
               : ItemsList(accountModel, posCatalogBloc, catalogItems, _addItem)
         ],
       ),
