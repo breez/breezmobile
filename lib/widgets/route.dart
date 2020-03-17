22ee462000048099bcd1aa54f533395cbe12346c
Roei Erez <roeierez@gmail.com>
2020-03-17 20:17:34 +0200

    fix build after flutter upgrade: point flushbar to master.

--- lib/widgets/route.dart
+++ lib/widgets/route.dart
@@ -8,7 +8,7 @@ class FadeInRoute<T> extends CupertinoPageRoute<T> {
   @override
   Widget buildTransitions(BuildContext context, Animation<double> animation,
       Animation<double> secondaryAnimation, Widget child) {
-    if (settings.isInitialRoute) return child;
+
     // Fades between routes. (If you don't want any animation,
     // just return child.)
     return FadeTransition(opacity: animation, child: child);
