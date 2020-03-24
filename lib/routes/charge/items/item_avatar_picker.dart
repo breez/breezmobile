648e19fb06efc7b6e35ac09e604591dd5a828029
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2020-03-24 16:48:24 +0300

    Extract widgets to methods for readability

--- lib/routes/charge/items/item_avatar_picker.dart
+++ lib/routes/charge/items/item_avatar_picker.dart
@@ -61,93 +61,106 @@ class ItemAvatarPickerState extends State<ItemAvatarPicker> {
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Center(
-                child: Badge(
-              showBadge: _selectedImage != "",
-              position: BadgePosition.topRight(top: 5, right: -10),
-              animationType: BadgeAnimationType.fade,
-              badgeColor: Theme.of(context).primaryTextTheme.subtitle2.color,
-              badgeContent: GestureDetector(
-                child: Container(
-                  height: 24,
-                  width: 24,
-                  child: Icon(
-                    Icons.delete_outline,
-                    size: Theme.of(context).iconTheme.deleteBadgeIconTheme.size,
-                    color:
-                        Theme.of(context).iconTheme.deleteBadgeIconTheme.color,
-                  ),
-                ),
-                onTap: () {
-                  setState(() {
-                    _selectedImage = "";
-                    widget.onImageSelected(_selectedImage);
-                  });
-                },
-              ),
-              child: ItemAvatar(_selectedImage ?? null,
-                  itemName: widget.itemName, radius: 48, useDecoration: true),
-            )),
+            _buildItemAvatar(context),
             SizedBox(height: 16),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextField(
-                    textCapitalization: TextCapitalization.none,
-                    controller: _imageFilterController,
-                    enabled: _iconList != null,
-                    decoration: InputDecoration(
-                        hintText: "Search for an image",
-                        contentPadding:
-                            const EdgeInsets.only(top: 16, left: 16),
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            _imageFilterController.text.isEmpty
-                                ? Icons.search
-                                : Icons.close,
-                            size: 20,
-                          ),
-                          onPressed: _imageFilterController.text.isEmpty
-                              ? null
-                              : () {
-                                  _imageFilterController.text = "";
-                                  FocusScope.of(context)
-                                      .requestFocus(FocusNode());
-                                },
-                          padding: EdgeInsets.only(right: 24, top: 4),
-                        ),
-                        border: UnderlineInputBorder()),
-                  ),
-                )
-              ],
-            ),
+            _buildSearchBar(context),
             SizedBox(height: 8),
-            AspectRatio(
-              aspectRatio: 1,
-              child: GridView.count(
-                crossAxisCount: 4,
-                children: _iconList
-                    .map((value, icon) {
-                      return MapEntry(
-                          value,
-                          IconButton(
-                            icon: Icon(icon),
-                            iconSize: 36,
-                            onPressed: () {
-                              setState(() {
-                                _selectedImage = "icon:$value";
-                                widget.onImageSelected(_selectedImage);
-                              });
-                            },
-                          ));
-                    })
-                    .values
-                    .toList(),
-              ),
-            ),
+            _buildIconGrid(),
             SizedBox(height: 8),
           ],
         ),
+      ),
+    );
+  }
+
+  Center _buildItemAvatar(BuildContext context) {
+    return Center(
+        child: Badge(
+      showBadge: _selectedImage != "",
+      position: BadgePosition.topRight(top: 5, right: -10),
+      animationType: BadgeAnimationType.fade,
+      badgeColor: Theme.of(context).primaryTextTheme.subtitle2.color,
+      badgeContent: _buildResetIconBadge(context),
+      child: ItemAvatar(_selectedImage ?? null,
+          itemName: widget.itemName, radius: 48, useDecoration: true),
+    ));
+  }
+
+  GestureDetector _buildResetIconBadge(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        height: 24,
+        width: 24,
+        child: Icon(
+          Icons.delete_outline,
+          size: Theme.of(context).iconTheme.deleteBadgeIconTheme.size,
+          color: Theme.of(context).iconTheme.deleteBadgeIconTheme.color,
+        ),
+      ),
+      onTap: () {
+        setState(() {
+          _selectedImage = "";
+          widget.onImageSelected(_selectedImage);
+        });
+      },
+    );
+  }
+
+  Row _buildSearchBar(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: TextField(
+            textCapitalization: TextCapitalization.none,
+            controller: _imageFilterController,
+            enabled: _iconList != null,
+            decoration: InputDecoration(
+                hintText: "Search for an image",
+                contentPadding: const EdgeInsets.only(top: 16, left: 16),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _imageFilterController.text.isEmpty
+                        ? Icons.search
+                        : Icons.close,
+                    size: 20,
+                  ),
+                  onPressed: _imageFilterController.text.isEmpty
+                      ? null
+                      : () {
+                          _imageFilterController.text = "";
+                          FocusScope.of(context).requestFocus(FocusNode());
+                        },
+                  padding: EdgeInsets.only(right: 24, top: 4),
+                ),
+                border: UnderlineInputBorder()),
+          ),
+        )
+      ],
+    );
+  }
+
+  AspectRatio _buildIconGrid() {
+    return AspectRatio(
+      aspectRatio: 1,
+      child: GridView.count(
+        crossAxisCount: 4,
+        children: _iconList
+            .map((value, icon) {
+              return MapEntry(
+                  value,
+                  IconButton(
+                    icon: Icon(icon),
+                    iconSize: 36,
+                    onPressed: () {
+                      setState(() {
+                        _selectedImage = "icon:$value";
+                        widget.onImageSelected(_selectedImage);
+                      });
+                    },
+                  ));
+            })
+            .values
+            .toList(),
       ),
     );
   }
