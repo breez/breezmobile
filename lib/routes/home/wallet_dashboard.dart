334e7b2861d9a43a51c30ad86a730685a70249f0
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2020-06-11 14:46:38 +0300

    Simplify display condition of fiat value

--- lib/routes/home/wallet_dashboard.dart
+++ lib/routes/home/wallet_dashboard.dart
@@ -108,9 +108,7 @@ class WalletDashboardState extends State<WalletDashboard> {
               child: Center(
                 child: widget._accountModel != null &&
                         !widget._accountModel.initial &&
-                        (widget._accountModel.fiatConversionList.isNotEmpty ||
-                            widget._accountModel.fiatCurrency != null) &&
-                        isAboveMinAmount(widget._accountModel.fiatCurrency)
+                        isAboveMinAmount(widget._accountModel?.fiatCurrency)
                     ? FlatButton(
                         onPressed: () {
                           var newFiatConversion = nextValidFiatConversion();
