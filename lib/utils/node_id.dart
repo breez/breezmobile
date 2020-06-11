6e9e6b5e88a02d1db7338518dbd95515d2fb822c
Roei Erez <roeierez@gmail.com>
2020-06-11 12:35:05 +0300

    accept node URI for spontaneous payments.

--- lib/utils/node_id.dart
+++ lib/utils/node_id.dart
@@ -1,4 +1,19 @@
+const NODE_URI_SEPARATOR = "@";
+const NODE_ID_LENGTH = 66;
 
-bool isValidNodeId(String nodeId) {
-  return nodeId?.length == 66;
+String parseNodeId(String nodeID) {
+  if (nodeID == null) {
+    return null;
+  }
+  if (nodeID.length == NODE_ID_LENGTH) {
+    return nodeID;
+  }
+
+  if (nodeID.length > NODE_ID_LENGTH &&
+      nodeID.substring(NODE_ID_LENGTH, NODE_ID_LENGTH + 1) ==
+          NODE_URI_SEPARATOR) {
+    return nodeID.substring(0, 66);
+  }
+
+  return null;
 }
