b8bb8287e3e8171b09b3dd5f547c6e58e13478fe
Roei Erez <roeierez@gmail.com>
2020-06-16 16:32:31 +0300

    send all logs directory.

--- lib/logger.dart
+++ lib/logger.dart
@@ -3,9 +3,11 @@ library breez.logger;
 import 'dart:async';
 import 'dart:io';
 
+import 'package:archive/archive_io.dart';
 import 'package:breez/services/breezlib/breez_bridge.dart';
 import 'package:breez/services/injector.dart';
 import 'package:logging/logging.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:share_extend/share_extend.dart';
 
 final Logger log = Logger('Breez');
@@ -16,8 +18,15 @@ Future<File> get _logFile async {
 }
 
 void shareLog() {
-  _logFile.then((file) {
-    ShareExtend.share(file.path, "file");
+  _logFile.then((file) async {
+    Directory tempDir = await getTemporaryDirectory();
+    tempDir = await tempDir.createTemp("logs");
+    var encoder = ZipFileEncoder();
+    var zipFile = '${tempDir.path}/log.zip';
+    encoder.create(zipFile);
+    encoder.addDirectory(file.parent);
+    encoder.close();
+    ShareExtend.share(zipFile, "file");
   });
 }
 
