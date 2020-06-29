bf763d111afa75fa9a9081022d279531b4c2aa02
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2020-06-29 06:02:56 +0300

    Add parameter parse logic to qr_scan utils
    Credit: kaisellgren/QueryString

--- lib/utils/qr_scan.dart
+++ lib/utils/qr_scan.dart
@@ -24,3 +24,16 @@ Future<String> _getClipboardData() async {
   ClipboardData clipboardData = await Clipboard.getData(Clipboard.kTextPlain);
   return clipboardData?.text;
 }
+
+Map parse(String query) {
+  var search = new RegExp('([^&=]+)=?([^&]*)');
+  var result = new Map();
+  if (query.startsWith('?')) query = query.substring(1);
+  decode(String s) => Uri.decodeComponent(s.replaceAll('+', ' '));
+
+  for (Match match in search.allMatches(query)) {
+    result[decode(match.group(1))] = decode(match.group(2));
+  }
+
+  return result;
+}
