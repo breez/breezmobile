4558477ca2bb1b847d12712a2d98f8ecf509535c
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2020-07-07 13:58:42 +0300

    Remove paste logic of QR scan

--- lib/utils/qr_scan.dart
+++ lib/utils/qr_scan.dart
@@ -8,19 +8,10 @@ Future<String> scan() async {
     String result = "";
     await BarcodeScanner.scan().then((scanResult) async {
       await Future.delayed(Duration(milliseconds: 250));
-      if (scanResult == "GET_CLIPBOARD_DATA") {
-        result = await _getClipboardData();
-      } else {
         result = scanResult;
-      }
     });
     return result;
   } on PlatformException catch (error) {
     throw error;
   }
-}
-
-Future<String> _getClipboardData() async {
-  ClipboardData clipboardData = await Clipboard.getData(Clipboard.kTextPlain);
-  return clipboardData?.text;
 }
