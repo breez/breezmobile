792c39d79627d3b774fc12cd392d3679135a7ce7
Roei Erez <roeierez@Roeis-iMac.local>
2020-09-22 12:15:57 +0300

    remove "in fiat" from message

--- lib/routes/charge/pos_payment_dialog.dart
+++ lib/routes/charge/pos_payment_dialog.dart
@@ -187,7 +187,7 @@ class _PosPaymentDialogState extends State<PosPaymentDialog> {
               : Padding(
                   padding: const EdgeInsets.only(bottom: 8.0),
                   child: Text(
-                      "A setup fee of ${Currency.SAT.format(widget.paymentRequest.lspFee)} (${account.fiatCurrency.format(widget.paymentRequest.lspFee)} in fiat) is applied to this invoice.",
+                      "A setup fee of ${Currency.SAT.format(widget.paymentRequest.lspFee)} (${account.fiatCurrency.format(widget.paymentRequest.lspFee)}) is applied to this invoice.",
                       textAlign: TextAlign.center,
                       style: Theme.of(context).primaryTextTheme.headline4),
                 ),
