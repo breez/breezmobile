185ec86e5e12ed1f05a3fdb51afc7e72519abade
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2020-10-02 05:36:34 +0300

    Apply theme changes and show fiat conversion below balance

--- lib/routes/home/wallet_dashboard.dart
+++ lib/routes/home/wallet_dashboard.dart
@@ -25,16 +25,14 @@ class WalletDashboard extends StatefulWidget {
 }
 
 class WalletDashboardState extends State<WalletDashboard> {
-  static const CHART_MAX_VERTICAL_OFFSET = 70.0;
-  static const CHART_MAX_HORIZONTAL_OFFSET = 30.0;
-  static const BALANCE_OFFSET_TRANSITION = 30.0;
-  bool _showFiatCurrency = false;
+  static const BALANCE_OFFSET_TRANSITION = 40.0;
 
   @override
   Widget build(BuildContext context) {
-    double startHeaderSize = Theme.of(context).textTheme.headline5.fontSize;
+    double startHeaderSize =
+        Theme.of(context).accentTextTheme.headline4.fontSize;
     double endHeaderFontSize =
-        Theme.of(context).textTheme.headline5.fontSize - 8.0;
+        Theme.of(context).accentTextTheme.headline4.fontSize - 8.0;
     bool showProgressBar = (widget._accSettings?.showConnectProgress == true &&
             !widget._accountModel.initial) ||
         widget._accountModel?.isInitialBootstrap == true;
@@ -54,7 +52,7 @@ class WalletDashboardState extends State<WalletDashboard> {
                   child: StatusIndicator(context, widget._accountModel))
               : SizedBox(),
           Positioned(
-            top: 30 - BALANCE_OFFSET_TRANSITION * widget._offsetFactor,
+            top: 40 - BALANCE_OFFSET_TRANSITION * widget._offsetFactor,
             child: Center(
               child: widget._accountModel != null &&
                       !widget._accountModel.initial
@@ -67,71 +65,69 @@ class WalletDashboardState extends State<WalletDashboard> {
                         widget._onCurrencyChange(
                             Currency.currencies[nextCurrencyIndex]);
                       },
-                      child: (_showFiatCurrency &&
-                              widget._accountModel.fiatCurrency != null)
-                          ? Text("${widget._accountModel.formattedFiatBalance}",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headline5
-                                  .copyWith(
-                                      fontSize: startHeaderSize -
-                                          (startHeaderSize - endHeaderFontSize) *
-                                              widget._offsetFactor))
-                          : Text(widget._accountModel.currency.format(widget._accountModel.balance, removeTrailingZeros: true),
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headline5
-                                  .copyWith(
-                                      fontSize: startHeaderSize -
-                                          (startHeaderSize - endHeaderFontSize) *
-                                              widget._offsetFactor)))
+                      child: RichText(
+                        text: TextSpan(
+                            style: Theme.of(context)
+                                .accentTextTheme
+                                .headline4
+                                .copyWith(
+                                    fontSize: startHeaderSize -
+                                        (startHeaderSize - endHeaderFontSize) *
+                                            widget._offsetFactor),
+                            text: widget._accountModel.currency.format(
+                                widget._accountModel.balance,
+                                removeTrailingZeros: true,
+                                includeDisplayName: false),
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: " " +
+                                    widget._accountModel.currency.displayName,
+                                style: Theme.of(context)
+                                    .accentTextTheme
+                                    .headline4
+                                    .copyWith(
+                                        fontSize: startHeaderSize * 0.6 -
+                                            (startHeaderSize * 0.6 -
+                                                    endHeaderFontSize) *
+                                                widget._offsetFactor),
+                              ),
+                            ]),
+                      ))
                   : SizedBox(),
             ),
           ),
           Positioned(
-              top: 70 - BALANCE_OFFSET_TRANSITION * widget._offsetFactor,
-              child: Center(
-                child: widget._accountModel != null &&
-                        !widget._accountModel.initial &&
-                        widget._accountModel.fiatConversionList.isNotEmpty &&
-                        isAboveMinAmount(widget._accountModel?.fiatCurrency)
-                    ? FlatButton(
-                        onPressed: widget._offsetFactor <= 0.20
-                            ? () {
-                                var newFiatConversion =
-                                    nextValidFiatConversion();
-                                if (newFiatConversion != null)
-                                  widget._onFiatCurrencyChange(
-                                      newFiatConversion.currencyData.shortName);
-                              }
-                            : null,
-                        child: Text(
-                            "${widget._accountModel.formattedFiatBalance}",
-                            style: Theme.of(context)
-                                .textTheme
-                                .subtitle2
-                                .copyWith(
-                                    color: Theme.of(context)
-                                        .textTheme
-                                        .subtitle2
-                                        .color
-                                        .withOpacity(pow(
-                                            0.95 - widget._offsetFactor, 8)))))
-                    : SizedBox(),
-              )),
+            top: 80 - BALANCE_OFFSET_TRANSITION * widget._offsetFactor,
+            child: Center(
+              child: widget._accountModel != null &&
+                      !widget._accountModel.initial &&
+                      widget._accountModel.fiatConversionList.isNotEmpty &&
+                      isAboveMinAmount(widget._accountModel?.fiatCurrency)
+                  ? FlatButton(
+                      onPressed: () {
+                        var newFiatConversion = nextValidFiatConversion();
+                        if (newFiatConversion != null)
+                          widget._onFiatCurrencyChange(
+                              newFiatConversion.currencyData.shortName);
+                      },
+                      child: Text(
+                          "${widget._accountModel.formattedFiatBalance}",
+                          style: Theme.of(context)
+                              .accentTextTheme
+                              .subtitle1
+                              .copyWith(
+                                  color: Theme.of(context)
+                                      .accentTextTheme
+                                      .subtitle1
+                                      .color
+                                      .withOpacity(pow(
+                                          1.00 - widget._offsetFactor, 8)))),
+                    )
+                  : SizedBox(),
+            ),
+          ),
         ],
       ),
-      behavior: HitTestBehavior.translucent,
-      onLongPressStart: (_) {
-        setState(() {
-          _showFiatCurrency = true;
-        });
-      },
-      onLongPressEnd: (_) {
-        setState(() {
-          _showFiatCurrency = false;
-        });
-      },
     );
   }
 
