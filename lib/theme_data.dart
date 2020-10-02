1eabb66bc495569706ef388fb4297aa6b12982d4
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2020-10-02 15:02:06 +0300

    Use new fontsized for balance texts

--- lib/theme_data.dart
+++ lib/theme_data.dart
@@ -107,12 +107,12 @@ final ThemeData blueTheme = ThemeData(
   accentTextTheme: TextTheme(
       headline4: TextStyle(
           color: Color.fromRGBO(0, 133, 251, 1.0),
-          fontSize: 26.0,
+          fontSize: 30.0,
           fontWeight: FontWeight.w400,
           height: 1.52),
       subtitle1: TextStyle(
           color: Color.fromRGBO(0, 133, 251, 1.0),
-          fontSize: 12.3,
+          fontSize: 13.5,
           fontWeight: FontWeight.w400,
           height: 1.24,
           letterSpacing: 0.2)),
@@ -201,12 +201,12 @@ final ThemeData darkTheme = ThemeData(
   accentTextTheme: TextTheme(
       headline4: TextStyle(
           color: Colors.white,
-          fontSize: 26.0,
+          fontSize: 30.0,
           fontWeight: FontWeight.w400,
           height: 1.52),
       subtitle1: TextStyle(
           color: Colors.white,
-          fontSize: 12.3,
+          fontSize: 13.5,
           fontWeight: FontWeight.w400,
           height: 1.24,
           letterSpacing: 0.2)),
