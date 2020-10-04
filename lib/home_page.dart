812b4e239273cd8fddc87de6dfea0812d982e77a
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2020-10-04 22:20:07 +0300

    Resolve conflict cont.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -18,6 +18,8 @@ import 'package:breez/bloc/user_profile/breez_user_model.dart';
 import 'package:breez/bloc/user_profile/user_profile_bloc.dart';
 import 'package:breez/routes/admin_login_dialog.dart';
 import 'package:breez/routes/charge/pos_invoice.dart';
+import 'package:breez/routes/home/bottom_actions_bar.dart';
+import 'package:breez/routes/home/qr_action_button.dart';
 import 'package:breez/theme_data.dart' as theme;
 import 'package:breez/widgets/enter_payment_info_dialog.dart';
 import 'package:breez/widgets/error_dialog.dart';
@@ -399,6 +401,15 @@ class HomeState extends State<Home> {
                                                               "src/icon/advanced.png"),
                                                     ],
                                                     _onNavigationItemSelected),
+                                                bottomNavigationBar:
+                                                    BottomActionsBar(account,
+                                                        firstPaymentItemKey),
+                                                floatingActionButton:
+                                                    QrActionButton(account,
+                                                        firstPaymentItemKey),
+                                                floatingActionButtonLocation:
+                                                    FloatingActionButtonLocation
+                                                        .centerDocked,
                                                 body: widget._screenBuilders[
                                                         _activeScreen] ??
                                                     _homePage(user.isPOS)),
