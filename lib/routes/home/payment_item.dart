f53151ef613081e0c3adab855cf4fa8dcd1ef34c
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2020-10-08 07:35:47 +0300

    Add top border to first item to separate the filter bar

--- lib/routes/home/payment_item.dart
+++ lib/routes/home/payment_item.dart
@@ -21,82 +21,96 @@ class PaymentItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(alignment: Alignment.bottomCenter, children: <Widget>[
-      ListTile(
-        tileColor: (_itemIndex % 2 == 0)
-            ? theme.customData[theme.themeId].paymentListBgColor
-            : theme.customData[theme.themeId].paymentListAlternateBgColor,
-        leading: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              shape: BoxShape.circle,
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
-                    offset: Offset(0.5, 0.5),
-                    blurRadius: 5.0),
-              ],
-            ),
-            child: _buildPaymentItemAvatar()),
-        key: _firstItem ? firstPaymentItemKey : null,
-        title: Text(
-          _paymentInfo.title,
-          style: Theme.of(context).accentTextTheme.subtitle2,
-          overflow: TextOverflow.ellipsis,
-        ),
-        subtitle: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                DateUtils.formatMonthDate(DateTime.fromMillisecondsSinceEpoch(
-                    _paymentInfo.creationTimestamp.toInt() * 1000)),
-                style: Theme.of(context).accentTextTheme.caption,
+      Container(
+        decoration: BoxDecoration(
+            border: _firstItem
+                ? Border(
+                    top: BorderSide(
+                        width: 2,
+                        color:
+                            theme.customData[theme.themeId].dashboardBgColor))
+                : null),
+        child: ListTile(
+          tileColor: (_itemIndex % 2 == 0)
+              ? theme.customData[theme.themeId].paymentListBgColor
+              : theme.customData[theme.themeId].paymentListAlternateBgColor,
+          leading: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      offset: Offset(0.5, 0.5),
+                      blurRadius: 5.0),
+                ],
               ),
-              _paymentInfo.pending
-                  ? Text(" (Pending)",
-                      style: Theme.of(context).accentTextTheme.caption.copyWith(
-                          color:
-                              theme.customData[theme.themeId].pendingTextColor))
-                  : SizedBox()
-            ]),
-        trailing: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Text(
-                    (_paymentInfo.type == PaymentType.SENT ||
-                                _paymentInfo.type == PaymentType.WITHDRAWAL ||
-                                _paymentInfo.type == PaymentType.CLOSED_CHANNEL
-                            ? "- "
-                            : "+ ") +
-                        _paymentInfo.currency.format(_paymentInfo.amount,
-                            includeDisplayName: false),
-                    style: Theme.of(context).accentTextTheme.headline6,
-                  )
-                ]),
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+              child: _buildPaymentItemAvatar()),
+          key: _firstItem ? firstPaymentItemKey : null,
+          title: Text(
+            _paymentInfo.title,
+            style: Theme.of(context).accentTextTheme.subtitle2,
+            overflow: TextOverflow.ellipsis,
+          ),
+          subtitle: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  DateUtils.formatMonthDate(DateTime.fromMillisecondsSinceEpoch(
+                      _paymentInfo.creationTimestamp.toInt() * 1000)),
+                  style: Theme.of(context).accentTextTheme.caption,
+                ),
+                _paymentInfo.pending
+                    ? Text(" (Pending)",
+                        style: Theme.of(context)
+                            .accentTextTheme
+                            .caption
+                            .copyWith(
+                                color: theme.customData[theme.themeId]
+                                    .pendingTextColor))
+                    : SizedBox()
+              ]),
+          trailing: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   mainAxisSize: MainAxisSize.min,
                   children: <Widget>[
-                    _paymentInfo.fee == 0
-                        ? SizedBox()
-                        : Text(
-                            "FEE " +
-                                _paymentInfo.currency.format(_paymentInfo.fee,
-                                    includeDisplayName: false),
-                            style: Theme.of(context).accentTextTheme.caption)
+                    Text(
+                      (_paymentInfo.type == PaymentType.SENT ||
+                                  _paymentInfo.type == PaymentType.WITHDRAWAL ||
+                                  _paymentInfo.type ==
+                                      PaymentType.CLOSED_CHANNEL
+                              ? "- "
+                              : "+ ") +
+                          _paymentInfo.currency.format(_paymentInfo.amount,
+                              includeDisplayName: false),
+                      style: Theme.of(context).accentTextTheme.headline6,
+                    )
                   ]),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      _paymentInfo.fee == 0
+                          ? SizedBox()
+                          : Text(
+                              "FEE " +
+                                  _paymentInfo.currency.format(_paymentInfo.fee,
+                                      includeDisplayName: false),
+                              style: Theme.of(context).accentTextTheme.caption)
+                    ]),
+              ),
+            ],
+          ),
+          onTap: () => showPaymentDetailsDialog(context, _paymentInfo),
         ),
-        onTap: () => showPaymentDetailsDialog(context, _paymentInfo),
       ),
       Divider(
         height: 0.0,
