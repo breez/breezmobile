59181c9048b643d6927e0dabc1a07301883d9fe3
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2020-10-15 17:55:25 +0300

    Close invoice dialog on receiving payment

--- lib/routes/create_invoice/qr_code_dialog.dart
+++ lib/routes/create_invoice/qr_code_dialog.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:breez/bloc/account/account_bloc.dart';
 import 'package:breez/bloc/account/account_model.dart';
 import 'package:breez/bloc/invoice/invoice_bloc.dart';
@@ -24,7 +26,45 @@ class QrCodeDialog extends StatefulWidget {
   }
 }
 
-class QrCodeDialogState extends State<QrCodeDialog> {
+class QrCodeDialogState extends State<QrCodeDialog>
+    with SingleTickerProviderStateMixin {
+  Animation<double> _opacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    var controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 1000));
+    _opacityAnimation = Tween<double>(begin: 0.0, end: 1.0)
+        .animate(CurvedAnimation(parent: controller, curve: Curves.ease));
+    controller.value = 1.0;
+    controller.addStatusListener((status) {
+      if (status == AnimationStatus.dismissed && this.mounted) {
+        Navigator.pop(context, true);
+      }
+    });
+
+    widget._invoiceBloc.readyInvoicesStream.first.then((payReqModel) {
+      _listenPaidInvoice(payReqModel, controller);
+    });
+  }
+
+  void _listenPaidInvoice(
+      PaymentRequestModel payReqModel, AnimationController controller) async {
+    var payreq =
+        await widget._invoiceBloc.paidInvoicesStream.firstWhere((payreq) {
+      bool ok = payreq.paymentHash == payReqModel.paymentHash;
+      return ok;
+    }, orElse: () => null);
+    if (payreq != null) {
+      Timer(Duration(milliseconds: 1000), () {
+        if (this.mounted) {
+          controller.reverse();
+        }
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return _buildQrCodeDialog();
@@ -34,141 +74,145 @@ class QrCodeDialogState extends State<QrCodeDialog> {
     return StreamBuilder<PaymentRequestModel>(
         stream: widget._invoiceBloc.readyInvoicesStream,
         builder: (context, snapshot) {
-          return SimpleDialog(
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Invoice",
-                  style: Theme.of(context).dialogTheme.titleTextStyle,
-                ),
+          return FadeTransition(
+            opacity: _opacityAnimation,
+            child: SimpleDialog(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Invoice",
+                    style: Theme.of(context).dialogTheme.titleTextStyle,
+                  ),
+                  StreamBuilder<AccountModel>(
+                      stream: widget._accountBloc.accountStream,
+                      builder: (accCtx, accSnapshot) {
+                        bool synced = accSnapshot.data?.synced == true;
+                        return StreamBuilder<PaymentRequestModel>(
+                          stream: widget._invoiceBloc.readyInvoicesStream,
+                          builder: (context, snapshot) {
+                            if (!snapshot.hasData || !synced) {
+                              return Container();
+                            }
+                            return Row(
+                              children: <Widget>[
+                                IconButton(
+                                  splashColor: Colors.transparent,
+                                  highlightColor: Colors.transparent,
+                                  padding: EdgeInsets.only(
+                                      top: 8.0,
+                                      bottom: 8.0,
+                                      right: 2.0,
+                                      left: 14.0),
+                                  icon: Icon(
+                                      IconData(0xe917, fontFamily: 'icomoon')),
+                                  color: Theme.of(context)
+                                      .primaryTextTheme
+                                      .button
+                                      .color,
+                                  onPressed: () {
+                                    ShareExtend.share(
+                                        "lightning:" + snapshot.data.rawPayReq,
+                                        "text");
+                                  },
+                                ),
+                                IconButton(
+                                  splashColor: Colors.transparent,
+                                  highlightColor: Colors.transparent,
+                                  padding: EdgeInsets.only(
+                                      top: 8.0,
+                                      bottom: 8.0,
+                                      right: 14.0,
+                                      left: 2.0),
+                                  icon: Icon(
+                                      IconData(0xe90b, fontFamily: 'icomoon')),
+                                  color: Theme.of(context)
+                                      .primaryTextTheme
+                                      .button
+                                      .color,
+                                  onPressed: () {
+                                    ServiceInjector().device.setClipboardText(
+                                        snapshot.data.rawPayReq);
+                                    showFlushbar(context,
+                                        message:
+                                            "Invoice data was copied to your clipboard.",
+                                        duration: Duration(seconds: 3));
+                                  },
+                                )
+                              ],
+                            );
+                          },
+                        );
+                      }),
+                ],
+              ),
+              titlePadding: EdgeInsets.fromLTRB(20.0, 22.0, 0.0, 8.0),
+              contentPadding:
+                  EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
+              children: <Widget>[
                 StreamBuilder<AccountModel>(
-                    stream: widget._accountBloc.accountStream,
-                    builder: (accCtx, accSnapshot) {
-                      bool synced = accSnapshot.data?.synced == true;
-                      return StreamBuilder<PaymentRequestModel>(
-                        stream: widget._invoiceBloc.readyInvoicesStream,
-                        builder: (context, snapshot) {
-                          if (!snapshot.hasData || !synced) {
-                            return Container();
-                          }
-                          return Row(
-                            children: <Widget>[
-                              IconButton(
-                                splashColor: Colors.transparent,
-                                highlightColor: Colors.transparent,
-                                padding: EdgeInsets.only(
-                                    top: 8.0,
-                                    bottom: 8.0,
-                                    right: 2.0,
-                                    left: 14.0),
-                                icon: Icon(
-                                    IconData(0xe917, fontFamily: 'icomoon')),
-                                color: Theme.of(context)
-                                    .primaryTextTheme
-                                    .button
-                                    .color,
-                                onPressed: () {
-                                  ShareExtend.share(
-                                      "lightning:" + snapshot.data.rawPayReq,
-                                      "text");
-                                },
+                  stream: widget._accountBloc.accountStream,
+                  builder: (context, accSnapshot) {
+                    if (snapshot.hasError) {
+                      return Container();
+                    }
+                    bool synced = accSnapshot.data?.synced;
+                    double syncProgress = accSnapshot.data?.syncProgress;
+                    return AnimatedCrossFade(
+                      firstChild: Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 310.0,
+                          child: synced == false
+                              ? CircularProgress(
+                                  color:
+                                      Theme.of(context).textTheme.button.color,
+                                  size: 100.0,
+                                  value: syncProgress,
+                                  title: "Synchronizing to the network")
+                              : Align(
+                                  alignment: Alignment(0, -0.33),
+                                  child: Container(
+                                    height: 80.0,
+                                    width: 80.0,
+                                    child: CircularProgressIndicator(
+                                      valueColor: AlwaysStoppedAnimation<Color>(
+                                          Theme.of(context)
+                                              .primaryTextTheme
+                                              .button
+                                              .color),
+                                      backgroundColor:
+                                          Theme.of(context).backgroundColor,
+                                    ),
+                                  ))),
+                      secondChild: Column(
+                        children: [
+                          AspectRatio(
+                            aspectRatio: 1,
+                            child: Container(
+                              width: 230.0,
+                              height: 230.0,
+                              child: CompactQRImage(
+                                data: snapshot.data?.rawPayReq,
                               ),
-                              IconButton(
-                                splashColor: Colors.transparent,
-                                highlightColor: Colors.transparent,
-                                padding: EdgeInsets.only(
-                                    top: 8.0,
-                                    bottom: 8.0,
-                                    right: 14.0,
-                                    left: 2.0),
-                                icon: Icon(
-                                    IconData(0xe90b, fontFamily: 'icomoon')),
-                                color: Theme.of(context)
-                                    .primaryTextTheme
-                                    .button
-                                    .color,
-                                onPressed: () {
-                                  ServiceInjector().device.setClipboardText(
-                                      snapshot.data.rawPayReq);
-                                  showFlushbar(context,
-                                      message:
-                                          "Invoice data was copied to your clipboard.",
-                                      duration: Duration(seconds: 3));
-                                },
-                              )
-                            ],
-                          );
-                        },
-                      );
-                    }),
-              ],
-            ),
-            titlePadding: EdgeInsets.fromLTRB(20.0, 22.0, 0.0, 8.0),
-            contentPadding:
-                EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
-            children: <Widget>[
-              StreamBuilder<AccountModel>(
-                stream: widget._accountBloc.accountStream,
-                builder: (context, accSnapshot) {
-                  if (snapshot.hasError) {
-                    return Container();
-                  }
-                  bool synced = accSnapshot.data?.synced;
-                  double syncProgress = accSnapshot.data?.syncProgress;
-                  return AnimatedCrossFade(
-                    firstChild: Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: 310.0,
-                        child: synced == false
-                            ? CircularProgress(
-                                color: Theme.of(context).textTheme.button.color,
-                                size: 100.0,
-                                value: syncProgress,
-                                title: "Synchronizing to the network")
-                            : Align(
-                                alignment: Alignment(0, -0.33),
-                                child: Container(
-                                  height: 80.0,
-                                  width: 80.0,
-                                  child: CircularProgressIndicator(
-                                    valueColor: AlwaysStoppedAnimation<Color>(
-                                        Theme.of(context)
-                                            .primaryTextTheme
-                                            .button
-                                            .color),
-                                    backgroundColor:
-                                        Theme.of(context).backgroundColor,
-                                  ),
-                                ))),
-                    secondChild: Column(
-                      children: [
-                        AspectRatio(
-                          aspectRatio: 1,
-                          child: Container(
-                            width: 230.0,
-                            height: 230.0,
-                            child: CompactQRImage(
-                              data: snapshot.data?.rawPayReq,
                             ),
                           ),
-                        ),
-                        Padding(padding: EdgeInsets.only(top: 24.0)),
-                        Container(
-                            width: MediaQuery.of(context).size.width,
-                            child: _buildExpiryAndFeeMessage(snapshot)),
-                        Padding(padding: EdgeInsets.only(top: 16.0)),
-                      ],
-                    ),
-                    duration: Duration(seconds: 1),
-                    crossFadeState: (!snapshot.hasData || !synced)
-                        ? CrossFadeState.showFirst
-                        : CrossFadeState.showSecond,
-                  );
-                },
-              ),
-              _buildCloseButton()
-            ],
+                          Padding(padding: EdgeInsets.only(top: 24.0)),
+                          Container(
+                              width: MediaQuery.of(context).size.width,
+                              child: _buildExpiryAndFeeMessage(snapshot)),
+                          Padding(padding: EdgeInsets.only(top: 16.0)),
+                        ],
+                      ),
+                      duration: Duration(seconds: 1),
+                      crossFadeState: (!snapshot.hasData || !synced)
+                          ? CrossFadeState.showFirst
+                          : CrossFadeState.showSecond,
+                    );
+                  },
+                ),
+                _buildCloseButton()
+              ],
+            ),
           );
         });
   }
