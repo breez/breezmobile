e785f650f69e93ff716e62a20924de29ea31efb6
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2020-11-13 01:11:35 +0300

    Replace description on details dialog with scrollable area that shows ~3 lines at a time

--- lib/widgets/payment_details_dialog.dart
+++ lib/widgets/payment_details_dialog.dart
@@ -88,13 +88,21 @@ Future<Null> showPaymentDetailsDialog(
               ? Container()
               : Padding(
                   padding: EdgeInsets.only(left: 16.0, right: 16.0),
-                  child: AutoSizeText(
-                    paymentInfo.description,
-                    style: Theme.of(context).primaryTextTheme.headline4,
-                    textAlign: paymentInfo.description.length > 40
-                        ? TextAlign.justify
-                        : TextAlign.center,
-                    maxLines: 3,
+                  child: Container(
+                    constraints: BoxConstraints(
+                        maxHeight: 54, minWidth: double.infinity),
+                    child: Scrollbar(
+                      child: SingleChildScrollView(
+                        child: AutoSizeText(
+                          paymentInfo.description,
+                          style: Theme.of(context).primaryTextTheme.headline4,
+                          textAlign: paymentInfo.description.length > 40 &&
+                                  !paymentInfo.description.contains("\n")
+                              ? TextAlign.start
+                              : TextAlign.center,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
           paymentInfo.amount == null
