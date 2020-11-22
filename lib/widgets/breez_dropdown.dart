dbfea3ddacbc3030a5ad1ea33b7e804493010726
Roei Erez <roeierez@Roeis-iMac.local>
2020-11-22 16:55:17 +0200

    fix flutter breaking changes.

--- lib/widgets/breez_dropdown.dart
+++ lib/widgets/breez_dropdown.dart
@@ -796,7 +796,7 @@ class _BreezDropdownButtonState<T> extends State<BreezDropdownButton<T>>
         padding: _kMenuItemPadding.resolve(textDirection),
         selectedIndex: _selectedIndex ?? 0,
         elevation: widget.elevation,
-        theme: Theme.of(context, shadowThemeOnly: true),
+        theme: Theme.of(context),
         style: _textStyle,
         barrierLabel:
             MaterialLocalizations.of(context).modalBarrierDismissLabel,
@@ -853,7 +853,7 @@ class _BreezDropdownButtonState<T> extends State<BreezDropdownButton<T>>
       widget.onChanged != null;
 
   Orientation _getOrientation(BuildContext context) {
-    Orientation result = MediaQuery.of(context, nullOk: true)?.orientation;
+    Orientation result = MediaQuery.of(context)?.orientation;
     if (result == null) {
       // If there's no MediaQuery, then use the window aspect to determine
       // orientation.
