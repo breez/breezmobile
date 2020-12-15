0dda77d08594df86fd758a1551bf51b4b4dd4ebb
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2020-12-15 21:43:06 +0300

    Use official webview plugin on marketplace

--- lib/routes/marketplace/vendor_webview.dart
+++ lib/routes/marketplace/vendor_webview.dart
@@ -1,16 +1,13 @@
-import 'dart:async';
 import 'dart:convert' as JSON;
-import 'dart:typed_data';
+import 'dart:io';
 
 import 'package:breez/bloc/account/account_bloc.dart';
 import 'package:breez/bloc/blocs_provider.dart';
 import 'package:breez/bloc/invoice/invoice_bloc.dart';
-import 'package:breez/bloc/user_profile/breez_user_model.dart';
-import 'package:breez/bloc/user_profile/user_profile_bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:flutter/services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-import '../../user_app.dart';
 import 'webln_handlers.dart';
 
 class VendorWebViewPage extends StatefulWidget {
@@ -30,96 +27,41 @@ class VendorWebViewPage extends StatefulWidget {
   }
 }
 
-class VendorWebViewPageState extends State<VendorWebViewPage> with RouteAware {
-  final _widgetWebview = FlutterWebviewPlugin();
-  StreamSubscription<BreezUserModel> _userSubscription;
-  StreamSubscription _postMessageListener;
+class VendorWebViewPageState extends State<VendorWebViewPage> {
   WeblnHandlers _weblnHandlers;
+  InvoiceBloc _invoiceBloc;
   bool _isInit = false;
-  Uint8List _screenshotData;
+
+  WebViewController _webViewController;
 
   @override
   void initState() {
     super.initState();
-    _widgetWebview.onDestroy.listen((_) {
-      if (Navigator.canPop(context)) {
-        Navigator.of(context).pop();
-      }
-    });
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
   }
 
   @override
   void didChangeDependencies() {
     if (!_isInit) {
-      
-      var invoiceBloc = AppBlocsProvider.of<InvoiceBloc>(context);
-      var accountBloc = AppBlocsProvider.of<AccountBloc>(context);
-      var userBloc = AppBlocsProvider.of<UserProfileBloc>(context);
-
-      _weblnHandlers =
-          WeblnHandlers(context, accountBloc, invoiceBloc);
-
-      String loadedURL;
-      _widgetWebview.onStateChanged.listen((state) async {
-        if (state.type == WebViewState.finishLoad && loadedURL != state.url) {
-          loadedURL = state.url;
-          _widgetWebview.evalJavascript(await _weblnHandlers.initWeblnScript);
-        }
-      });
-
-      _postMessageListener = _widgetWebview.onPostMessage.listen((msg) {
-        if (msg != null) {
-          var postMessage = (widget._title == "ln.pizza")
-              ? {"action": "sendPayment", "payReq": msg}
-              : JSON.jsonDecode(msg);
-          _weblnHandlers.handleMessage(postMessage).then((resScript) {
-            if (resScript != null) {
-              _widgetWebview.evalJavascript(resScript);
-              _widgetWebview.show();
-              setState(() {
-                _screenshotData = null;
-              });
-            }
-          });
-        }
-      });
-
-      _userSubscription = userBloc.userStream.listen((user) {
-        user.locked ? _hideWebView() : _showWebView();
-      });
+      _invoiceBloc = AppBlocsProvider.of<InvoiceBloc>(context);
+      _weblnHandlers = WeblnHandlers(context, widget.accountBloc, _invoiceBloc);
 
       _isInit = true;
     }
-    routeObserver.subscribe(this, ModalRoute.of(context));
     super.didChangeDependencies();
   }
 
   @override
   void dispose() {
-    routeObserver.unsubscribe(this);
-    _postMessageListener?.cancel();
-    _widgetWebview.dispose();
     _weblnHandlers?.dispose();
-    _userSubscription?.cancel();
     super.dispose();
-  }
-
-
-  @override
-  // Called when the current route has been pushed.
-  void didPushNext() {
-    _hideWebView();
-  }
-
-  @override
-  // Called when the top route has been popped off, and the current route shows up.
-  void didPopNext() {
-    _showWebView();
   }
 
   @override
   Widget build(BuildContext context) {
-    return WebviewScaffold(
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      resizeToAvoidBottomPadding: false,
       appBar: AppBar(
         actions: <Widget>[
           IconButton(
@@ -139,46 +81,61 @@ class VendorWebViewPageState extends State<VendorWebViewPage> with RouteAware {
         ),
         elevation: 0.0,
       ),
-      url: widget._url,
-      withJavascript: true,
-      withZoom: false,
-      clearCache: true,
-      initialChild:
-          _screenshotData != null ? Image.memory(_screenshotData) : null,
+      body: Listener(
+        onPointerDown: (_) {
+          // hide keyboard on click
+          SystemChannels.textInput.invokeMethod('TextInput.hide');
+        },
+        child: WebView(
+            onWebViewCreated: (WebViewController webViewController) {
+              setState(() {
+                _webViewController = webViewController;
+              });
+            },
+            javascriptMode: JavascriptMode.unrestricted,
+            javascriptChannels: <JavascriptChannel>[
+              _breezJavascriptChannel(context),
+            ].toSet(),
+            onPageFinished: (String url) async {
+              // intercept ln link clicks
+              _webViewController.evaluateJavascript(await rootBundle
+                  .loadString('src/scripts/lightningLinkInterceptor.js'));
+              // redirect post messages to javascript channel
+              _webViewController.evaluateJavascript(
+                  "window.onmessage = (message) => window.BreezWebView.postMessage(message.data);");
+              _webViewController
+                  .evaluateJavascript(await _weblnHandlers.initWeblnScript);
+              print('Page finished loading: $url');
+            },
+            initialUrl: widget._url),
+      ),
     );
   }
 
-  Future _showWebView() async {
-    await _widgetWebview.show();
-    setState(() {
-      _screenshotData = null;
-    });
-  }
-
-  Future _hideWebView() {
-    Completer beforeCompleter = Completer();
-    FocusScope.of(context).requestFocus(FocusNode());
-    // Wait for keyboard and screen animations to settle
-    Timer(Duration(milliseconds: 750), () {
-      // Take screenshot and show payment request dialog
-      _takeScreenshot().then((imageData) {
-        setState(() {
-          _screenshotData = imageData;
-        });
-        // Wait for memory image to load
-        Timer(Duration(milliseconds: 200), () {
-          // Hide Webview to interact with payment request dialog
-          _widgetWebview.hide();
-          beforeCompleter.complete();
-        });
-      });
-    });
-
-    return beforeCompleter.future;
-  }
-
-  Future _takeScreenshot() async {
-    Uint8List _imageData = await _widgetWebview.takeScreenshot();
-    return _imageData;
+  JavascriptChannel _breezJavascriptChannel(BuildContext context) {
+    return JavascriptChannel(
+      name: "BreezWebView",
+      onMessageReceived: (JavascriptMessage message) {
+        if (message != null) {
+          var postMessage = (widget._title == "ln.pizza")
+              ? {"action": "sendPayment", "payReq": message.message}
+              : JSON.jsonDecode(message.message);
+          // handle lightning links and WebLN payments
+          if (postMessage["lightningLink"] != null &&
+              postMessage["lightningLink"]
+                  .toLowerCase()
+                  .startsWith("lightning:")) {
+            _invoiceBloc.newLightningLinkSink
+                .add(postMessage["lightningLink"].substring(10));
+          } else {
+            _weblnHandlers.handleMessage(postMessage).then((resScript) {
+              if (resScript != null) {
+                _webViewController.evaluateJavascript(resScript);
+              }
+            });
+          }
+        }
+      },
+    );
   }
 }
