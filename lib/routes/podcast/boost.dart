67f9a352b0864c5ab3fad5118bf3adb67923be25
Roei Erez <roeierez@gmail.com>
2021-03-08 13:44:53 +0200

    persist boost

--- lib/routes/podcast/boost.dart
+++ lib/routes/podcast/boost.dart
@@ -1,5 +1,8 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:breez/bloc/blocs_provider.dart';
 import 'package:breez/bloc/user_profile/breez_user_model.dart';
+import 'package:breez/bloc/user_profile/user_actions.dart';
+import 'package:breez/bloc/user_profile/user_profile_bloc.dart';
 import 'package:breez/utils/min_font_size.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -18,174 +21,185 @@ class BoostWidget extends StatefulWidget {
 }
 
 class _BoostWidgetState extends State<BoostWidget> {
-  int selectedBoostIndex;
-
-  @override
-  void initState() {
-    super.initState();
-    selectedBoostIndex =
-        widget.boostAmountList.indexOf(widget.userModel.preferredBoostValue);
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Container(
-          width: 88,
-          child: FlatButton.icon(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(6.0),
-                side: BorderSide(
-                    color: Theme.of(context).brightness == Brightness.light
-                        ? Color(0xFF0085fb)
-                        : Colors.white70,
-                    width: 1.6)),
-            icon: ImageIcon(
-              AssetImage("src/icon/boost.png"),
-              size: 20,
-              color: Theme.of(context).appBarTheme.actionsIconTheme.color,
-            ),
-            label: Container(
-              width: 44,
-              child: AutoSizeText(
-                "BOOST!",
-                style: TextStyle(
-                  fontSize: 14,
-                  height: 1.2,
-                  letterSpacing: 0,
-                  fontWeight: FontWeight.w500,
-                  color: Theme.of(context).buttonColor,
-                ),
-                minFontSize: MinFontSize(context).minFontSize,
-                stepGranularity: 0.1,
-                maxLines: 1,
-              ),
-            ),
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-            onPressed: () {
-              widget.onBoost(
-                  widget.boostAmountList.elementAt(selectedBoostIndex));
-            },
-          ),
-        ),
-        Flexible(
-          fit: FlexFit.tight,
-          flex: 1,
-          child: Center(
-            child: Container(
-              width: 92,
-              child: Stack(
-                fit: StackFit.loose,
-                children: [
-                  GestureDetector(
-                      child: Container(
-                          width: 32,
-                          height: 64,
-                          child: Material(
-                            color: Colors.transparent,
-                            borderRadius: BorderRadius.circular(32),
-                            child: InkWell(
-                              borderRadius: BorderRadius.circular(32),
-                              onTap: () {
-                                setState(() {
-                                  (selectedBoostIndex == 0)
-                                      ? selectedBoostIndex = 0
-                                      : selectedBoostIndex--;
-                                });
-                              },
-                              splashColor: Theme.of(context).splashColor,
-                              highlightColor: Colors.transparent,
-                              child: Icon(
-                                Icons.remove_circle_outline,
-                                size: 20,
-                                color: Theme.of(context)
-                                    .appBarTheme
-                                    .actionsIconTheme
-                                    .color,
-                              ),
-                            ),
-                          ))),
-                  Positioned(
-                    left: 24,
-                    top: 16,
-                    child: SizedBox(
-                      width: 42,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 32,
-                            height: 20,
-                            child: AutoSizeText(
-                              NumberFormat.compact().format(widget
-                                  .boostAmountList
-                                  .elementAt(selectedBoostIndex)),
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: 14.3,
-                                letterSpacing: 1,
-                                fontWeight: FontWeight.w600,
-                                height: 1.2,
-                              ),
-                              minFontSize: MinFontSize(context).minFontSize,
-                              stepGranularity: 0.1,
-                              maxLines: 1,
-                            ),
-                          ),
-                          AutoSizeText(
-                            "sats",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(fontSize: 10, letterSpacing: 1),
-                            minFontSize: MinFontSize(context).minFontSize,
-                            stepGranularity: 0.1,
-                          )
-                        ],
+    final userBloc = AppBlocsProvider.of<UserProfileBloc>(context);
+    return StreamBuilder<BreezUserModel>(
+        stream: userBloc.userStream,
+        builder: (context, snapshot) {
+          if (snapshot.data == null) {
+            return SizedBox();
+          }
+
+          final selectedIndex = widget.boostAmountList
+              .indexOf(widget.userModel.preferredBoostValue);
+
+          return Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 88,
+                child: FlatButton.icon(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6.0),
+                      side: BorderSide(
+                          color:
+                              Theme.of(context).brightness == Brightness.light
+                                  ? Color(0xFF0085fb)
+                                  : Colors.white70,
+                          width: 1.6)),
+                  icon: ImageIcon(
+                    AssetImage("src/icon/boost.png"),
+                    size: 20,
+                    color: Theme.of(context).appBarTheme.actionsIconTheme.color,
+                  ),
+                  label: Container(
+                    width: 44,
+                    child: AutoSizeText(
+                      "BOOST!",
+                      style: TextStyle(
+                        fontSize: 14,
+                        height: 1.2,
+                        letterSpacing: 0,
+                        fontWeight: FontWeight.w500,
+                        color: Theme.of(context).buttonColor,
                       ),
+                      minFontSize: MinFontSize(context).minFontSize,
+                      stepGranularity: 0.1,
+                      maxLines: 1,
                     ),
                   ),
-                  Positioned(
-                    left: 60,
-                    child: GestureDetector(
-                        child: Container(
-                            width: 32,
-                            height: 64,
-                            child: Material(
-                              color: Colors.transparent,
-                              borderRadius: BorderRadius.circular(32),
-                              child: InkWell(
-                                borderRadius: BorderRadius.circular(32),
-                                onTap: () {
-                                  setState(() {
-                                    (selectedBoostIndex ==
-                                            widget.boostAmountList.length - 1)
-                                        ? selectedBoostIndex =
-                                            widget.boostAmountList.length - 1
-                                        : selectedBoostIndex++;
-                                  });
-                                },
-                                splashColor: Theme.of(context).splashColor,
-                                highlightColor: Colors.transparent,
-                                child: Icon(
-                                  Icons.add_circle_outline,
-                                  size: 20,
-                                  color: Theme.of(context)
-                                      .appBarTheme
-                                      .actionsIconTheme
-                                      .color,
-                                ),
-                              ),
-                            ))),
-                  ),
-                ],
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                  onPressed: () {
+                    widget.onBoost(
+                        widget.boostAmountList.elementAt(selectedIndex));
+                  },
+                ),
               ),
-            ),
-          ),
-        ),
-      ],
-    );
+              Flexible(
+                fit: FlexFit.tight,
+                flex: 1,
+                child: Center(
+                  child: Container(
+                    width: 92,
+                    child: Stack(
+                      fit: StackFit.loose,
+                      children: [
+                        GestureDetector(
+                            child: Container(
+                                width: 32,
+                                height: 64,
+                                child: Material(
+                                  color: Colors.transparent,
+                                  borderRadius: BorderRadius.circular(32),
+                                  child: InkWell(
+                                    borderRadius: BorderRadius.circular(32),
+                                    onTap: () {
+                                      var nextIndex = (selectedIndex == 0)
+                                          ? selectedIndex
+                                          : selectedIndex - 1;
+                                      final nextAmount = widget.boostAmountList
+                                          .elementAt(nextIndex);
+                                      userBloc.userActionsSink
+                                          .add(SetBoostAmount(nextAmount));
+                                    },
+                                    splashColor: Theme.of(context).splashColor,
+                                    highlightColor: Colors.transparent,
+                                    child: Icon(
+                                      Icons.remove_circle_outline,
+                                      size: 20,
+                                      color: Theme.of(context)
+                                          .appBarTheme
+                                          .actionsIconTheme
+                                          .color,
+                                    ),
+                                  ),
+                                ))),
+                        Positioned(
+                          left: 24,
+                          top: 16,
+                          child: SizedBox(
+                            width: 42,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  width: 32,
+                                  height: 20,
+                                  child: AutoSizeText(
+                                    NumberFormat.compact().format(
+                                        snapshot.data.preferredBoostValue),
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 14.3,
+                                      letterSpacing: 1,
+                                      fontWeight: FontWeight.w600,
+                                      height: 1.2,
+                                    ),
+                                    minFontSize:
+                                        MinFontSize(context).minFontSize,
+                                    stepGranularity: 0.1,
+                                    maxLines: 1,
+                                  ),
+                                ),
+                                AutoSizeText(
+                                  "sats",
+                                  textAlign: TextAlign.center,
+                                  style:
+                                      TextStyle(fontSize: 10, letterSpacing: 1),
+                                  minFontSize: MinFontSize(context).minFontSize,
+                                  stepGranularity: 0.1,
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          left: 60,
+                          child: GestureDetector(
+                              child: Container(
+                                  width: 32,
+                                  height: 64,
+                                  child: Material(
+                                    color: Colors.transparent,
+                                    borderRadius: BorderRadius.circular(32),
+                                    child: InkWell(
+                                      borderRadius: BorderRadius.circular(32),
+                                      onTap: () {
+                                        var nextIndex = (selectedIndex >=
+                                                widget.boostAmountList.length -
+                                                    1)
+                                            ? selectedIndex
+                                            : selectedIndex + 1;
+                                        final nextAmount = widget
+                                            .boostAmountList
+                                            .elementAt(nextIndex);
+                                        userBloc.userActionsSink
+                                            .add(SetBoostAmount(nextAmount));
+                                      },
+                                      splashColor:
+                                          Theme.of(context).splashColor,
+                                      highlightColor: Colors.transparent,
+                                      child: Icon(
+                                        Icons.add_circle_outline,
+                                        size: 20,
+                                        color: Theme.of(context)
+                                            .appBarTheme
+                                            .actionsIconTheme
+                                            .color,
+                                      ),
+                                    ),
+                                  ))),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        });
   }
 }
