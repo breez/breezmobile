c0e8661423e1bc75e31d76711bbdf12aff340304
Roei Erez <roeierez@gmail.com>
2021-03-14 22:54:56 +0200

    fix path

--- .github/scripts/sign.sh
+++ .github/scripts/sign.sh
@@ -10,13 +10,13 @@ echo "after build"
 zip -r "symbols_$GITHUB_RUN_NUMBER.zip" ./build/Runner.xcarchive/dSYMs/Runner.app.dSYM
 export uploadCommand="put symbols_$GITHUB_RUN_NUMBER.zip"
 sftp builderfiles@packages.breez.technology:config/conf <<< $uploadCommand
-xcodebuild -quiet -exportArchive -archivePath $PWD/build/Runner.xcarchive -exportOptionsPlist ../.github/scripts//export-options.plist -exportPath $PWD/build/Runner.ipa
+xcodebuild -quiet -exportArchive -archivePath $PWD/build/Runner.xcarchive -exportOptionsPlist ../.github/scripts/export-options.plist -exportPath $PWD/build
 
-ls -lrt $PWD/build/Runner.ipa
+ls -lrt $PWD/build/
 echo "after archive"
 #upload to testflight
 altool="$(dirname "$(xcode-select -p)")/Developer/usr/bin/altool"
-ipa="$PWD/build/Runner.ipa/Runner.ipa"
+ipa="$PWD/build/Runner.ipa"
 export uploadCommand="put $ipa"
 sftp builderfiles@packages.breez.technology:config/conf <<< $uploadCommand
 "$altool" --upload-app --type ios --file "$ipa" --username $APP_USERNAME --password $APP_PASSWORD
