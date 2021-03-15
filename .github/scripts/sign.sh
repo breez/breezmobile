fab6be04f092d4c136498a58e172b5762bec5bbb
Roei Erez <roeierez@gmail.com>
2021-03-15 08:29:54 +0200

    start build number from 4000

--- .github/scripts/sign.sh
+++ .github/scripts/sign.sh
@@ -2,13 +2,14 @@
 set -eo pipefail
 
 pushd ios
+buildNumber=$(($GITHUB_RUN_NUMBER + 4000))
 GOOGLE_SIGN_IN_URL=$(/usr/libexec/PlistBuddy -c "Print :REVERSED_CLIENT_ID" Runner/GoogleService-Info.plist)
 /usr/libexec/PlistBuddy -c "Set :CFBundleURLTypes:0:CFBundleURLSchemes:0 $GOOGLE_SIGN_IN_URL" Runner/Info.plist
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $GITHUB_RUN_NUMBER" Runner/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" Runner/Info.plist
 xcodebuild -quiet -workspace Runner.xcworkspace -scheme Runner -sdk iphoneos -configuration Release archive -archivePath $PWD/build/Runner.xcarchive
 echo "after build"
-zip -r "symbols_$GITHUB_RUN_NUMBER.zip" ./build/Runner.xcarchive/dSYMs/Runner.app.dSYM
-export uploadCommand="put symbols_$GITHUB_RUN_NUMBER.zip"
+zip -r "symbols_$buildNumber.zip" ./build/Runner.xcarchive/dSYMs/Runner.app.dSYM
+export uploadCommand="put symbols_$buildNumber.zip"
 sftp builderfiles@packages.breez.technology:config/conf <<< $uploadCommand
 xcodebuild -quiet -exportArchive -archivePath $PWD/build/Runner.xcarchive -exportOptionsPlist ../.github/scripts/export-options.plist -exportPath $PWD/build
 
