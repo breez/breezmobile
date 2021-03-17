d21524ef1a9668fdb0f488d6af8e76e39aab5c3f
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2021-03-17 12:28:18 +0300

    Update anytime dependency
    Rebased breez-merge onto anytime/master

--- lib/routes/podcast/theme.dart
+++ lib/routes/podcast/theme.dart
@@ -108,6 +108,7 @@ Widget withPodcastTheme(BreezUserModel user, Widget child) {
               ),
               appBarTheme: currentTheme.appBarTheme.copyWith(
                 color: currentTheme.backgroundColor,
+                backgroundColor: Color.fromRGBO(5, 93, 235, 1.0),
                 foregroundColor: Colors.black,
                 textTheme: currentTheme.appBarTheme.textTheme.copyWith(
                     headline6: currentTheme.appBarTheme.textTheme.headline6
@@ -181,7 +182,7 @@ Widget withPodcastTheme(BreezUserModel user, Widget child) {
                     color: Color(0xff444444),
                   ),
               appBarTheme: currentTheme.appBarTheme.copyWith(
-                  //backgroundColor: Color(0xFF0c2031),
+                  backgroundColor: Color(0xFF0c2031),
                   color: currentTheme.backgroundColor),
             ),
   );
