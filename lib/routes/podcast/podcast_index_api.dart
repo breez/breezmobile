19256afbb3c7080a696d63998a3d557d61f050e0
Roei Erez <roeierez@gmail.com>
2021-04-06 12:49:27 +0300

    change API keys.

--- lib/routes/podcast/podcast_index_api.dart
+++ lib/routes/podcast/podcast_index_api.dart
@@ -52,8 +52,8 @@ class PodcastIndexAPI extends PodcastApi {
   static Future<SearchResult> _charts(int size) {
     return Search(userAgent: Environment.userAgent()).charts(
         searchProvider: PodcastIndexProvider(
-            key: 'XXWQEGULBJABVHZUM8NF',
-            secret: 'KZ2uy4upvq4t3e\$m\$3r2TeFS2fEpFTAaF92xcNdX'),
+            key: 'NBVJ9GPWMLPXJMFFD3KV',
+            secret: 'be7kR2CWeCFk4nkKCKV4XAX35y2eXrDeCPvs#v4F'),
         queryParams: {
           'val': 'lightning',
           'aponly': 'true'
