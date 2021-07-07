ab1149023c940427bec1f38bf684c4e29d36391e
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2021-07-07 12:17:41 +0300

    Open LNURL Withdraw screen with Breez theme

--- lib/handlers/lnurl_handler.dart
+++ lib/handlers/lnurl_handler.dart
@@ -1,6 +1,7 @@
 import 'package:breez/bloc/lnurl/lnurl_actions.dart';
 import 'package:breez/bloc/lnurl/lnurl_bloc.dart';
 import 'package:breez/bloc/lnurl/lnurl_model.dart';
+import 'package:breez/routes/podcast/theme.dart';
 import 'package:breez/routes/sync_progress_dialog.dart';
 import 'package:breez/widgets/error_dialog.dart';
 import 'package:breez/widgets/loader.dart';
@@ -50,7 +51,8 @@ class LNURLHandler {
       });
 
       Navigator.of(context).push(FadeInRoute(
-        builder: (_) => CreateInvoicePage(lnurlWithdraw: response),
+        builder: (_) =>
+            withBreezTheme(context, CreateInvoicePage(lnurlWithdraw: response)),
       ));
     } else if (response is AuthFetchResponse) {
       Navigator.popUntil(context, (route) {
