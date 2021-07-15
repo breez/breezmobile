97f1314b7f4c48a79e9c758d21054c138798d4ac
Roei Erez <roeierez@gmail.com>
2021-07-15 14:58:47 +0300

    fix lnurl success dialog

--- lib/lnurl_success_action_dialog.dart
+++ lib/lnurl_success_action_dialog.dart
@@ -1,7 +1,10 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:breez/services/breezlib/data/rpc.pbgrpc.dart';
+import 'package:breez/services/injector.dart';
 import 'package:breez/widgets/error_dialog.dart';
+import 'package:breez/widgets/flushbar.dart';
 import 'package:flutter/material.dart';
+import 'package:share_extend/share_extend.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void showLNURLSuccessAction(BuildContext context, SuccessAction sa) {
@@ -30,19 +33,10 @@ void showLNURLSuccessAction(BuildContext context, SuccessAction sa) {
                     ? SizedBox(
                         height: 0,
                       )
-                    : Container(
-                        height: 36.0,
-                        padding: EdgeInsets.only(top: 8.0),
-                        child: GestureDetector(
-                          onTap: () {
-                            launch(sa.url);
-                          },
-                          child: Text(
-                            sa.url,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(color: Colors.blue),
-                          ),
-                        ))
+                    : Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: _URLRow(sharedValue: sa.url),
+                      ),
               ])));
 }
 
@@ -72,5 +66,81 @@ class _TextMessage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class _URLRow extends StatelessWidget {
+  final String sharedValue;
+
+  const _URLRow({Key key, this.sharedValue}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _expansionTileTheme = Theme.of(context).copyWith(
+        unselectedWidgetColor: Theme.of(context).primaryTextTheme.button.color,
+        colorScheme: ColorScheme.dark(
+          secondary: Theme.of(context).primaryTextTheme.button.color,
+        ),
+        dividerColor: Theme.of(context).backgroundColor);
+    return Theme(
+        data: _expansionTileTheme,
+        child: Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.zero,
+                  child: GestureDetector(
+                    onTap: () => launch(sharedValue),
+                    child: Text('$sharedValue',
+                        textAlign: TextAlign.left,
+                        overflow: TextOverflow.clip,
+                        maxLines: 4,
+                        style: DefaultTextStyle.of(context)
+                            .style
+                            .copyWith(fontSize: 12, color: Colors.blue)),
+                  ),
+                ),
+              ),
+              Expanded(
+                  flex: 0,
+                  child: Padding(
+                      padding: EdgeInsets.zero,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          IconButton(
+                            alignment: Alignment.centerRight,
+                            padding: EdgeInsets.zero,
+                            iconSize: 16.0,
+                            color:
+                                Theme.of(context).primaryTextTheme.button.color,
+                            icon: Icon(
+                              IconData(0xe90b, fontFamily: 'icomoon'),
+                            ),
+                            onPressed: () {
+                              ServiceInjector()
+                                  .device
+                                  .setClipboardText(sharedValue);
+                              showFlushbar(context,
+                                  message: "link was copied to your clipboard.",
+                                  duration: Duration(seconds: 4));
+                            },
+                          ),
+                          IconButton(
+                            padding: EdgeInsets.zero,
+                            iconSize: 16.0,
+                            color:
+                                Theme.of(context).primaryTextTheme.button.color,
+                            icon: Icon(Icons.share),
+                            onPressed: () {
+                              ShareExtend.share(sharedValue, "text");
+                            },
+                          ),
+                        ],
+                      )))
+            ]));
   }
 }
