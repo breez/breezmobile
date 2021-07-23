98cb081c92b0bd676faa026128553b09b9f18feb
Ademar Alves de Oliveira <ademar111190@gmail.com>
2021-07-23 16:52:53 -0300

    Fix custom amount dialog

--- lib/routes/podcast/custom_amount_dialog.dart
+++ lib/routes/podcast/custom_amount_dialog.dart
@@ -1,4 +1,3 @@
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -41,7 +40,7 @@ class CustomAmountDialogState extends State<CustomAmountDialog> {
 
   Widget _buildPaymentRequestDialog() {
     return AlertDialog(
-      title: AutoSizeText(
+      title: Text(
         "Enter a Custom Amount:",
         style:
             Theme.of(context).dialogTheme.titleTextStyle.copyWith(fontSize: 16),
