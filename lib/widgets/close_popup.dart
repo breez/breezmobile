b80293786bed5737e0d9c6b920118d6b23e0f46f
Roei Erez <roeierez@gmail.com>
2021-08-10 09:29:39 +0300

    fix back from lock screen

--- lib/widgets/close_popup.dart
+++ lib/widgets/close_popup.dart
@@ -1,14 +1,22 @@
+import 'dart:io';
+
 import 'package:breez/widgets/error_dialog.dart';
 import 'package:flutter/widgets.dart';
 
 WillPopCallback willPopCallback(
   BuildContext context, {
+  bool immediateExit = false,
   String title: 'Exit Breez',
   String message: 'Do you really want to quit Breez?',
   Function canCancel,
 }) {
   return () async {
     if (canCancel != null && canCancel()) return true;
-    return promptAreYouSure(context, title, Text(message));
+    return promptAreYouSure(context, title, Text(message)).then((ok) {
+      if (ok && immediateExit) {
+        exit(0);
+      }
+      return ok;
+    });
   };
 }
