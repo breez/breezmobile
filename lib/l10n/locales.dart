1b7822b2a080357973b4a0a56085f2cd2570a220
Ademar Alves de Oliveira <ademar111190@gmail.com>
2021-09-22 08:50:25 -0300

    Add the initial support for translations

---
+++ lib/l10n/locales.dart
@@ -0,0 +1,22 @@
+import 'package:anytime/l10n/L.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+Iterable<LocalizationsDelegate<dynamic>> localizationsDelegates() {
+  return [
+    AppLocalizations.delegate,
+    const LocalisationsDelegate(),
+    GlobalMaterialLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+  ];
+}
+
+Iterable<Locale> supportedLocales() {
+  return [
+    const Locale('en', ''),
+    const Locale('de', ''),
+    const Locale('pt', ''),
+  ];
+}
