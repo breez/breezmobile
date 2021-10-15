5f041c8ed3e912a327ae1ff61cd501d759bee26d
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2021-10-15 12:40:52 +0300

    Use appropriate behaviors for hosted & entered lounge items on lounge list

--- lib/routes/lounge/lounge_item.dart
+++ lib/routes/lounge/lounge_item.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:breez/bloc/account/account_bloc.dart';
 import 'package:breez/bloc/blocs_provider.dart';
 import 'package:breez/bloc/lounge/actions.dart';
 import 'package:breez/bloc/lounge/bloc.dart';
+import 'package:breez/bloc/lounge/lounge_payments_bloc.dart';
 import 'package:breez/bloc/lounge/model.dart';
+import 'package:breez/bloc/user_profile/user_actions.dart';
 import 'package:breez/bloc/user_profile/user_profile_bloc.dart';
 import 'package:breez/theme_data.dart' as theme;
 import 'package:breez/widgets/flushbar.dart';
@@ -25,20 +28,39 @@ class LoungeItem extends StatefulWidget {
 
 class _LoungeItemState extends State<LoungeItem> {
   final GlobalKey _menuKey = new GlobalKey();
+  String currentLoungeID;
+  LoungePaymentsBloc paymentsBloc;
+  UserProfileBloc userProfileBloc;
+  bool _isInit = false;
+
+  @override
+  void didChangeDependencies() {
+    if (!_isInit) {
+      paymentsBloc = AppBlocsProvider.of<LoungePaymentsBloc>(context);
+      userProfileBloc = AppBlocsProvider.of<UserProfileBloc>(context);
+      _isInit = true;
+    }
+    super.didChangeDependencies();
+  }
 
   @override
   void initState() {
     super.initState();
 
-    JitsiMeet.addListener(JitsiMeetingListener(
-        onConferenceWillJoin: _onConferenceWillJoin,
-        onConferenceJoined: _onConferenceJoined,
-        onConferenceTerminated: _onConferenceTerminated,
-        onError: _onError));
+    JitsiMeet.addListener(
+      JitsiMeetingListener(
+        onError: _onError,
+        onBoost: _onBoost,
+        changeSatsPerMinute: _changeSatsPerMinute,
+        setCustomBoostAmount: _setCustomBoostValue,
+        setCustomSatsPerMinAmount: _setCustomSatsPerMinAmount,
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    LoungesBloc loungesBloc = AppBlocsProvider.of<LoungesBloc>(context);
     return Padding(
       padding: const EdgeInsets.only(bottom: 8, left: 8, right: 8),
       child: Column(
@@ -64,8 +86,6 @@ class _LoungeItemState extends State<LoungeItem> {
                   ),
                   onPressed: () {
                     Navigator.of(context).pop();
-                    LoungesBloc loungesBloc =
-                        AppBlocsProvider.of<LoungesBloc>(context);
                     DeleteLounge deleteLounge = DeleteLounge(widget.lounge.id);
                     loungesBloc.actionsSink.add(deleteLounge);
                     deleteLounge.future.then((_) {}, onError: (_) {
@@ -121,15 +141,16 @@ class _LoungeItemState extends State<LoungeItem> {
                       },
                     ),
                     Tooltip(
-                      message: "Join",
+                      message: "Enter",
                       child: IconButton(
                         icon: Icon(
                           Icons.open_in_new,
                           color: theme.BreezColors.blue[500],
                           size: 22,
                         ),
-                        onPressed: () =>
-                            _enterLounge(widget.lounge.loungeID, context),
+                        onPressed: () => widget.lounge.isHosted
+                            ? _hostLounge(widget.lounge.loungeID, context)
+                            : _enterLounge(widget.lounge.loungeID, loungesBloc),
                       ),
                     ),
                   ],
@@ -142,7 +163,7 @@ class _LoungeItemState extends State<LoungeItem> {
     );
   }
 
-  _enterLounge(String loungeID, BuildContext context) async {
+  _hostLounge(String loungeID, BuildContext context) async {
     var userProfileBloc = AppBlocsProvider.of<UserProfileBloc>(context);
     var accountBloc = AppBlocsProvider.of<AccountBloc>(context);
 
@@ -181,38 +202,140 @@ class _LoungeItemState extends State<LoungeItem> {
       };
 
     debugPrint("JitsiMeetingOptions: $options");
-    await JitsiMeet.joinMeeting(
-      options,
-      listener: JitsiMeetingListener(
-          onConferenceWillJoin: (message) {
-            debugPrint("${options.room} will join with message: $message");
-          },
-          onConferenceJoined: (message) {
-            debugPrint("${options.room} joined with message: $message");
-          },
-          onConferenceTerminated: (message) {
-            debugPrint("${options.room} terminated with message: $message");
-          },
-          genericListeners: [
-            JitsiGenericListener(
-                eventName: 'readyToClose',
-                callback: (dynamic message) {
-                  debugPrint("readyToClose callback");
-                }),
-          ]),
-    );
+    await JitsiMeet.joinMeeting(options);
   }
 
-  void _onConferenceWillJoin(message) {
-    debugPrint("_onConferenceWillJoin broadcasted with message: $message");
+  _enterLounge(String loungeID, LoungesBloc loungesBloc) async {
+    var userProfileBloc = AppBlocsProvider.of<UserProfileBloc>(context);
+    var user = await userProfileBloc.userStream.firstWhere((u) => u != null);
+
+    setState(() {
+      currentLoungeID = loungeID;
+    });
+    // Enable or disable any feature flag here
+    // If feature flag are not provided, default values will be used
+    // Full list of feature flags (and defaults) available in the README
+    Map<FeatureFlagEnum, bool> featureFlags = {
+      FeatureFlagEnum.WELCOME_PAGE_ENABLED: false,
+    };
+    if (!kIsWeb) {
+      // Here is an example, disabling features for each platform
+      if (Platform.isAndroid) {
+        // Disable ConnectionService usage on Android to avoid issues (see README)
+        featureFlags[FeatureFlagEnum.CALL_INTEGRATION_ENABLED] = false;
+      } else if (Platform.isIOS) {
+        // Disable PIP on iOS as it looks weird
+        featureFlags[FeatureFlagEnum.PIP_ENABLED] = false;
+      }
+    }
+
+    String paymentOptions = jsonEncode({
+      "presetBoostAmountsList":
+          user.loungePaymentOptions.presetBoostAmountsList,
+      "presetSatsPerMinuteAmountsList":
+          user.loungePaymentOptions.presetSatsPerMinuteAmountsList,
+      "customBoostValue": user.loungePaymentOptions.customBoostValue,
+      "customSatsPerMinAmountValue":
+          user.loungePaymentOptions.customSatsPerMinValue,
+      "selectedBoostAmountIndex": user.loungePaymentOptions.boostAmountList
+          .indexOf(user.loungePaymentOptions.preferredBoostValue),
+      "selectedSatsPerMinuteAmountIndex": user
+          .loungePaymentOptions.satsPerMinuteIntervalsList
+          .indexOf(user.loungePaymentOptions.preferredSatsPerMinValue),
+    });
+
+    // Define meetings options here
+    var options = JitsiMeetingOptions(room: loungeID)
+      ..userDisplayName = user.name
+      ..userEmail = ""
+      ..audioMuted = true
+      ..videoMuted = true
+      ..featureFlags.addAll(featureFlags)
+      ..paymentOptions = paymentOptions
+      ..isLightTheme = theme.themeId == "BLUE"
+      ..webOptions = {
+        "roomName": loungeID,
+        "width": "100%",
+        "height": "100%",
+        "enableWelcomePage": false,
+        "chromeExtensionBanner": null,
+        "userInfo": {"displayName": user.name}
+      };
+
+    debugPrint("JitsiMeetingOptions: $options");
+    try {
+      await JitsiMeet.joinMeeting(
+        options,
+        listener: JitsiMeetingListener(
+            onBoost: (message) {
+              debugPrint("Called onBoost with message: $message");
+            },
+            changeSatsPerMinute: (message) {
+              debugPrint("Called changeSatsPerMinute with message: $message");
+            },
+            setCustomBoostAmount: (message) {
+              debugPrint("Called setCustomBoostAmount");
+            },
+            setCustomSatsPerMinAmount: (message) {
+              debugPrint("Called setCustomSatsPerMinAmount");
+            },
+            genericListeners: [
+              JitsiGenericListener(
+                  eventName: 'readyToClose',
+                  callback: (dynamic message) {
+                    debugPrint("readyToClose callback");
+                  }),
+            ]),
+      ).then((value) async {
+        List<Lounge> lounges =
+            await loungesBloc.loungesStream.firstWhere((l) => l != null);
+        if (lounges.firstWhere((lounge) => (lounge.loungeID == loungeID),
+                orElse: () => null) ==
+            null) {
+          AddLounge addLounge = AddLounge(
+            Lounge(loungeID: loungeID, title: loungeID, isHosted: false),
+          );
+
+          loungesBloc.actionsSink.add(addLounge);
+          addLounge.future.then((_) {});
+        }
+      });
+    } catch (e) {
+      showFlushbar(context, message: e.message.toString());
+    }
   }
 
-  void _onConferenceJoined(message) {
-    debugPrint("_onConferenceJoined broadcasted with message: $message");
+  void _onBoost(message) {
+    var boostAmount = double.parse(message["boostAmount"]).toInt();
+    var paymentInfo = message["paymentInfo"];
+    paymentsBloc.actionsSink
+        .add(PayBoost(boostAmount, "", currentLoungeID, paymentInfo));
   }
 
-  void _onConferenceTerminated(message) {
-    debugPrint("_onConferenceTerminated broadcasted with message: $message");
+  void _changeSatsPerMinute(message) async {
+    var user = await userProfileBloc.userStream.firstWhere((u) => u != null);
+    var satsPerMinute = double.parse(message["satsPerMinute"]).toInt();
+    paymentsBloc.actionsSink.add(AdjustAmount(satsPerMinute));
+    userProfileBloc.userActionsSink.add(SetLoungePaymentOptions(user
+        .loungePaymentOptions
+        .copyWith(preferredSatsPerMinValue: satsPerMinute)));
+  }
+
+  void _setCustomBoostValue(message) async {
+    var user = await userProfileBloc.userStream.firstWhere((u) => u != null);
+    var customBoostValue = double.parse(message["customBoostValue"]).toInt();
+    userProfileBloc.userActionsSink.add(SetLoungePaymentOptions(user
+        .loungePaymentOptions
+        .copyWith(customBoostValue: customBoostValue)));
+  }
+
+  void _setCustomSatsPerMinAmount(message) async {
+    var user = await userProfileBloc.userStream.firstWhere((u) => u != null);
+    var customSatsPerMinValue =
+        double.parse(message["customSatsPerMinValue"]).toInt();
+    userProfileBloc.userActionsSink.add(SetLoungePaymentOptions(user
+        .loungePaymentOptions
+        .copyWith(customSatsPerMinValue: customSatsPerMinValue)));
   }
 
   _onError(error) {
