70ba45ef4bc5c557e08161c779b00089db44af51
Ademar Alves de Oliveira <ademar111190@gmail.com>
2022-01-05 10:46:37 -0300

    Translate sync popup

--- lib/widgets/animated_loader_dialog.dart
+++ lib/widgets/animated_loader_dialog.dart
@@ -1,18 +1,26 @@
-import 'package:flutter/material.dart';
 import 'package:breez/theme_data.dart' as theme;
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 import 'loading_animated_text.dart';
 
-AlertDialog createAnimatedLoaderDialog(BuildContext context, String text,
-    {bool withOKButton = true}) {
+AlertDialog createAnimatedLoaderDialog(
+  BuildContext context,
+  String text, {
+  bool withOKButton = true,
+}) {
+  final themeData = Theme.of(context);
+  final texts = AppLocalizations.of(context);
+
   return AlertDialog(
     contentPadding: EdgeInsets.only(left: 24.0, right: 24.0, top: 24.0),
     content: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
+      children: [
         LoadingAnimatedText(
           text,
-          textStyle: Theme.of(context).dialogTheme.contentTextStyle,
+          textStyle: themeData.dialogTheme.contentTextStyle,
           textAlign: TextAlign.center,
         ),
         Image.asset(
@@ -26,17 +34,17 @@ AlertDialog createAnimatedLoaderDialog(BuildContext context, String text,
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.max,
           children: withOKButton
-              ? <Widget>[
+              ? [
                   TextButton(
-                    child: Text('OK',
-                        style: Theme.of(context).primaryTextTheme.button),
-                    onPressed: () {
-                      Navigator.pop(context, false);
-                    },
-                  )
+                    child: Text(
+                      texts.backup_in_progress_action_confirm,
+                      style: themeData.primaryTextTheme.button,
+                    ),
+                    onPressed: () => Navigator.pop(context, false),
+                  ),
                 ]
               : [],
-        )
+        ),
       ],
     ),
   );
