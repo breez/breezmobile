b6e5a44abfec17b7ca84f3eb90b72ba939dfd236
Roei Erez <roeierez@gmail.com>
2022-01-11 14:27:32 +0200

    omit symbols upload

--- .github/scripts/sign.sh
+++ .github/scripts/sign.sh
@@ -10,7 +10,7 @@ xcodebuild -quiet -workspace Runner.xcworkspace -scheme Runner -sdk iphoneos -co
 echo "after build"
 zip -r "symbols_$buildNumber.zip" ./build/Runner.xcarchive/dSYMs/Runner.app.dSYM
 export uploadCommand="put symbols_$buildNumber.zip"
-sftp builderfiles@packages.breez.technology:config/conf <<< $uploadCommand
+#sftp builderfiles@packages.breez.technology:config/conf <<< $uploadCommand
 xcodebuild -quiet -exportArchive -archivePath $PWD/build/Runner.xcarchive -exportOptionsPlist ../.github/scripts/export-options.plist -exportPath $PWD/build/Runner.app
 
 ls -lrt $PWD/build/Runner.app
