595e08588a4f5f2edfee3db66f817f20afba337e
Ademar Alves de Oliveira <ademar111190@gmail.com>
2022-01-21 12:28:44 -0300

    Translate payment confirmation dialog

--- lib/widgets/payment_confirmation_dialog.dart
+++ lib/widgets/payment_confirmation_dialog.dart
@@ -4,10 +4,10 @@ import 'package:breez/bloc/account/account_bloc.dart';
 import 'package:breez/bloc/account/account_model.dart';
 import 'package:breez/bloc/invoice/invoice_model.dart';
 import 'package:fixnum/fixnum.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class PaymentConfirmationDialog extends StatefulWidget {
+class PaymentConfirmationDialog extends StatelessWidget {
   final AccountBloc accountBloc;
   final PaymentRequestModel invoice;
   final Int64 _amountToPay;
@@ -16,112 +16,135 @@ class PaymentConfirmationDialog extends StatefulWidget {
   final Function(SendPayment payment) _onPaymentApproved;
   final double minHeight;
 
-  PaymentConfirmationDialog(this.accountBloc, this.invoice, this._amountToPay,
-      this._amountToPayStr, this._onCancel, this._onPaymentApproved, this.minHeight);
+  const PaymentConfirmationDialog(
+    this.accountBloc,
+    this.invoice,
+    this._amountToPay,
+    this._amountToPayStr,
+    this._onCancel,
+    this._onPaymentApproved,
+    this.minHeight,
+  );
 
-  @override
-  PaymentConfirmationDialogState createState() {
-    return PaymentConfirmationDialogState();
-  }
-}
-
-class PaymentConfirmationDialogState extends State<PaymentConfirmationDialog> {
   @override
   Widget build(BuildContext context) {
     return Dialog(
       child: Container(
-        constraints: BoxConstraints(minHeight: widget.minHeight),
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              mainAxisSize: MainAxisSize.min,
-              children: _buildConfirmationDialog())),
+        constraints: BoxConstraints(minHeight: minHeight),
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.min,
+          children: _buildConfirmationDialog(context),
+        ),
+      ),
     );
   }
 
-  List<Widget> _buildConfirmationDialog() {
-    List<Widget> _confirmationDialog = <Widget>[];
-    _confirmationDialog.add(_buildTitle());
-    _confirmationDialog.add(_buildContent());
-    _confirmationDialog.add(_buildActions());
-    return _confirmationDialog;
+  List<Widget> _buildConfirmationDialog(BuildContext context) {
+    return [
+      _buildTitle(context),
+      _buildContent(context),
+      _buildActions(context),
+    ];
   }
 
-  Container _buildTitle() {
+  Container _buildTitle(BuildContext context) {
+    final themeData = Theme.of(context);
+    final texts = AppLocalizations.of(context);
+
     return Container(
       height: 64.0,
       padding: EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 8.0),
       child: Text(
-        "Payment Confirmation",
-        style: Theme.of(context).dialogTheme.titleTextStyle,
+        texts.payment_confirmation_dialog_title,
+        style: themeData.dialogTheme.titleTextStyle,
         textAlign: TextAlign.center,
       ),
     );
   }
 
-  Widget _buildContent() {
+  Widget _buildContent(BuildContext context) {
+    final themeData = Theme.of(context);
+    final queryData = MediaQuery.of(context);
+    final texts = AppLocalizations.of(context);
+
     return Container(
       child: Padding(
         padding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
         child: Container(
-          width: MediaQuery.of(context).size.width,
+          width: queryData.size.width,
           child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Text(
-                  'Are you sure you want to pay',
-                  style: Theme.of(context).dialogTheme.contentTextStyle,
-                  textAlign: TextAlign.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                texts.payment_confirmation_dialog_confirmation,
+                style: themeData.dialogTheme.contentTextStyle,
+                textAlign: TextAlign.center,
+              ),
+              AutoSizeText.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: _amountToPayStr,
+                      style: themeData.dialogTheme.contentTextStyle.copyWith(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextSpan(
+                      text: texts.payment_confirmation_dialog_confirmation_end,
+                    )
+                  ],
                 ),
-                AutoSizeText.rich(
-                    TextSpan(children: <TextSpan>[
-                      TextSpan(
-                          text: widget._amountToPayStr,
-                          style: Theme.of(context)
-                              .dialogTheme
-                              .contentTextStyle
-                              .copyWith(
-                                  fontSize: 20.0, fontWeight: FontWeight.bold)),
-                      TextSpan(text: " ?")
-                    ]),
-                    maxLines: 2,
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).dialogTheme.contentTextStyle),
-              ]),
+                maxLines: 2,
+                textAlign: TextAlign.center,
+                style: themeData.dialogTheme.contentTextStyle,
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Container _buildActions() {
-    List<Widget> children = <Widget>[
+  Container _buildActions(BuildContext context) {
+    final themeData = Theme.of(context);
+    final texts = AppLocalizations.of(context);
+
+    List<Widget> children = [
       TextButton(
         style: ButtonStyle(
-          overlayColor: MaterialStateProperty.resolveWith<Color>(
-                  (Set<MaterialState> states) {
-                if (states.contains(MaterialState.pressed))
-                  return Colors.transparent;
-                return null; // Defer to the widget's default.
-              }),
+          overlayColor: MaterialStateProperty.resolveWith<Color>((states) {
+            if (states.contains(MaterialState.pressed))
+              return Colors.transparent;
+            return null; // Defer to the widget's default.
+          }),
         ),
-        child: Text("NO", style: Theme.of(context).primaryTextTheme.button),
-        onPressed: () =>
-            widget._onCancel(),
+        child: Text(
+          texts.payment_confirmation_dialog_action_no,
+          style: themeData.primaryTextTheme.button,
+        ),
+        onPressed: () => _onCancel(),
       ),
       TextButton(
         style: ButtonStyle(
-          overlayColor: MaterialStateProperty.resolveWith<Color>(
-                  (Set<MaterialState> states) {
-                if (states.contains(MaterialState.pressed))
-                  return Colors.transparent;
-                return null; // Defer to the widget's default.
-              }),
+          overlayColor: MaterialStateProperty.resolveWith<Color>((states) {
+            if (states.contains(MaterialState.pressed))
+              return Colors.transparent;
+            return null; // Defer to the widget's default.
+          }),
         ),
-        child: Text("YES", style: Theme.of(context).primaryTextTheme.button),
+        child: Text(
+          texts.payment_confirmation_dialog_action_yes,
+          style: themeData.primaryTextTheme.button,
+        ),
         onPressed: () {
-          widget._onPaymentApproved(SendPayment(
-              PayRequest(widget.invoice.rawPayReq, widget._amountToPay)));
+          _onPaymentApproved(SendPayment(PayRequest(
+            invoice.rawPayReq,
+            _amountToPay,
+          )));
         },
       ),
     ];
