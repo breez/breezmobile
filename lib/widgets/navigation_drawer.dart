544b3966b74263130c2c6f938646177000880b48
Ademar Alves de Oliveira <ademar111190@gmail.com>
2022-01-21 15:13:17 -0300

    Translate home drawer edge case errors

--- lib/widgets/navigation_drawer.dart
+++ lib/widgets/navigation_drawer.dart
@@ -11,6 +11,7 @@ import 'package:breez/widgets/breez_avatar_dialog.dart';
 import 'package:breez/widgets/breez_drawer_header.dart';
 import 'package:breez/widgets/error_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class DrawerItemConfig {
   final GlobalKey key;
@@ -22,12 +23,16 @@ class DrawerItemConfig {
   final Widget switchWidget;
   final bool isSelected;
 
-  DrawerItemConfig(this.name, this.title, this.icon,
-      {this.key,
-      this.onItemSelected,
-      this.disabled = false,
-      this.switchWidget,
-      this.isSelected = false});
+  const DrawerItemConfig(
+    this.name,
+    this.title,
+    this.icon, {
+    this.key,
+    this.onItemSelected,
+    this.disabled = false,
+    this.switchWidget,
+    this.isSelected = false,
+  });
 }
 
 class DrawerItemConfigGroup {
@@ -36,66 +41,87 @@ class DrawerItemConfigGroup {
   final String groupAssetImage;
   final bool withDivider;
 
-  DrawerItemConfigGroup(this.items,
-      {this.groupTitle, this.groupAssetImage, this.withDivider = true});
+  const DrawerItemConfigGroup(
+    this.items, {
+    this.groupTitle,
+    this.groupAssetImage,
+    this.withDivider = true,
+  });
 }
 
 class NavigationDrawer extends StatelessWidget {
   final bool _avatar;
   final List<DrawerItemConfigGroup> _drawerGroupedItems;
   final void Function(String screenName) _onItemSelected;
+  final _scrollController = ScrollController();
 
   NavigationDrawer(
-      this._avatar, this._drawerGroupedItems, this._onItemSelected);
-
-  final ScrollController _scrollController = ScrollController();
+    this._avatar,
+    this._drawerGroupedItems,
+    this._onItemSelected,
+  );
 
   @override
   Widget build(BuildContext context) {
-    UserProfileBloc userProfileBloc =
-        AppBlocsProvider.of<UserProfileBloc>(context);
+    final themeData = Theme.of(context);
+    final userProfileBloc = AppBlocsProvider.of<UserProfileBloc>(context);
 
-    List<Widget> children = <Widget>[];
-    children.insert(0, Padding(padding: EdgeInsets.only(top: 16)));
+    List<Widget> children = [
+      _breezDrawerHeader(userProfileBloc, _avatar),
+      Padding(padding: EdgeInsets.only(top: 16)),
+    ];
     _drawerGroupedItems.forEach((groupItems) {
       children.addAll(_createDrawerGroupWidgets(
-          groupItems, context, _drawerGroupedItems.indexOf(groupItems),
-          withDivider: children.length > 0 && groupItems.withDivider));
+        groupItems,
+        context,
+        _drawerGroupedItems.indexOf(groupItems),
+        withDivider: children.length > 0 && groupItems.withDivider,
+      ));
     });
 
-    children.insert(0, _breezDrawerHeader(userProfileBloc, _avatar));
-
     return Theme(
-      data: Theme.of(context).copyWith(
-          canvasColor: theme.customData[theme.themeId].navigationDrawerBgColor),
+      data: themeData.copyWith(
+        canvasColor: theme.customData[theme.themeId].navigationDrawerBgColor,
+      ),
       child: Drawer(
-          child: ListView(
-              controller: _scrollController,
-              // Important: Remove any padding from the ListView.
-              padding: EdgeInsets.only(bottom: 20.0),
-              children: children)),
+        child: ListView(
+          controller: _scrollController,
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.only(bottom: 20.0),
+          children: children,
+        ),
+      ),
     );
   }
 
   List<Widget> _createDrawerGroupWidgets(
-      DrawerItemConfigGroup group, BuildContext context, int index,
-      {bool withDivider = false}) {
+    DrawerItemConfigGroup group,
+    BuildContext context,
+    int index, {
+    bool withDivider = false,
+  }) {
     List<Widget> groupItems = group.items
         .map((action) => _actionTile(
-            action, context, action.onItemSelected ?? _onItemSelected))
+              action,
+              context,
+              action.onItemSelected ?? _onItemSelected,
+            ))
         .toList();
     if (group.groupTitle != null && groupItems.length > 0) {
       groupItems = group.items
           .map((action) => _actionTile(
-              action, context, action.onItemSelected ?? _onItemSelected,
-              subTile: true))
+                action,
+                context,
+                action.onItemSelected ?? _onItemSelected,
+                subTile: true,
+              ))
           .toList();
-      groupItems = <Widget>[]
-        ..add(_ExpansionTile(
-            items: groupItems,
-            title: group.groupTitle,
-            icon: AssetImage(group.groupAssetImage),
-            controller: _scrollController));
+      groupItems = []..add(_ExpansionTile(
+          items: groupItems,
+          title: group.groupTitle,
+          icon: AssetImage(group.groupAssetImage),
+          controller: _scrollController,
+        ));
     }
 
     if (groupItems.length > 0 && withDivider && index != 0) {
@@ -109,7 +135,9 @@ class _ListDivider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsets.only(left: 8.0, right: 8.0), child: Divider());
+      padding: EdgeInsets.only(left: 8.0, right: 8.0),
+      child: Divider(),
+    );
   }
 }
 
@@ -124,45 +152,54 @@ Widget _breezDrawerHeader(UserProfileBloc user, bool drawAvatar) {
 }
 
 StreamBuilder<BreezUserModel> _buildDrawerHeaderContent(
-    UserProfileBloc user, bool drawAvatar) {
+  UserProfileBloc user,
+  bool drawAvatar,
+) {
   return StreamBuilder<BreezUserModel>(
-      stream: user.userStream,
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return Container();
-        } else {
-          List<Widget> drawerHeaderContent = <Widget>[];
-          drawerHeaderContent.add(_buildThemeSwitch(snapshot, user, context));
-          if (drawAvatar) {
-            drawerHeaderContent
-              ..add(_buildAvatarButton(snapshot))
-              ..add(_buildBottomRow(snapshot, context));
-          }
-          return GestureDetector(
-            onTap: drawAvatar
-                ? () {
-                    showDialog<bool>(
-                      useRootNavigator: false,
-                      context: context,
-                      barrierDismissible: false,
-                      builder: (context) => breezAvatarDialog(context, user),
-                    );
-                  }
-                : null,
-            child: Column(children: drawerHeaderContent),
-          );
+    stream: user.userStream,
+    builder: (context, snapshot) {
+      if (!snapshot.hasData) {
+        return Container();
+      } else {
+        List<Widget> drawerHeaderContent = [];
+        drawerHeaderContent.add(_buildThemeSwitch(snapshot, user, context));
+        if (drawAvatar) {
+          drawerHeaderContent
+            ..add(_buildAvatarButton(snapshot))
+            ..add(_buildBottomRow(snapshot, context));
         }
-      });
+        return GestureDetector(
+          onTap: drawAvatar
+              ? () {
+                  showDialog<bool>(
+                    useRootNavigator: false,
+                    context: context,
+                    barrierDismissible: false,
+                    builder: (context) => breezAvatarDialog(context, user),
+                  );
+                }
+              : null,
+          child: Column(children: drawerHeaderContent),
+        );
+      }
+    },
+  );
 }
 
-GestureDetector _buildThemeSwitch(AsyncSnapshot<BreezUserModel> snapshot,
-    UserProfileBloc user, BuildContext context) {
+GestureDetector _buildThemeSwitch(
+  AsyncSnapshot<BreezUserModel> snapshot,
+  UserProfileBloc user,
+  BuildContext context,
+) {
   return GestureDetector(
     onTap: () => _changeTheme(
-        snapshot.data.themeId == "BLUE" ? "DARK" : "BLUE", user, context),
+      snapshot.data.themeId == "BLUE" ? "DARK" : "BLUE",
+      user,
+      context,
+    ),
     child: Row(
       mainAxisAlignment: MainAxisAlignment.end,
-      children: <Widget>[
+      children: [
         Padding(
           padding: EdgeInsets.only(
             top: 10,
@@ -172,9 +209,11 @@ GestureDetector _buildThemeSwitch(AsyncSnapshot<BreezUserModel> snapshot,
             width: 64,
             padding: EdgeInsets.all(4),
             decoration: ShapeDecoration(
-                shape: StadiumBorder(), color: theme.marketplaceButtonColor),
+              shape: StadiumBorder(),
+              color: theme.marketplaceButtonColor,
+            ),
             child: Row(
-              children: <Widget>[
+              children: [
                 Image.asset(
                   "src/icon/ic_lightmode.png",
                   height: 24,
@@ -190,11 +229,13 @@ GestureDetector _buildThemeSwitch(AsyncSnapshot<BreezUserModel> snapshot,
                     color: Colors.white30,
                   ),
                 ),
-                ImageIcon(AssetImage("src/icon/ic_darkmode.png"),
-                    color: snapshot.data.themeId == "DARK"
-                        ? Colors.white
-                        : Colors.white30,
-                    size: 24.0),
+                ImageIcon(
+                  AssetImage("src/icon/ic_darkmode.png"),
+                  color: snapshot.data.themeId == "DARK"
+                      ? Colors.white
+                      : Colors.white30,
+                  size: 24.0,
+                ),
               ],
             ),
           ),
@@ -204,78 +245,101 @@ GestureDetector _buildThemeSwitch(AsyncSnapshot<BreezUserModel> snapshot,
   );
 }
 
-Future _changeTheme(String themeId, UserProfileBloc userProfileBloc,
-    BuildContext context) async {
+Future _changeTheme(
+  String themeId,
+  UserProfileBloc userProfileBloc,
+  BuildContext context,
+) async {
+  final themeData = Theme.of(context);
+  final texts = AppLocalizations.of(context);
+
   var action = ChangeTheme(themeId);
   userProfileBloc.userActionsSink.add(action);
   action.future.then((_) {}).catchError((err) {
     promptError(
-        context,
-        "Internal Error",
-        Text(
-          err.toString(),
-          style: Theme.of(context).dialogTheme.contentTextStyle,
-        ));
+      context,
+      texts.home_drawer_error_internal,
+      Text(
+        err.toString(),
+        style: themeData.dialogTheme.contentTextStyle,
+      ),
+    );
   });
 }
 
 Row _buildAvatarButton(AsyncSnapshot<BreezUserModel> snapshot) {
   return Row(
-    children: <Widget>[
+    children: [
       BreezAvatar(snapshot.data.avatarURL, radius: 24.0),
     ],
   );
 }
 
 Row _buildBottomRow(
-    AsyncSnapshot<BreezUserModel> snapshot, BuildContext context) {
+  AsyncSnapshot<BreezUserModel> snapshot,
+  BuildContext context,
+) {
   return Row(
     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    children: <Widget>[
-      _buildUsername(snapshot),
+    children: [
+      _buildUsername(context, snapshot),
     ],
   );
 }
 
-Padding _buildUsername(AsyncSnapshot<BreezUserModel> snapshot) {
+Padding _buildUsername(
+  BuildContext context,
+  AsyncSnapshot<BreezUserModel> snapshot,
+) {
+  final texts = AppLocalizations.of(context);
+
   return Padding(
     padding: EdgeInsets.only(top: 8.0),
     child: AutoSizeText(
-      snapshot.data.name ?? "No Name",
+      snapshot.data.name ?? texts.home_drawer_error_no_name,
       style: theme.navigationDrawerHandleStyle,
     ),
   );
 }
 
 Widget _actionTile(
-    DrawerItemConfig action, BuildContext context, Function onItemSelected,
-    {bool subTile}) {
+  DrawerItemConfig action,
+  BuildContext context,
+  Function onItemSelected, {
+  bool subTile,
+}) {
+  final themeData = Theme.of(context);
   TextStyle itemStyle = theme.drawerItemTextStyle;
+
   Color color;
-  //DefaultTextStyle.of(context).style.color;
   if (action.disabled) {
-    color = Theme.of(context).disabledColor;
+    color = themeData.disabledColor;
     itemStyle = itemStyle.copyWith(color: color);
   }
   return Padding(
-    padding: EdgeInsets.only(left: 0.0, right: subTile != null ? 0.0 : 16.0),
-    // Check flutter issue: "List Tile Does Not Respect shape when tileColor is provided #63877"
+    padding: EdgeInsets.only(
+      left: 0.0,
+      right: subTile != null ? 0.0 : 16.0,
+    ),
     child: Ink(
       decoration: subTile != null
           ? null
           : BoxDecoration(
               color: action.isSelected
-                  ? Theme.of(context).primaryColorLight
+                  ? themeData.primaryColorLight
                   : Colors.transparent,
-              borderRadius: BorderRadius.horizontal(right: Radius.circular(32)),
+              borderRadius: BorderRadius.horizontal(
+                right: Radius.circular(32),
+              ),
             ),
       child: ListTile(
         key: action.key,
         shape: subTile != null
             ? null
             : RoundedRectangleBorder(
-                borderRadius:
-                    BorderRadius.horizontal(right: Radius.circular(32)),
+                borderRadius: BorderRadius.horizontal(
+                  right: Radius.circular(32),
+                ),
               ),
         leading: Padding(
           padding: subTile != null
@@ -289,7 +353,10 @@ Widget _actionTile(
         ),
         title: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Text(action.title, style: itemStyle),
+          child: Text(
+            action.title,
+            style: itemStyle,
+          ),
         ),
         trailing: action.switchWidget,
         onTap: action.disabled
@@ -309,54 +376,66 @@ class _ExpansionTile extends StatelessWidget {
   final AssetImage icon;
   final ScrollController controller;
 
-  const _ExpansionTile(
-      {Key key, this.items, this.title, this.icon, this.controller})
-      : super(key: key);
+  const _ExpansionTile({
+    Key key,
+    this.items,
+    this.title,
+    this.icon,
+    this.controller,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final _expansionTileTheme =
-        Theme.of(context).copyWith(dividerColor: Theme.of(context).canvasColor);
+    final themeData = Theme.of(context);
+    final _expansionTileTheme = themeData.copyWith(
+      dividerColor: themeData.canvasColor,
+    );
     return Theme(
-        data: _expansionTileTheme,
-        child: ExpansionTile(
-          title: Padding(
-            padding: EdgeInsets.only(left: 8.0, right: 8.0),
-            child: icon.assetName == ""
-                ? null
-                : Text(
-                    title,
-                    style: theme.drawerItemTextStyle,
+      data: _expansionTileTheme,
+      child: ExpansionTile(
+        title: Padding(
+          padding: EdgeInsets.only(left: 8.0, right: 8.0),
+          child: icon.assetName == ""
+              ? null
+              : Text(
+                  title,
+                  style: theme.drawerItemTextStyle,
+                ),
+        ),
+        leading: Padding(
+          padding: EdgeInsets.only(left: 8.0),
+          child: icon.assetName == ""
+              ? Text(
+                  title,
+                  style: theme.drawerItemTextStyle.copyWith(
+                    fontWeight: FontWeight.w500,
                   ),
-          ),
-          leading: Padding(
-            padding: EdgeInsets.only(left: 8.0),
-            child: icon.assetName == ""
-                ? Text(
-                    title,
-                    style: theme.drawerItemTextStyle
-                        .copyWith(fontWeight: FontWeight.w500),
-                  )
-                : ImageIcon(
-                    icon,
-                    size: 26.0,
-                    color: Colors.white,
-                  ),
-          ),
-          children: items
-              .map((item) =>
-                  Padding(padding: EdgeInsets.only(left: 0.0), child: item))
-              .toList(),
-          onExpansionChanged: (isExpanded) {
-            if (isExpanded)
-              Timer(
-                  Duration(milliseconds: 200),
-                  () => controller.animateTo(
-                      controller.position.maxScrollExtent + 28.0,
-                      duration: Duration(milliseconds: 400),
-                      curve: Curves.ease));
-            // 28 = bottom padding of list + intrinsic bottom padding
-          },
-        ));
+                )
+              : ImageIcon(
+                  icon,
+                  size: 26.0,
+                  color: Colors.white,
+                ),
+        ),
+        children: items
+            .map((item) => Padding(
+                  padding: EdgeInsets.only(left: 0.0),
+                  child: item,
+                ))
+            .toList(),
+        onExpansionChanged: (isExpanded) {
+          if (isExpanded)
+            Timer(
+              Duration(milliseconds: 200),
+              () => controller.animateTo(
+                controller.position.maxScrollExtent + 28.0,
+                duration: Duration(milliseconds: 400),
+                curve: Curves.ease,
+              ),
+            );
+          // 28 = bottom padding of list + intrinsic bottom padding
+        },
+      ),
+    );
   }
 }
