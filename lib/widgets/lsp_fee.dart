3bb923e72dc0f588bf2d87e9c0c3eb0e533cbc72
Ademar Alves de Oliveira <ademar111190@gmail.com>
2022-01-21 16:08:29 -0300

    Translate lsp fee dialog

--- lib/widgets/lsp_fee.dart
+++ lib/widgets/lsp_fee.dart
@@ -1,22 +1,32 @@
 import 'package:breez/bloc/account/account_model.dart';
 import 'package:breez/bloc/lsp/lsp_model.dart';
 import 'package:breez/routes/home/moonpay_route.dart';
-import 'package:flutter/material.dart';
 import 'package:fixnum/fixnum.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'error_dialog.dart';
 
 promptLSPFeeAndNavigate(
-    BuildContext context, AccountModel account, LSPInfo lsp, String route) {
-  var navigator = Navigator.of(context);
+  BuildContext context,
+  AccountModel account,
+  LSPInfo lsp,
+  String route,
+) {
+  final themeData = Theme.of(context);
+  final navigator = Navigator.of(context);
+  final texts = AppLocalizations.of(context);
+
   promptAreYouSure(
-          context,
-          "Setup Fee",
-          Text(_formatFeeMessage(account, lsp),
-              style: Theme.of(context).dialogTheme.contentTextStyle),
-          cancelText: "CANCEL",
-          okText: "OK")
-      .then((ok) {
+    context,
+    texts.lsp_fee_warning_title,
+    Text(
+      _formatFeeMessage(context, account, lsp),
+      style: themeData.dialogTheme.contentTextStyle,
+    ),
+    cancelText: texts.lsp_fee_warning_action_cancel,
+    okText: texts.lsp_fee_warning_action_ok,
+  ).then((ok) {
     if (ok) {
       if (route == "/buy_bitcoin") {
         showMonpayWebview(context);
@@ -27,13 +37,43 @@ promptLSPFeeAndNavigate(
   });
 }
 
-String _formatFeeMessage(AccountModel acc, LSPInfo lsp) {
-  var minFees = (new Int64(lsp.channelMinimumFeeMsat)) ~/ 1000;
-  String minFeesMessage = (lsp != null && lsp.channelMinimumFeeMsat > 0)
-    ? "with a minimum of ${acc.currency.format(minFees)} sats " : "";
-  if (acc.connected) {
-    var liquidity = acc.currency.format(acc.maxInboundLiquidity);
-    return "A setup fee of ${lsp.channelFeePermyriad / 100}% ${minFeesMessage}will be applied for buying more than $liquidity.";
+String _formatFeeMessage(
+  BuildContext context,
+  AccountModel acc,
+  LSPInfo lsp,
+) {
+  if (lsp == null) return "";
+  final texts = AppLocalizations.of(context);
+
+  final showMinFeeMessage = lsp.channelMinimumFeeMsat > 0;
+  final connected = acc.connected;
+
+  final minFeeFormatted = acc.currency.format(
+    Int64(lsp.channelMinimumFeeMsat ?? 0) ~/ 1000,
+  );
+  final setUpFee = (lsp.channelFeePermyriad / 100).toString();
+  final liquidity = acc.currency.format(acc.maxInboundLiquidity);
+
+  if (connected && showMinFeeMessage) {
+    return texts.invoice_ln_address_warning_with_min_fee_account_connected(
+      setUpFee,
+      minFeeFormatted,
+      liquidity,
+    );
+  } else if (connected && !showMinFeeMessage) {
+    return texts.invoice_ln_address_warning_without_min_fee_account_connected(
+      setUpFee,
+      liquidity,
+    );
+  } else if (!connected && showMinFeeMessage) {
+    return texts.invoice_ln_address_warning_with_min_fee_account_not_connected(
+      setUpFee,
+      minFeeFormatted,
+    );
+  } else {
+    return texts
+        .invoice_ln_address_warning_without_min_fee_account_not_connected(
+      setUpFee,
+    );
   }
-  return "A setup fee of ${lsp.channelFeePermyriad / 100}% ${minFeesMessage}will be applied on the received amount.";
 }
