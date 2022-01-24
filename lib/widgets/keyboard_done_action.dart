b16de0330be8e94017285dae132e437665aa539a
Ademar Alves de Oliveira <ademar111190@gmail.com>
2022-01-24 16:04:44 -0300

    Translate keyboard done action

--- lib/widgets/keyboard_done_action.dart
+++ lib/widgets/keyboard_done_action.dart
@@ -1,6 +1,7 @@
 import 'package:breez/theme_data.dart' as theme;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 const double _kBarSize = 45.0;
 
@@ -31,31 +32,35 @@ class KeyboardDoneAction {
   void _showOverlay() {
     OverlayState os = Overlay.of(focusNodes[0].context);
     _overlayEntry = OverlayEntry(builder: (context) {
+      final texts = AppLocalizations.of(context);
+      final queryData = MediaQuery.of(context);
       // Update and build footer, if any
       return Positioned(
-        bottom: MediaQuery.of(context).viewInsets.bottom,
+        bottom: queryData.viewInsets.bottom,
         left: 0,
         right: 0,
         child: Material(
           color: Colors.grey[200],
           child: Container(
             height: _kBarSize,
-            width: MediaQuery.of(context).size.width,
+            width: queryData.size.width,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.end,
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
+              children: [
                 InkWell(
-                  onTap: () {
-                    FocusScope.of(context).requestFocus(FocusNode());
-                  },
+                  onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
                   child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text("Done",
-                          style: TextStyle(
-                              color: theme.BreezColors.blue[500],
-                              fontWeight: FontWeight.bold))),
-                )
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      texts.keyboard_done_action,
+                      style: TextStyle(
+                        color: theme.BreezColors.blue[500],
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
