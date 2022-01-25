fb1705433b42b5a94267d858b391bf2b754ee552
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2022-01-25 18:53:49 +0300

    Fix scroll jump issue on Apps page

--- lib/routes/marketplace/marketplace.dart
+++ lib/routes/marketplace/marketplace.dart
@@ -8,63 +8,69 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'vendor_row.dart';
 
-class MarketplacePage extends StatelessWidget {
+class MarketplacePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MarketplacePageState();
+  }
+}
+
+class MarketplacePageState extends State<MarketplacePage> {
+  final _scaffoldKey = new GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
-    final themeData = Theme.of(context);
-    final texts = AppLocalizations.of(context);
-    final accountBloc = AppBlocsProvider.of<AccountBloc>(context);
     final marketplaceBloc = AppBlocsProvider.of<MarketplaceBloc>(context);
 
-    return StreamBuilder(
-      stream: marketplaceBloc.vendorsStream,
-      builder: (context, snapshot) {
-        List<VendorModel> vendorsModel = snapshot.data;
-
-        if (vendorsModel == null || vendorsModel.isEmpty) {
-          return _buildScaffold(
-            context,
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  texts.market_place_no_vendors,
-                  textAlign: TextAlign.center,
-                  style: themeData.textTheme.headline4.copyWith(
-                    color: theme.themeId == "BLUE"
-                        ? themeData.canvasColor
-                        : Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          );
-        }
-
-        return _buildScaffold(
-          context,
-          _buildVendors(accountBloc, vendorsModel),
-        );
-      },
-    );
-  }
-
-  Widget _buildScaffold(BuildContext context, Widget body) {
-    final themeData = Theme.of(context);
     return Scaffold(
-      key: GlobalKey<ScaffoldState>(),
+      key: _scaffoldKey,
       backgroundColor: theme.themeId == "BLUE"
-          ? themeData.backgroundColor
-          : themeData.canvasColor,
-      body: body,
+          ? Theme.of(context).backgroundColor
+          : Theme.of(context).canvasColor,
+      body: StreamBuilder(
+        stream: marketplaceBloc.vendorsStream,
+        builder: (context, snapshot) {
+          List<VendorModel> vendorsModel = snapshot.data;
+
+          if (vendorsModel == null || vendorsModel.isEmpty) {
+            return _buildNoVendorsMessage(context);
+          }
+
+          return _buildVendors(vendorsModel);
+        },
+      ),
     );
   }
 
-  Widget _buildVendors(AccountBloc accountBloc, List<VendorModel> vendorModel) {
-    return ListView.builder(
-      itemBuilder: (context, i) => VendorRow(accountBloc, vendorModel[i]),
-      itemCount: vendorModel.length,
-      itemExtent: 200.0,
+  Center _buildNoVendorsMessage(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(
+          AppLocalizations.of(context).market_place_no_vendors,
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.headline4.copyWith(
+                color: theme.themeId == "BLUE"
+                    ? Theme.of(context).canvasColor
+                    : Colors.white,
+              ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildVendors(List<VendorModel> vendorModel) {
+    final accountBloc = AppBlocsProvider.of<AccountBloc>(context);
+
+    return Container(
+      height: MediaQuery.of(context).size.height -
+          kToolbarHeight -
+          MediaQuery.of(context).padding.top,
+      child: ListView.builder(
+        itemBuilder: (context, i) => VendorRow(accountBloc, vendorModel[i]),
+        itemCount: vendorModel.length,
+        itemExtent: 200.0,
+      ),
     );
   }
 }
