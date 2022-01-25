0cbfa2e3001a000bcc6bea3a71653cb673fae4d3
Ademar Alves de Oliveira <ademar111190@gmail.com>
2022-01-25 08:25:48 -0300

    Translate close popup

--- lib/widgets/close_popup.dart
+++ lib/widgets/close_popup.dart
@@ -2,17 +2,23 @@ import 'dart:io';
 
 import 'package:breez/widgets/error_dialog.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 WillPopCallback willPopCallback(
   BuildContext context, {
   bool immediateExit = false,
-  String title: 'Exit Breez',
-  String message: 'Do you really want to quit Breez?',
+  String title,
+  String message,
   Function canCancel,
 }) {
+  final texts = AppLocalizations.of(context);
   return () async {
     if (canCancel != null && canCancel()) return true;
-    return promptAreYouSure(context, title, Text(message)).then((ok) {
+    return promptAreYouSure(
+      context,
+      title ?? texts.close_popup_title,
+      Text(message ?? texts.close_popup_message),
+    ).then((ok) {
       if (ok && immediateExit) {
         exit(0);
       }
