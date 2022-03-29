1f633342bd73940297c4ff14b3677dd62a545ccc
Ademar Alves de Oliveira <ademar111190@gmail.com>
2022-03-29 16:53:20 -0300

    Translate sweep all coins

--- lib/routes/withdraw_funds/sweep_all_coins_confirmation.dart
+++ lib/routes/withdraw_funds/sweep_all_coins_confirmation.dart
@@ -11,6 +11,7 @@ import 'package:breez/widgets/loader.dart';
 import 'package:breez/widgets/single_button_bottom_bar.dart';
 import 'package:fixnum/fixnum.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SweepAllCoinsConfirmation extends StatefulWidget {
   final AccountBloc accountBloc;
@@ -18,13 +19,13 @@ class SweepAllCoinsConfirmation extends StatefulWidget {
   final Function() onPrevious;
   final Future Function(TxDetail tx) onConfirm;
 
-  const SweepAllCoinsConfirmation(
-      {Key key,
-      this.onPrevious,
-      this.onConfirm,
-      this.accountBloc,
-      this.address})
-      : super(key: key);
+  const SweepAllCoinsConfirmation({
+    Key key,
+    this.onPrevious,
+    this.onConfirm,
+    this.accountBloc,
+    this.address,
+  }) : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
@@ -85,75 +86,78 @@ class SweepAllCoinsConfirmationState extends State<SweepAllCoinsConfirmation> {
 
   @override
   Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
+    final texts = AppLocalizations.of(context);
     return Scaffold(
       appBar: AppBar(
-          iconTheme: Theme.of(context).appBarTheme.iconTheme,
-          textTheme: Theme.of(context).appBarTheme.textTheme,
-          backgroundColor: Theme.of(context).canvasColor,
-          leading: backBtn.BackButton(onPressed: () {
-            widget.onPrevious();
-          }),
-          title: Text("Choose Processing Speed",
-              style: Theme.of(context).appBarTheme.textTheme.headline6),
-          elevation: 0.0),
+        iconTheme: themeData.appBarTheme.iconTheme,
+        textTheme: themeData.appBarTheme.textTheme,
+        backgroundColor: themeData.canvasColor,
+        leading: backBtn.BackButton(onPressed: () {
+          widget.onPrevious();
+        }),
+        title: Text(
+          texts.sweep_all_coins_speed,
+          style: themeData.appBarTheme.textTheme.headline6,
+        ),
+        elevation: 0.0,
+      ),
       body: StreamBuilder<AccountModel>(
-          stream: AppBlocsProvider.of<AccountBloc>(context).accountStream,
-          builder: (context, snapshot) {
-            AccountModel acc = snapshot.data;
-            return FutureBuilder(
-                future: _txsDetailsFuture,
-                builder: (context, futureSnapshot) {
-                  if (futureSnapshot.error != null) {
-                    //render error
-                    return _ErrorMessage(
-                        message:
-                            "Failed to retrieve fees. Please try again later.");
-                  }
-                  if (futureSnapshot.connectionState != ConnectionState.done ||
-                      acc == null) {
-                    //render loader
-                    return SizedBox();
-                  }
+        stream: AppBlocsProvider.of<AccountBloc>(context).accountStream,
+        builder: (context, snapshot) {
+          AccountModel acc = snapshot.data;
+          return FutureBuilder(
+            future: _txsDetailsFuture,
+            builder: (context, futureSnapshot) {
+              if (futureSnapshot.error != null) {
+                //render error
+                return _ErrorMessage(
+                  message: texts.sweep_all_coins_error_retrieve_fees,
+                );
+              }
+              if (futureSnapshot.connectionState != ConnectionState.done ||
+                  acc == null) {
+                //render loader
+                return SizedBox();
+              }
 
-                  if (feeOptions.where((f) => f != null).length == 0) {
-                    return _ErrorMessage(
-                        message:
-                            "The amount is too small to broadcast. Please try again later.");
-                  }
+              if (feeOptions.where((f) => f != null).length == 0) {
+                return _ErrorMessage(
+                  message: texts.sweep_all_coins_error_amount_small,
+                );
+              }
 
-                  return Container(
-                    height: 500.0,
-                    padding: EdgeInsets.only(
-                        left: 16.0, right: 16.0, bottom: 40.0, top: 24.0),
-                    width: MediaQuery.of(context).size.width,
-                    child: Column(
-                      //mainAxisSize: MainAxisSize.max,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Container(
-                          child: FeeChooser(
-                            economyFee: feeOptions[0],
-                            regularFee: feeOptions[1],
-                            priorityFee: feeOptions[2],
-                            selectedIndex: this.selectedFeeIndex,
-                            onSelect: (index) {
-                              this.setState(() {
-                                this.selectedFeeIndex = index;
-                              });
-                            },
-                          ),
-                        ),
-                        SizedBox(height: 36.0),
-                        buildSummary(acc),
-                      ],
+              return Container(
+                height: 500.0,
+                padding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 40.0),
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: FeeChooser(
+                        economyFee: feeOptions[0],
+                        regularFee: feeOptions[1],
+                        priorityFee: feeOptions[2],
+                        selectedIndex: this.selectedFeeIndex,
+                        onSelect: (index) => setState(() {
+                          this.selectedFeeIndex = index;
+                        }),
+                      ),
                     ),
-                  );
-                });
-          }),
+                    SizedBox(height: 36.0),
+                    buildSummary(context, acc),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
       bottomNavigationBar: !_showConfirm
           ? null
           : SingleButtonBottomBar(
-              text: "CONFIRM",
+              text: texts.sweep_all_coins_action_confirm,
               onPressed: () {
                 Navigator.of(context).push(createLoaderRoute(context));
                 widget.onConfirm(transactions[selectedFeeIndex]).then((_) {
@@ -161,30 +165,40 @@ class SweepAllCoinsConfirmationState extends State<SweepAllCoinsConfirmation> {
                 }).catchError((error) {
                   Navigator.of(context).pop();
                   promptError(
-                      context,
-                      null,
-                      Text(error.toString(),
-                          style:
-                              Theme.of(context).dialogTheme.contentTextStyle));
+                    context,
+                    null,
+                    Text(
+                      error.toString(),
+                      style: themeData.dialogTheme.contentTextStyle,
+                    ),
+                  );
                 });
               },
             ),
     );
   }
 
-  Widget buildSummary(AccountModel acc) {
-    var receive = _sweepAmount - feeOptions[selectedFeeIndex].sats;
+  Widget buildSummary(BuildContext context, AccountModel acc) {
+    final themeData = Theme.of(context);
+    final texts = AppLocalizations.of(context);
+    final receive = _sweepAmount - feeOptions[selectedFeeIndex].sats;
     return Container(
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(5.0)),
-          border: Border.all(
-              color: Theme.of(context).colorScheme.onSurface.withOpacity(0.4))),
-      child: ListView(shrinkWrap: true, children: <Widget>[
-        ListTile(
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        border: Border.all(
+          color: themeData.colorScheme.onSurface.withOpacity(0.4),
+        ),
+      ),
+      child: ListView(
+        shrinkWrap: true,
+        children: [
+          ListTile(
             title: Container(
               child: AutoSizeText(
-                "You send:",
-                style: TextStyle(color: Colors.white),
+                texts.sweep_all_coins_label_send,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
                 maxLines: 1,
                 minFontSize: MinFontSize(context).minFontSize,
                 stepGranularity: 0.1,
@@ -193,37 +207,22 @@ class SweepAllCoinsConfirmationState extends State<SweepAllCoinsConfirmation> {
             trailing: Container(
               child: AutoSizeText(
                 acc.currency.format(_sweepAmount),
-                style: TextStyle(color: Theme.of(context).errorColor),
-                maxLines: 1,
-                minFontSize: MinFontSize(context).minFontSize,
-                stepGranularity: 0.1,
-              ),
-            )),
-        ListTile(
-            title: Container(
-              child: AutoSizeText(
-                "Transaction fee:",
-                style: TextStyle(color: Colors.white.withOpacity(0.4)),
-                maxLines: 1,
-                minFontSize: MinFontSize(context).minFontSize,
-                stepGranularity: 0.1,
-              ),
-            ),
-            trailing: Container(
-              child: AutoSizeText(
-                "-${acc.currency.format(Int64(feeOptions[selectedFeeIndex].sats))}",
                 style: TextStyle(
-                    color: Theme.of(context).errorColor.withOpacity(0.4)),
+                  color: themeData.errorColor,
+                ),
                 maxLines: 1,
                 minFontSize: MinFontSize(context).minFontSize,
                 stepGranularity: 0.1,
               ),
-            )),
-        ListTile(
+            ),
+          ),
+          ListTile(
             title: Container(
               child: AutoSizeText(
-                "You receive:",
-                style: TextStyle(color: Colors.white),
+                texts.sweep_all_coins_label_transaction_fee,
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.4),
+                ),
                 maxLines: 1,
                 minFontSize: MinFontSize(context).minFontSize,
                 stepGranularity: 0.1,
@@ -231,18 +230,53 @@ class SweepAllCoinsConfirmationState extends State<SweepAllCoinsConfirmation> {
             ),
             trailing: Container(
               child: AutoSizeText(
-                acc.currency.format(
-                        _sweepAmount - feeOptions[selectedFeeIndex].sats) +
-                    (acc.fiatCurrency == null
-                        ? ""
-                        : " (${acc.fiatCurrency.format(receive)})"),
-                style: TextStyle(color: Theme.of(context).errorColor),
+                texts.sweep_all_coins_fee(
+                  acc.currency.format(
+                    Int64(feeOptions[selectedFeeIndex].sats),
+                  ),
+                ),
+                style: TextStyle(
+                  color: themeData.errorColor.withOpacity(0.4),
+                ),
                 maxLines: 1,
                 minFontSize: MinFontSize(context).minFontSize,
                 stepGranularity: 0.1,
               ),
-            ))
-      ]),
+            ),
+          ),
+          ListTile(
+            title: Container(
+              child: AutoSizeText(
+                texts.sweep_all_coins_label_receive,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+                maxLines: 1,
+                minFontSize: MinFontSize(context).minFontSize,
+                stepGranularity: 0.1,
+              ),
+            ),
+            trailing: Container(
+              child: AutoSizeText(
+                acc.fiatCurrency == null
+                    ? texts.sweep_all_coins_amount_no_fiat(
+                        acc.currency.format(receive),
+                      )
+                    : texts.sweep_all_coins_amount_with_fiat(
+                        acc.currency.format(receive),
+                        acc.fiatCurrency.format(receive),
+                      ),
+                style: TextStyle(
+                  color: themeData.errorColor,
+                ),
+                maxLines: 1,
+                minFontSize: MinFontSize(context).minFontSize,
+                stepGranularity: 0.1,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -250,23 +284,27 @@ class SweepAllCoinsConfirmationState extends State<SweepAllCoinsConfirmation> {
 class _ErrorMessage extends StatelessWidget {
   final String message;
 
-  const _ErrorMessage({Key key, this.message}) : super(key: key);
+  const _ErrorMessage({
+    Key key,
+    this.message,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsets.only(top: 40.0, left: 40.0, right: 40.0),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              child: Text(
-                message,
-                textAlign: TextAlign.center,
-              ),
-            )
-          ],
-        ));
+      padding: const EdgeInsets.only(top: 40.0, left: 40.0, right: 40.0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Text(
+              message,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
