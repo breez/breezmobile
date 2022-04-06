0560adf734298459ef8152cb6595044247bba651
Ademar Alves de Oliveira <ademar111190@gmail.com>
2022-04-06 14:03:00 -0300

    Tranlate small route pages

--- lib/routes/select_provider_error_dialog.dart
+++ lib/routes/select_provider_error_dialog.dart
@@ -2,34 +2,41 @@ import 'package:breez/theme_data.dart' as theme;
 import 'package:breez/widgets/error_dialog.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void showProviderErrorDialog(
-    BuildContext context, String error, Function() onSelect) {
-  String message = "In order to activate Breez, please ";
-  if (error != null) {
-    message =
-        "There was an error connecting to the selected provider. " + message;
-  }
+  BuildContext context,
+  String error,
+  Function() onSelect,
+) {
+  final themeData = Theme.of(context);
+  final texts = AppLocalizations.of(context);
+
   promptError(
     context,
-    "Connection Error",
+    texts.select_provider_error_dialog_title,
     RichText(
       text: TextSpan(
-          style: Theme.of(context).dialogTheme.contentTextStyle,
-          text: message,
-          children: <TextSpan>[
-            TextSpan(
-                text: "select ",
-                style: theme.blueLinkStyle,
-                recognizer: TapGestureRecognizer()
-                  ..onTap = () async {
-                    Navigator.of(context).pop();
-                    onSelect();
-                  }),
-            TextSpan(
-                text: "a provider.",
-                style: Theme.of(context).dialogTheme.contentTextStyle),
-          ]),
+        style: themeData.dialogTheme.contentTextStyle,
+        text: error != null
+            ? texts.select_provider_error_dialog_message_error
+            : texts.select_provider_error_dialog_message,
+        children: [
+          TextSpan(
+            text: texts.select_provider_error_dialog_select_provider_begin,
+            style: theme.blueLinkStyle,
+            recognizer: TapGestureRecognizer()
+              ..onTap = () async {
+                Navigator.of(context).pop();
+                onSelect();
+              },
+          ),
+          TextSpan(
+            text: texts.select_provider_error_dialog_select_provider_end,
+            style: themeData.dialogTheme.contentTextStyle,
+          ),
+        ],
+      ),
     ),
   );
 }
