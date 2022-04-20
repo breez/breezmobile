aa56e04e07e40602a99de2ebfe56b4e42e411873
Ademar Alves de Oliveira <ademar111190@gmail.com>
2022-04-20 17:42:11 -0300

    Add support to LN-URL image metadata

--- lib/routes/lnurl_fetch_invoice_page.dart
+++ lib/routes/lnurl_fetch_invoice_page.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:breez/services/breezlib/data/rpc.pb.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:breez/bloc/account/account_bloc.dart';
 import 'package:breez/bloc/account/account_model.dart';
@@ -12,6 +11,7 @@ import 'package:breez/bloc/lnurl/lnurl_bloc.dart';
 import 'package:breez/bloc/lnurl/lnurl_model.dart';
 import 'package:breez/logger.dart';
 import 'package:breez/theme_data.dart' as theme;
+import 'package:breez/utils/lnurl_metadata_extension.dart';
 import 'package:breez/utils/min_font_size.dart';
 import 'package:breez/widgets/amount_form_field.dart';
 import 'package:breez/widgets/back_button.dart' as backBtn;
@@ -172,113 +172,115 @@ class LNURLFetchInvoicePageState extends State<LNURLFetchInvoicePage> {
           return Form(
             key: _formKey,
             child: Padding(
-              padding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 40.0),
-              child: Scrollbar(
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      response.commentAllowed > 0
-                          ? TextFormField(
-                              controller: _commentController,
-                              keyboardType: TextInputType.multiline,
-                              textInputAction: TextInputAction.done,
-                              maxLines: null,
-                              maxLength: response.commentAllowed.toInt(),
-                              maxLengthEnforced: true,
-                              decoration: InputDecoration(
-                                labelText: texts.lnurl_fetch_invoice_comment,
-                              ),
-                              style: theme.FieldTextStyle.textStyle,
-                            )
-                          : Container(),
-                      AmountFormField(
-                        context: context,
-                        accountModel: acc,
-                        texts: texts,
-                        focusNode: _amountFocusNode,
-                        controller: _amountController,
-                        validatorFn: (amt) {
-                          final min = response.minAmount;
-                          final max = response.maxAmount;
-
-                          if (amt < min) {
-                            return texts.lnurl_fetch_invoice_error_min(
-                              acc.currency.format(min),
-                            );
-                          }
-
-                          if (amt > max) {
-                            return texts.lnurl_fetch_invoice_error_max(
-                              acc.currency.format(max),
-                            );
-                          }
-
-                          return null;
-                        },
-                        style: theme.FieldTextStyle.textStyle,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 8),
-                        child: Text(
-                          texts.lnurl_fetch_invoice_limit(
-                            acc.currency.format(response.minAmount),
-                            acc.currency.format(response.maxAmount),
+              padding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 0.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  response.commentAllowed > 0
+                      ? TextFormField(
+                          controller: _commentController,
+                          keyboardType: TextInputType.multiline,
+                          textInputAction: TextInputAction.done,
+                          maxLines: null,
+                          maxLength: response.commentAllowed.toInt(),
+                          maxLengthEnforced: true,
+                          decoration: InputDecoration(
+                            labelText: texts.lnurl_fetch_invoice_comment,
                           ),
-                          textAlign: TextAlign.left,
-                          style: theme.FieldTextStyle.labelStyle,
-                        ),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: 48,
-                        padding: const EdgeInsets.only(top: 16.0),
-                        child: _buildDescription(context, acc),
-                      ),
-                      StreamBuilder(
-                        stream: accountBloc.accountStream,
-                        builder: (context, accSnapshot) {
-                          if (!accSnapshot.hasData) {
-                            return Container();
-                          }
+                          style: theme.FieldTextStyle.textStyle,
+                        )
+                      : Container(),
+                  AmountFormField(
+                    context: context,
+                    accountModel: acc,
+                    texts: texts,
+                    focusNode: _amountFocusNode,
+                    controller: _amountController,
+                    validatorFn: (amt) {
+                      final min = response.minAmount;
+                      final max = response.maxAmount;
 
-                          String message;
-                          if (accSnapshot.hasError) {
-                            message = accSnapshot.error.toString();
-                          }
+                      if (amt < min) {
+                        return texts.lnurl_fetch_invoice_error_min(
+                          acc.currency.format(min),
+                        );
+                      }
 
-                          if (message != null) {
-                            // In case error doesn't have a trailing full stop
-                            if (!message.endsWith('.')) {
-                              message += '.';
-                            }
-                            return Container(
-                              padding: const EdgeInsets.only(
-                                top: 50.0,
-                                left: 30.0,
-                                right: 30.0,
-                              ),
-                              child: Column(
-                                children: [
-                                  Text(
-                                    message,
-                                    textAlign: TextAlign.center,
-                                    style: theme.warningStyle.copyWith(
-                                      color: themeData.errorColor,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            );
-                          } else {
-                            return SizedBox();
-                          }
-                        },
-                      )
-                    ],
+                      if (amt > max) {
+                        return texts.lnurl_fetch_invoice_error_max(
+                          acc.currency.format(max),
+                        );
+                      }
+
+                      return null;
+                    },
+                    style: theme.FieldTextStyle.textStyle,
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8),
+                    child: Text(
+                      texts.lnurl_fetch_invoice_limit(
+                        acc.currency.format(response.minAmount),
+                        acc.currency.format(response.maxAmount),
+                      ),
+                      textAlign: TextAlign.left,
+                      style: theme.FieldTextStyle.labelStyle,
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 48,
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: _buildDescription(context, acc),
+                  ),
+                  StreamBuilder(
+                    stream: accountBloc.accountStream,
+                    builder: (context, accSnapshot) {
+                      if (!accSnapshot.hasData) {
+                        return Container();
+                      }
+
+                      String message;
+                      if (accSnapshot.hasError) {
+                        message = accSnapshot.error.toString();
+                      }
+
+                      if (message != null) {
+                        // In case error doesn't have a trailing full stop
+                        if (!message.endsWith('.')) {
+                          message += '.';
+                        }
+                        return Container(
+                          padding: const EdgeInsets.only(
+                            top: 50.0,
+                            left: 30.0,
+                            right: 30.0,
+                          ),
+                          child: Column(
+                            children: [
+                              Text(
+                                message,
+                                textAlign: TextAlign.center,
+                                style: theme.warningStyle.copyWith(
+                                  color: themeData.errorColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      } else {
+                        return SizedBox();
+                      }
+                    },
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 22),
+                      child: _buildImage(context),
+                    ),
+                  ),
+                ],
               ),
             ),
           );
@@ -293,11 +295,29 @@ class LNURLFetchInvoicePageState extends State<LNURLFetchInvoicePage> {
   ) {
     return GestureDetector(
       child: AutoSizeText(
-        "${_getMetadataText(_payFetchResponse.metadata)}",
+        _payFetchResponse.metadata.metadataText(),
         style: theme.textStyle,
         maxLines: 1,
         minFontSize: MinFontSize(context).minFontSize,
       ),
+    );
+  }
+
+  Widget _buildImage(BuildContext context) {
+    final image = _payFetchResponse.metadata.metadataImage();
+    if (image == null) return Container();
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ConstrainedBox(
+          constraints: BoxConstraints(
+            maxWidth: 125,
+            maxHeight: 125,
+          ),
+          child: image,
+        ),
+      ],
     );
   }
 
@@ -310,48 +330,6 @@ class LNURLFetchInvoicePageState extends State<LNURLFetchInvoicePage> {
     if (response.minAmount == response.maxAmount) {
       _amountController.text = "${response.minAmount}";
     }
-  }
-
-  String _getMetadataText(List<LNUrlPayMetadata> metadata) {
-    /*
-
-    `metadata` json array must contain one `text/plain` entry, all other types of entries are optional:
-
-    ```
-    [
-        [
-            "text/plain", // must always be present
-            content // actual metadata content
-        ],
-        [
-            "image/png;base64", // optional 512x512px PNG thumbnail which will represent this lnurl in a list or grid
-            content // base64 string, up to 136536 characters (100Kb of image data in base-64 encoding)
-        ],
-        [
-            "image/jpeg;base64", // optional 512x512px JPG thumbnail which will represent this lnurl in a list or grid
-            content // base64 string, up to 136536 characters (100Kb of image data in base-64 encoding)
-        ],
-        ... // more objects for future types
-    ]
-    ```
-
-    and be sent as a string:
-
-    ```
-    "[[\"text/plain\", \"lorem ipsum blah blah\"]]"
-    ```
-    */
-    var result = '';
-    log.info('_getMetadataText: metadata: $metadata');
-
-    for (var datum in metadata) {
-      if (datum.entry[0] == 'text/plain') {
-        result = datum.entry[1];
-        break;
-      }
-    }
-
-    return result;
   }
 
   void _getInvoice(
---
+++ lib/utils/lnurl_metadata_extension.dart
@@ -0,0 +1,35 @@
+import 'package:breez/services/breezlib/data/rpc.pb.dart';
+import 'package:flutter/cupertino.dart';
+
+Map<List<LNUrlPayMetadata>, Image> _imageCache = {};
+
+extension LnurlMetadata on List<LNUrlPayMetadata> {
+  String metadataText() {
+    final textSource = firstWhere(
+      (e) => e.hasDescription() && e.description.isNotEmpty,
+      orElse: () => null,
+    );
+    if (textSource == null) return "";
+    return textSource.description;
+  }
+
+  Image metadataImage() {
+    if (_imageCache.containsKey(this)) {
+      return _imageCache[this];
+    }
+
+    final imageSource = firstWhere(
+      (e) => e.hasImage(),
+      orElse: () => null,
+    );
+
+    if (imageSource == null) {
+      _imageCache[this] = null;
+      return null;
+    }
+
+    final image = Image.memory(imageSource.image.bytes);
+    _imageCache[this] = image;
+    return image;
+  }
+}
