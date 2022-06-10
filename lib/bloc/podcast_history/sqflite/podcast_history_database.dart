3e0354ec37f9508edadce32befae74b0772e1d9b
Zealth <zealth@Zealths-MacBook-Air.local>
2022-06-10 16:14:15 +0530

    add podcast history database

---
+++ lib/bloc/podcast_history/sqflite/podcast_history_database.dart
@@ -0,0 +1,67 @@
+import 'package:breez/bloc/podcast_history/sqflite/podcast_history_model.dart';
+import 'package:sqflite/sqflite.dart';
+import '../../pos_catalog/sqlite/db.dart';
+
+class PodcastHistoryDatabase {
+  static final PodcastHistoryDatabase instance = PodcastHistoryDatabase._init();
+
+  static Database _database;
+  PodcastHistoryDatabase._init();
+
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database;
+    }
+    _database = await _initDB("podcast_history.db");
+    return _database;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasePath();
+    final path = dbPath + filePath;
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    final fieldIdType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    final textType = 'TEXT NOT NULL';
+    final integerType = 'INTEGER NOT NULL';
+    final doubleType = 'DECIMAL NOT NULL';
+
+    await db.execute('''
+CREATE TABLE $podcastHistoryTable(
+${PodcastHistoryFields.fieldId} $fieldIdType,
+${PodcastHistoryFields.podcastId} $textType,
+${PodcastHistoryFields.timeStamp} $textType,
+${PodcastHistoryFields.satsSpent} $integerType,
+${PodcastHistoryFields.boostagramsSent} $integerType,
+${PodcastHistoryFields.podcastName} $textType,
+${PodcastHistoryFields.podcastImageUrl} $textType,
+${PodcastHistoryFields.durationInMins} $doubleType
+ )
+ ''');
+  }
+
+  Future<PodcastHistoryModel> create(PodcastHistoryModel podcastHistory) async {
+    final db = await instance.database;
+    final id = await db.insert(podcastHistoryTable, podcastHistory.toJson());
+    return podcastHistory.copy(fieldId: id);
+  }
+
+  Future<List<PodcastHistoryModel>> readAllHistory(
+      {DateTime filterStartDate, DateTime filterEndDate}) async {
+    final db = await instance.database;
+    List<Map<String, Object>> result;
+
+    if (filterStartDate == null || filterEndDate == null) {
+      result = await db.query(
+        podcastHistoryTable,
+      );
+    } else {
+      result = await db.rawQuery(
+          ''' SELECT * FROM $podcastHistoryTable WHERE ${PodcastHistoryFields.timeStamp} >= '${filterStartDate.toIso8601String()}' AND ${PodcastHistoryFields.timeStamp}<='${filterEndDate.toIso8601String()}' ''');
+    }
+
+    return result.map((json) => PodcastHistoryModel.fromJson(json)).toList();
+  }
+}
---
+++ lib/bloc/podcast_history/sqflite/podcast_history_model.dart
@@ -0,0 +1,88 @@
+final String podcastHistoryTable = 'podcast_history_table';
+
+class PodcastHistoryFields {
+  static final List<String> values = [
+    fieldId,
+    podcastId,
+    timeStamp,
+    satsSpent,
+    boostagramsSent,
+    podcastName,
+    durationInMins,
+    podcastImageUrl
+  ];
+
+  static final String fieldId = '_id';
+  static final String podcastId = 'podcastId';
+  static final String timeStamp = 'timeStamp';
+  static final String satsSpent = 'satsSpent';
+  static final String boostagramsSent = 'boostagramSent';
+  static final String durationInMins = 'durationInMins';
+  static final String podcastName = 'podcastName';
+  static final String podcastImageUrl = 'podcastImageUrl';
+}
+
+class PodcastHistoryModel {
+  final int fieldId;
+  final String podcastId;
+  final DateTime timeStamp;
+  final int satsSpent;
+  final int boostagramsSent;
+  final double durationInMins;
+  final String podcastName;
+  final String podcastImageUrl;
+
+  PodcastHistoryModel(
+      {this.fieldId,
+      this.podcastId,
+      this.timeStamp,
+      this.satsSpent,
+      this.boostagramsSent,
+      this.durationInMins,
+      this.podcastName,
+      this.podcastImageUrl});
+
+  Map<String, Object> toJson() => {
+        PodcastHistoryFields.fieldId: fieldId,
+        PodcastHistoryFields.podcastId: podcastId,
+        PodcastHistoryFields.durationInMins: durationInMins,
+        PodcastHistoryFields.timeStamp:
+            timeStamp != null ? timeStamp.toIso8601String() : null,
+        PodcastHistoryFields.satsSpent: satsSpent,
+        PodcastHistoryFields.boostagramsSent: boostagramsSent,
+        PodcastHistoryFields.podcastName: podcastName,
+        PodcastHistoryFields.podcastImageUrl: podcastImageUrl,
+      };
+
+  static PodcastHistoryModel fromJson(Map<String, Object> json) =>
+      PodcastHistoryModel(
+        fieldId: json[PodcastHistoryFields.fieldId] as int,
+        podcastId: json[PodcastHistoryFields.podcastId] as String,
+        timeStamp: json[PodcastHistoryFields.timeStamp] != null
+            ? DateTime.parse(json[PodcastHistoryFields.timeStamp] as String)
+            : null,
+        satsSpent: json[PodcastHistoryFields.satsSpent] as int,
+        boostagramsSent: json[PodcastHistoryFields.boostagramsSent] as int,
+        durationInMins: json[PodcastHistoryFields.durationInMins] as double,
+        podcastName: json[PodcastHistoryFields.podcastName] as String,
+        podcastImageUrl: json[PodcastHistoryFields.podcastImageUrl] as String,
+      );
+
+  PodcastHistoryModel copy(
+          {int fieldId,
+          String podcastId,
+          DateTime timeStamp,
+          int satsSpent,
+          int boostagramsSent,
+          String podcastName,
+          String podcastImageUrl}) =>
+      PodcastHistoryModel(
+        fieldId: fieldId ?? this.fieldId,
+        podcastId: podcastId ?? this.podcastId,
+        satsSpent: satsSpent ?? this.satsSpent,
+        durationInMins: durationInMins ?? this.durationInMins,
+        boostagramsSent: boostagramsSent ?? this.boostagramsSent,
+        podcastName: podcastName ?? this.podcastName,
+        podcastImageUrl: podcastImageUrl ?? this.podcastImageUrl,
+      );
+}
