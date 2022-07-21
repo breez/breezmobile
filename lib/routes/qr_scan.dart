1c6d30b0a14ff459a47b6005af18f18822698b14
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2022-07-21 15:16:57 +0300

    Extract ImagePicker and Cancel button into stand alone widgets

--- lib/routes/qr_scan.dart
+++ lib/routes/qr_scan.dart
@@ -28,72 +28,50 @@ class QRScanState extends State<QRScan> {
     return Scaffold(
       body: Stack(
         children: [
-          buildScanner(context),
-          buildImagePicker(),
-          buildCancelButton(context),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0.0,
+            top: 0.0,
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  flex: 5,
+                  child: MobileScanner(
+                    key: qrKey,
+                    allowDuplicates: false,
+                    controller: cameraController,
+                    onDetect: (barcode, args) {
+                      if (barcode.rawValue == null) {
+                        debugPrint('Failed to scan QR code.');
+                      } else {
+                        final String code = barcode.rawValue;
+                        Navigator.of(context).pop(code);
+                      }
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            right: 10,
+            top: 5,
+            child: const ImagePickerButton(),
+          ),
+          Positioned(
+            bottom: 30.0,
+            right: 0,
+            left: 0,
+            child: defaultTargetPlatform == TargetPlatform.iOS
+                ? const QRScanCancelButton()
+                : const SizedBox(),
+          ),
           const ScanOverlay(),
         ],
       ),
     );
   }
-
-  Positioned buildScanner(BuildContext context) {
-    return Positioned(
-      left: 0,
-      right: 0,
-      bottom: 0.0,
-      top: 0.0,
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 5,
-            child: MobileScanner(
-              key: qrKey,
-              allowDuplicates: false,
-              controller: cameraController,
-              onDetect: (barcode, args) {
-                if (barcode.rawValue == null) {
-                  debugPrint('Failed to scan QR code.');
-                } else {
-                  final String code = barcode.rawValue;
-                  Navigator.of(context).pop(code);
-                }
-              },
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  Positioned buildImagePicker() {
-    return Positioned(
-      right: 10,
-      top: 5,
-      child: IconButton(
-        padding: const EdgeInsets.fromLTRB(0, 32, 24, 0),
-        icon: SvgPicture.asset(
-          "src/icon/image.svg",
-          color: Colors.white,
-          width: 32,
-          height: 32,
-        ),
-        onPressed: () async {
-          final picker = ImagePicker();
-          XFile pickedFile = await picker
-              .pickImage(source: ImageSource.gallery)
-              .catchError((err) {});
-          final File file = File(pickedFile.path);
-          try {
-            String data = await Scan.parse(file.path);
-            if (!mounted) return;
-            Navigator.of(context).pop(data);
-          } catch (_) {}
-        },
-      ),
-    );
-  }
-
   /*
   Positioned buildSwitchCameraButton() {
     return Positioned(
@@ -151,32 +129,62 @@ class QRScanState extends State<QRScan> {
     );
   }
   */
+}
 
-  Positioned buildCancelButton(BuildContext context) {
-    return Positioned(
-      bottom: 30.0,
-      right: 0,
-      left: 0,
-      child: defaultTargetPlatform == TargetPlatform.iOS
-          ? Center(
-              child: Container(
-                decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(Radius.circular(12.0)),
-                    border: Border.all(color: Colors.white.withOpacity(0.8))),
-                child: TextButton(
-                    style: TextButton.styleFrom(
-                      padding: const EdgeInsets.only(right: 35, left: 35),
-                    ),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text(
-                      "CANCEL",
-                      style: TextStyle(color: Colors.white),
-                    )),
-              ),
-            )
-          : const SizedBox(),
+class ImagePickerButton extends StatelessWidget {
+  const ImagePickerButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      padding: const EdgeInsets.fromLTRB(0, 32, 24, 0),
+      icon: SvgPicture.asset(
+        "src/icon/image.svg",
+        color: Colors.white,
+        width: 32,
+        height: 32,
+      ),
+      onPressed: () async {
+        final picker = ImagePicker();
+        XFile pickedFile = await picker
+            .pickImage(source: ImageSource.gallery)
+            .catchError((err) {});
+        final File file = File(pickedFile.path);
+        try {
+          String data = await Scan.parse(file.path);
+          Navigator.of(context).pop(data);
+        } catch (_) {}
+      },
+    );
+  }
+}
+
+class QRScanCancelButton extends StatelessWidget {
+  const QRScanCancelButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(12.0)),
+            border: Border.all(color: Colors.white.withOpacity(0.8))),
+        child: TextButton(
+            style: TextButton.styleFrom(
+              padding: const EdgeInsets.only(right: 35, left: 35),
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text(
+              "CANCEL",
+              style: TextStyle(color: Colors.white),
+            )),
+      ),
     );
   }
 }
