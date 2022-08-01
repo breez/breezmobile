91d03dabdd780f8a0c40531cc7b6aa2d7ad67883
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2022-08-01 16:16:38 +0300

    Add imports and fix notificationBuilder

--- android/app/src/main/java/com/breez/client/NotificationActionReceiver.java
+++ android/app/src/main/java/com/breez/client/NotificationActionReceiver.java
@@ -1,12 +1,19 @@
 package com.breez.client;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.os.Build;
 import android.util.Log;
 
 import com.google.firebase.messaging.RemoteMessage;
+import androidx.core.app.NotificationCompat;
 
 import static com.breez.client.BreezFirebaseMessagingService.EXTRA_REMOTE_MESSAGE;
 
@@ -34,26 +41,16 @@ public class NotificationActionReceiver extends BroadcastReceiver {
         int flags = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S ? PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE : PendingIntent.FLAG_UPDATE_CURRENT;
         PendingIntent appIntent = PendingIntent.getActivity(context, 0, notificationIntent, flags);
 
-        NotificationCompat.Action action =
-                new NotificationCompat.Action.Builder(ic_delete,
-                        actionText, appIntent)
-                        .build();
-
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, "channel_id")
-                .setContentTitle(title)
-                .setContentText(body)
                 .setAutoCancel(true)
                 .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
                 .setContentIntent(appIntent)
-                .setContentInfo("CONTENT")
                 .setLargeIcon(icon)
                 .setColor(0xFF0089f9)
                 .setLights(0xFF0089f9, 1000, 300)
                 .setColorized(true)
                 .setDefaults(android.app.Notification.DEFAULT_VIBRATE)
                 .setSmallIcon(R.mipmap.breez_notify)
-                .addAction(action)
-                .setStyle(new NotificationCompat.BigTextStyle().bigText(body))
                 .setPriority(2);
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
