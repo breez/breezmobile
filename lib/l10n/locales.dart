49e861b8303ddc261c512fc5df83719d6e2a1ed1
Ademar Alves de Oliveira <ademar111190@gmail.com>
2022-08-10 12:04:13 -0300

    Sync Anytime to pull its news translations improvements and additions:
    - Enable Italian thank you @theDavidCoen
    - Add Spanish
    - Update Portuguese
    - Make room for French and Finnish translations
    - Move generated files to generated folder
    - Update L to take advantage of generated code
    
    Related https://github.com/breez/anytime_podcast_player/commit/06c1af145c8e646f8ec0935c33d1cf6051210004

--- lib/l10n/locales.dart
+++ lib/l10n/locales.dart
@@ -1,7 +1,9 @@
 import 'package:anytime/l10n/L.dart';
+import 'package:anytime/l10n/generated/anytime_texts.dart' as anytime_texts;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 Iterable<LocalizationsDelegate<dynamic>> localizationsDelegates() {
   return [
@@ -14,27 +16,18 @@ Iterable<LocalizationsDelegate<dynamic>> localizationsDelegates() {
   ];
 }
 
-Iterable<Locale> supportedLocales() {
-  return [
-    const Locale('en', ''),
-    const Locale('es', ''),
-    const Locale('fi', ''),
-    const Locale('fr', ''),
-    const Locale('it', ''),
-    const Locale('pt', ''),
-  ];
-}
-
 class AnytimeFallbackLocalizationDelegate extends LocalizationsDelegate<L> {
   const AnytimeFallbackLocalizationDelegate();
 
+  // Only support if the locale is supported by the app and by anytime.
   @override
   bool isSupported(Locale locale) =>
-      !['en', 'de', 'pt'].contains(locale.languageCode);
+      AppLocalizations.supportedLocales.any((l) => l.languageCode == locale.languageCode) &&
+      anytime_texts.AppLocalizations.supportedLocales.any((l) => l.languageCode == locale.languageCode);
 
   // Fallback to English if Breez's supported locale is not supported by Anytime
   @override
-  Future<L> load(Locale locale) => L.load(const Locale('en', ''), null);
+  Future<L> load(Locale locale) => SynchronousFuture(L.load(const Locale('en', ''), null));
 
   @override
   bool shouldReload(_) => false;
