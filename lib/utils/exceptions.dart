1aecd796cdb80c33f6e7a17842b308edfcd197e1
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2022-10-12 12:38:55 +0300

    Expand lnUrlRegex

--- lib/utils/exceptions.dart
+++ lib/utils/exceptions.dart
@@ -1,7 +1,7 @@
 String extractExceptionMessage(Object exception) {
   final detailRegex = r'((?<=\"detail\":\")(.*)(?=.*\"}))';
   final nsLocalizedRegex = r'((?<={NSLocalizedDescription=)(.*)(?=}))';
-  final lnUrlRegex = r'((?<=LNURL:.)(.*)(?=.method:))';
+  final lnUrlRegex = r'((?<=LNURL.*:.)(.*)(?=.method:))';
   final grouped = RegExp('$nsLocalizedRegex|$lnUrlRegex|$detailRegex');
   return grouped.stringMatch(exception.toString()) ?? exception.toString();
 }
