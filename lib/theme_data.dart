691091447fd877276f0c65fb2b5319d1dba9499d
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2022-11-17 20:44:16 +0300

    Set default theme to dark theme

--- lib/theme_data.dart
+++ lib/theme_data.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-String themeId = "BLUE";
+String themeId = "DARK";
 
 class CustomData {
   BlendMode loaderColorBlendMode;
