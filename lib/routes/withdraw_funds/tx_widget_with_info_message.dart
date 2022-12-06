5576c64c98b694cc5088f6bc819cbf5e846446c3
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2022-12-06 11:26:46 +0300

    Refactor TxWidgetWithInfoMsg
    swapInProgress can never be null here

--- lib/routes/withdraw_funds/tx_widget_with_info_message.dart
+++ lib/routes/withdraw_funds/tx_widget_with_info_message.dart
@@ -15,32 +15,30 @@ class TxWidgetWithInfoMsg extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final texts = AppLocalizations.of(context);
-    final txId = swapInProgress?.claimTxId ?? swapInProgress?.lockTxID;
 
-    return (swapInProgress == null)
-        ? Center(child: Loader())
-        : Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: 50.0, left: 30.0, right: 30.0),
-                child: Text(
-                  txId.isEmpty
-                      ? texts
-                          .swap_in_progress_message_processing_previous_request
-                      : texts.swap_in_progress_message_waiting_confirmation,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              if (txId.isNotEmpty) ...[
-                TxWidget(
-                  txID: txId,
-                  txURL: "https://blockstream.info/tx/$txId",
-                  padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 0.0),
-                )
-              ]
-            ],
-          );
+    final txId = swapInProgress.claimTxId ?? swapInProgress.lockTxID;
+
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: 50.0, left: 30.0, right: 30.0),
+          child: Text(
+            txId.isEmpty
+                ? texts.swap_in_progress_message_processing_previous_request
+                : texts.swap_in_progress_message_waiting_confirmation,
+            textAlign: TextAlign.center,
+          ),
+        ),
+        if (txId.isNotEmpty) ...[
+          TxWidget(
+            txID: txId,
+            txURL: "https://blockstream.info/tx/$txId",
+            padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 0.0),
+          )
+        ]
+      ],
+    );
   }
 }
