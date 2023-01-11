8545f6539b2d28185c5f579b1dbc765468c778c8
ruben beck <ruben.beck@pm.me>
2023-01-11 14:53:26 +0100

    new tor manager from obot, tor thread chrashes on statup

--- ios/Plugins/Breez/Tor.swift
+++ ios/Plugins/Breez/Tor.swift
@@ -1,219 +1,255 @@
-// Ref. https://github.com/iCepa/iCepa/blob/main/Shared/TorManager.swift 
-
+ 
 import Foundation
 import Flutter
 
 
 class Tor : NSObject, FlutterPlugin {
-
-    static let localhost = "127.0.0.1"
     
-    static let torProxyPort: UInt16     = 39050
-    static let controlPort: UInt16      = 39060
-    static let dnsPort:UInt16           = 39053
-    //static let HTTPTunnelPort:UInt16     = 38888
+    enum Status: String, Codable {
+        case stopped = "stopped"
+        case starting = "starting"
+        case started = "started"
+    }
+    
+    enum Errors: Error, LocalizedError {
+        case cookieUnreadable
+        case noSocksAddr
+        case noDnsAddr
+        case smartConnectFailed
+
+        var errorDescription: String? {
+            switch self {
+
+            case .cookieUnreadable:
+                return "Tor cookie unreadable"
+
+            case .noSocksAddr:
+                return "No SOCKS port"
+
+            case .noDnsAddr:
+                return "No DNS port"
+
+            case .smartConnectFailed:
+                return "Smart Connect failed"
+            }
+        }
+    }
+
+    
+    static let localhost = "127.0.0.1"
+
+    var status = Status.stopped
     
     private var torThread: TorThread?
+
     private var torController: TorController?
+
     private var torConf: TorConfiguration?
-    
-    
+
     private var torRunning: Bool {
-        guard torThread?.isExecuting ?? false else {
-            return false
-        }
-        if let lock = torConf?.dataDirectory?.appendingPathComponent("lock") {
-            return FileManager.default.fileExists(atPath: lock.path)
-        }
-        return false
-    }
-    
-    private var cookie: Data? {
-        if let cookieUrl = torConf?.dataDirectory?.appendingPathComponent("control_auth_cookie") {
-            return try? Data(contentsOf: cookieUrl)
-            
-        }
-        return nil
+        (torThread?.isExecuting ?? false) && (torConf?.isLocked ?? false)
         
     }
-    
 
+    private lazy var controllerQueue = DispatchQueue.global(qos: .userInitiated)
+
+    private var progressObs: Any?
+    private var establishedObs: Any?
+    
+    
 
     public static func register(with registrar: FlutterPluginRegistrar) {
-
-	let TOR_CHANNEL  = "com.breez.client/tor"
-	    let instance = Tor()
+        
+        let TOR_CHANNEL  = "com.breez.client/tor"
+        let instance = Tor()
 	    let channel = FlutterMethodChannel(name: TOR_CHANNEL, binaryMessenger: registrar.messenger())
-	    registrar.addMethodCallDelegate(instance, channel: channel)
+        registrar.addMethodCallDelegate(instance, channel: channel)
 
     }
-
+    
+    
     func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-
-	print("Tor.swift: handle")
-
-	    guard (call.method == "startTorService") else {
-		result(FlutterMethodNotImplemented)
-		return
-	    }
-
-	startTor(call: call, result: result)
+        
+        
+        print("Tor.swift: handle")
+        guard (call.method == "startTorService") else {
+            result(FlutterMethodNotImplemented)
+            return
+        }
+        do {
+            try startTor(call: call, result: result)
+        } catch {
+            result(FlutterError(code: "TorError", message: "Breez encountered an unexpected error)", details: ""))
+        }
+        
+        
     }
+    
+    func startTor(call: FlutterMethodCall, result: @escaping FlutterResult) throws {
+        
+        self.status = .starting
+        
+        if !torRunning {
+            print("getting tor conf")
+            torConf = getTorConf(result: result)
 
+//            if let debug = torConf?.compile().joined(separator: ", ") {
+//                Logger.log(debug, to: FileManager.default.torLogFile)
+//            }
 
-    func startTor(call: FlutterMethodCall, result: @escaping FlutterResult) {
-        
-        
-        guard !(torThread?.isExecuting ?? false) else { return };
-        torConf = getTorConf(result: result)
-        
-        torThread = TorThread(configuration: torConf)
-        
-        torThread?.start()
-        
-        
-        print("Tor.swift: startTor: starting tor thread.")
+            torThread = TorThread(configuration: torConf)
 
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-
-            if self.torController == nil {
-                self.torController = TorController(
-                    socketHost: Tor.localhost,
-                    port: Tor.controlPort
-                )
+            torThread?.start()
+        }
+        
+        controllerQueue.asyncAfter(deadline: .now() + 0.65) {
+            if self.torController == nil, let url = self.torConf?.controlPortFile {
+                self.torController = TorController(controlPortFile: url)
             }
             
             if !(self.torController?.isConnected ?? false) {
                 do {
-                    // FIXME(nochiel) This fails on iOS with a "Foundation._GenericObjCError error 0".
-                    // I'm not sure why but if we needed the controller it would have to
-                    // be fixed here.
                     try self.torController?.connect()
                 } catch let error {
+                    self.status = .stopped
                     result(FlutterError(code: "TorError", message: "Breez could not connect to the Tor controller: \(error.localizedDescription)", details: ""))
+                    return
                 }
             }
-
             
-            guard let cookie = self.cookie else {
+            
+            guard let cookie = self.torConf?.cookie else {
                 result(FlutterError(code: "TorError",message: "Cookie error", details: ""))
+                self.status = .stopped
                 return
             }
                     
             
             
-            self.torController?.authenticate(with: cookie, completion: { success, error in
+            self.torController?.authenticate(with: cookie) { success, error in
                 if let error = error {
-                    result(FlutterError(code: "TorError", message: error.localizedDescription, details: ""))
+                    self.status = .stopped
+                    result(FlutterError(code: "TorError", message: "Breez could not connect to the Tor controller: \(Errors.cookieUnreadable)", details: "\(error)"))
                     return
+                    
                 }
                 
-                var progressObs: Any?
-                progressObs = self.torController?.addObserver(forStatusEvents: {
-                    (type, severity, action, arguments) -> Bool in
-
+                
+                self.progressObs = self.torController?.addObserver(forStatusEvents: {
+                    [weak self] (type, severity, action, arguments) -> Bool in
                     if type == "STATUS_CLIENT" && action == "BOOTSTRAP" {
-                        let progress = Int(arguments!["PROGRESS"]!)!
-                        print("#startTunnel progress=\(progress)")
-
-                        if progress >= 100 {
-                            self.torController?.removeObserver(progressObs)
+                        let progress: Int?
+                        if let p = arguments?["PROGRESS"] {
+                            progress = Int(p)
+                        } else {
+                            progress = nil
                         }
-
+                        print("#startTunnel progress=\(progress?.description ?? "(nil)")")
+                        
+                        if progress ?? 0 >= 100 {
+                            self?.torController?.removeObserver(self?.progressObs)
+                        }
                         return true
                     }
-
                     return false
                 })
-
-                var observer: Any?
-                observer = self.torController?.addObserver(forCircuitEstablished: { established in
+                self.establishedObs = self.torController?.addObserver(forCircuitEstablished: { [weak self]
+                    established in
                     guard established else {
                         return
                     }
-
-                    self.torController?.removeObserver(observer)
+                    self?.torController?.removeObserver(self?.establishedObs)
+                    self?.torController?.removeObserver(self?.progressObs)
+                    self?.torController?.getInfoForKeys(["net/listeners/socks", "net/listeners/dns"]) { response in
+                        guard let socksAddr = response.first, !socksAddr.isEmpty else {
+                            self?.status = .stopped
+                            return // insert error here
+                        }
+                        guard let dnsAddr = response.last, !dnsAddr.isEmpty else {
+                            self?.status = .stopped
+                            return // insert error
+                        }
+                        self?.status = .started
+                        
+                    }
                 })
-
-                    
-            })
-             
-            let connected:Bool = ((self.torController?.isConnected) != nil)
-            print("tor controller is connected?",connected)
-           
-            
-            let config = [
-                "SOCKS"	    : "\(Tor.localhost):\(Tor.torProxyPort)",
-                "Control"   : "\(Tor.localhost):\(Tor.controlPort)",
-                "HTTP"	    : "\(Tor.localhost):\(Tor.dnsPort)",
-            ]
-            result(config)
-            return
-
+            }
+        }
+        let config = [
+            "SOCKS"        : "127.0.0.1:9050",
+            "Control"   : "127.0.0.1:9051",
+            "HTTP"        : "127.0.0.1:0",
+        ]
+        
+        result(config)
+    }
+    
+    func getCircuits(_ completion: @escaping ([TorCircuit]) -> Void) {
+        if let torController = torController {
+            torController.getCircuits(completion)
+        }
+        else {
+            completion([])
         }
     }
     
-    
-    // Private methods
+    // MARK: Private methods
     
    
     
     private func getTorConf(result: @escaping FlutterResult) -> TorConfiguration {
-        
-        
         let conf = TorConfiguration()
-        
-        
-        
-        conf.options = [
-            "AutomapHostsOnResolve": "1",
-            "AvoidDiskWrites": "1",
-            "ClientOnly": "1",
-            
-            
-            /*"VirtualAddrNetworkIPv4": "10.192.0.0/10",
-            "VirtualAddrNetworkIPv6": "[FC00::]/7",*/
-            
-            // Log
-            "Log": "[~circ,~guard]info stdout",
-            "LogMessageDomains": "1",
-            "SafeLogging": "0",
-            // FIXME(nochiel) Robustness: If we set these to auto, Tor will ensure it finds an open port.
-            // We can then retrieve these open ports using TorController and pass them back to TorBloc as a result.
-            // "SocksPort": "auto",
-            // "ControlPort" : "auto",
-            //"HTTPTunnelPort" : "\(Tor.localhost):\(Tor.HTTPTunnelPort)",
-            // "DNSPort": "auto",
-            
-            "DNSPort": "\(Tor.localhost):\(Tor.dnsPort)",
-            "SocksPort": "\(Tor.localhost):\(Tor.torProxyPort)",
-            "ControlPort" : "\(Tor.localhost):\(Tor.controlPort)",
-        ]
+
+        conf.ignoreMissingTorrc = true
+        conf.cookieAuthentication = true
+        conf.autoControlPort = true
+        conf.clientOnly = true
+        conf.avoidDiskWrites = true
         
         if let dataDirectory = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first?.appendingPathComponent("tor", isDirectory: true) {
             try? FileManager.default.removeItem(at: dataDirectory)
             try? FileManager.default.createDirectory(at: dataDirectory, withIntermediateDirectories: true)
             guard FileManager.default.fileExists(atPath: dataDirectory.path) else {
-                result(FlutterError(code: "FAIL",
-                                    message: "Breez could not create the Tor directory",
-                                    details: nil))
-                return conf
-            }
+                            result(FlutterError(code: "FAIL",message: "Breez could not create the Tor directory",
+                                                details: nil))
+                            return conf
+                        }
             conf.dataDirectory = dataDirectory
-            conf.controlSocket = dataDirectory.appendingPathComponent("control_port")
+            conf.clientAuthDirectory = dataDirectory.appendingPathComponent("auth")
+            
         }
         
-        conf.cookieAuthentication =  true
-        
-        
-        
-        conf.arguments += [
-            "--allow-missing-torrc",
-            "--ignore-missing-torrc",
-        ]
-        
-        
-        print("Tor.swift: returning config.",conf)
+
+        // GeoIP files for circuit node country display.
+        conf.geoipFile = Bundle.geoIp?.geoipFile
+        conf.geoip6File = Bundle.geoIp?.geoip6File
+
+
+        conf.options = [
+            // DNS
+            "DNSPort": "auto",
+            "AutomapHostsOnResolve": "1",
+            // By default, localhost resp. link-local addresses will be returned by Tor.
+            // That seems to not get accepted by iOS. Use private network addresses instead.
+            "VirtualAddrNetworkIPv4": "10.192.0.0/10",
+            "VirtualAddrNetworkIPv6": "[FC00::]/7",
+
+            // Log
+            "LogMessageDomains": "1",
+            "SafeLogging": "1",
+
+            // SOCKS5
+            "SocksPort": "auto",
+
+            // Miscelaneous
+            "MaxMemInQueues": "5MB"]
+
+//            if Logger.ENABLE_LOGGING,
+//               let logfile = FileManager.default.torLogFile?.truncate()
+//            {
+//                conf.options["Log"] = "notice file \(logfile.path)"
+//            }
+
         return conf
     }
     
@@ -222,12 +258,20 @@ class Tor : NSObject, FlutterPlugin {
             torController?.resetConnection(callback)
     }
     
+    
+    
 
     deinit {
+        torController?.removeObserver(self.establishedObs)
+        torController?.removeObserver(self.progressObs)
+
         torController?.disconnect()
         torController = nil
+
         torThread?.cancel()
         torThread = nil
+
+        torConf = nil
     }
 
 }
