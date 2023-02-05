579e83de609d318f5c1fef1c96108432d09e7d00
Roei Erez <roeierez@gmail.com>
2023-02-05 15:01:26 +0200

    apply translations to lnurl_webview

--- lib/routes/marketplace/lnurl_auth.dart
+++ lib/routes/marketplace/lnurl_auth.dart
@@ -4,13 +4,17 @@ import 'package:breez/bloc/lnurl/lnurl_actions.dart';
 import 'package:breez/bloc/lnurl/lnurl_bloc.dart';
 import 'package:breez/bloc/lnurl/lnurl_model.dart';
 import 'package:breez/bloc/marketplace/vendor_model.dart';
+import 'package:breez_translations/breez_translations_locales.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<String> handleLNUrlAuth(VendorModel vendor, Uri endpointURI, LNUrlBloc lnurlBloc, String responsdID) async {
+Future<String> handleLNUrlAuth(
+    BuildContext context, VendorModel vendor, Uri endpointURI, LNUrlBloc lnurlBloc, String responsdID) async {
+  final texts = context.texts();
   var response = vendor.id == "lnmarkets" ? await http.post(endpointURI) : await http.get(endpointURI);
 
   if (response.statusCode != 200 && response.statusCode != 201) {
-    throw Exception("Failed to call ${vendor.displayName} API");
+    throw Exception(texts.lnurl_webview_error_message(vendor.displayName));
   }
   Map<String, dynamic> decoded = json.decode(response.body);
   String lnUrl = decoded[responsdID] as String;
@@ -18,7 +22,7 @@ Future<String> handleLNUrlAuth(VendorModel vendor, Uri endpointURI, LNUrlBloc ln
   lnurlBloc.actionsSink.add(fetchAction);
   var fetchResponse = await fetchAction.future;
   if (fetchResponse.runtimeType != AuthFetchResponse) {
-    throw "Invalid URL";
+    throw texts.lnurl_webview_error_invalid_url;
   }
   AuthFetchResponse authResponse = fetchResponse as AuthFetchResponse;
   var action = Login(authResponse, jwt: true);
