19db05d25e948f17bca8c1830f5f72bdf1f671d1
Ademar Alves de Oliveira <ademar111190@gmail.com>
2023-03-24 15:10:57 -0300

    Add a recover flow when user has removed the permission of its account

--- android/app/src/main/java/com/breez/client/plugins/breez/breezlib/GoogleAuthenticator.java
+++ android/app/src/main/java/com/breez/client/plugins/breez/breezlib/GoogleAuthenticator.java
@@ -14,7 +14,12 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.TaskCompletionSource;
 import com.google.android.gms.tasks.Tasks;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.FileList;
 
 import java.util.Collections;
 
@@ -95,7 +100,7 @@ public class GoogleAuthenticator implements PluginRegistry.ActivityResultListene
 
     public boolean validateAccessTokenAllowingPrompt() {
         Log.d(TAG, "getAccessTokenWithPrompt");
-        GoogleSignInAccount googleAccount = null;
+        GoogleSignInAccount googleAccount;
         try {
             googleAccount = ensureSignedIn(true);
         } catch (Exception e) {
@@ -121,7 +126,7 @@ public class GoogleAuthenticator implements PluginRegistry.ActivityResultListene
         }
 
         try {
-            return credential.getToken() != null;
+            return verifyCredentialHasWriteAccess(credential.getToken(), true);
         } catch (Exception e) {
             Log.w(TAG, "getAccessTokenWithPrompt failed", e);
             if (e instanceof UserRecoverableAuthException) {
@@ -142,7 +147,7 @@ public class GoogleAuthenticator implements PluginRegistry.ActivityResultListene
                 }
 
                 try {
-                    return credential.getToken() != null;
+                    return verifyCredentialHasWriteAccess(credential.getToken(), true);
                 } catch (Exception ex) {
                     Log.w(TAG, "getAccessTokenWithPrompt failed in recoverable", ex);
                     return false;
@@ -150,6 +155,105 @@ public class GoogleAuthenticator implements PluginRegistry.ActivityResultListene
             }
             return false;
         }
+    }
+
+    private boolean verifyCredentialHasWriteAccess(String token, boolean allowRecover) {
+        Log.d(TAG, "verifyCredentialHasWriteAccess token = " + token);
+        if (token == null || token.isEmpty()) {
+            return false;
+        }
+
+        try {
+            Drive driveService = new Drive.Builder(
+                    new NetHttpTransport.Builder().build(),
+                    new GsonFactory(),
+                    request -> {
+                    })
+                    .setHttpRequestInitializer(req -> req.getHeaders().setAuthorization("Bearer " + token))
+                    .setApplicationName("Breez").build();
+            FileList result = driveService.files().list().setSpaces("appDataFolder").execute();
+            Log.d(TAG, "verifyCredentialHasWriteAccess result = " + result);
+        } catch (Exception e) {
+            if (e instanceof GoogleJsonResponseException && ((GoogleJsonResponseException) e).getStatusCode() == 401) {
+                if (allowRecover) {
+                    Log.d(TAG, "verifyCredentialHasWriteAccess failed with 401, trying to sign in to recovery write permission");
+                    return recoverUnauthorizedAccess();
+                } else {
+                    Log.w(TAG, "verifyCredentialHasWriteAccess failed with 401, but not allowed to recover");
+                    return false;
+                }
+            } else {
+                Log.w(TAG, "verifyCredentialHasWriteAccess failed", e);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean recoverUnauthorizedAccess() {
+        Log.d(TAG, "recoverUnauthorizedAccess");
+        try {
+            signOut();
+        } catch (Exception e) {
+            Log.w(TAG, "recover signOut failed", e);
+            return false;
+        }
+
+        GoogleSignInAccount googleAccount;
+        try {
+            googleAccount = Tasks.await(signIn());
+        } catch (Exception e) {
+            Log.w(TAG, "recover signIn failed", e);
+            return false;
+        }
+
+        GoogleAccountCredential credential;
+        try {
+            credential = GoogleAccountCredential.usingOAuth2(
+                    activityBinding.getActivity(),
+                    Collections.singleton(DriveScopes.DRIVE_APPDATA));
+        } catch (Exception e) {
+            Log.w(TAG, "recover failed", e);
+            return false;
+        }
+
+        String token;
+        try {
+            credential.setSelectedAccount(googleAccount.getAccount());
+            token = credential.getToken();
+        } catch (Exception e) {
+            // For some reason, on this flow google throws an UserRecoverableAuthException despite of the user
+            // be on the auth fow, so we just trigger it again to the user give us the auth on both pages
+            if (e instanceof UserRecoverableAuthException) {
+                GoogleSignInAccount signInResult;
+                try {
+                    signInResult = Tasks.await(signIn());
+                } catch (Exception ex) {
+                    Log.w(TAG, "recover signIn failed", ex);
+                    return false;
+                }
+
+                try {
+                    credential.setSelectedAccount(signInResult.getAccount());
+                } catch (Exception ex) {
+                    Log.w(TAG, "recover setSelectedAccount failed", ex);
+                    return false;
+                }
+
+                try {
+                    token = credential.getToken();
+                } catch (Exception ex) {
+                    Log.w(TAG, "recover getToken failed", ex);
+                    return false;
+                }
+            } else {
+                Log.w(TAG, "recover failed", e);
+                return false;
+            }
+        }
+
+        return verifyCredentialHasWriteAccess(token, false);
     }
 
     private GoogleSignInClient createSignInClient() {
@@ -178,11 +282,11 @@ public class GoogleAuthenticator implements PluginRegistry.ActivityResultListene
                     task.getResult(ApiException.class);
                     GoogleSignInAccount res = task.getResult();
 
-                    Log.i(TAG, "Sign in intent success, ,token = " + res.getAccount());
+                    Log.i(TAG, "Sign in intent success, token = " + res.getAccount());
                     m_signInProgressTask.setResult(task.getResult());
                 } catch (ApiException e) {
                     m_signInProgressTask.setException(new Exception("AuthError"));
-                    Log.w(TAG, "Sign in Failed…", e);
+                    Log.w(TAG, "Sign in Failed… " + e.getMessage(), e);
                 }
             });
             return true;
