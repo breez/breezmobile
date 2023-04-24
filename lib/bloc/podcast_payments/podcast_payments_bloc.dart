4582bbf20180640bdfa98d709d90f95b70ad34d0
Alwin <alwin@conshax.com>
2023-04-24 16:44:24 +0200

    don't set itemID with internal id

--- lib/bloc/podcast_payments/podcast_payments_bloc.dart
+++ lib/bloc/podcast_payments/podcast_payments_bloc.dart
@@ -408,7 +408,6 @@ class PodcastPaymentsBloc with AsyncActionsHandler {
     tlv["action"] = boost ? "boost" : "stream";
     tlv["time"] = _formatDuration(position.position);
     tlv["feedID"] = _getPodcastIndexID(episode);
-    tlv["itemID"] = episode.id;
     tlv["episode_guid"] = episode.guid;
     tlv["app_name"] = "Breez";
     tlv["value_msat_total"] = msatTotal;
