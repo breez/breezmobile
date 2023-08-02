818e5f3e07fae6a50822de250a4b78d1ef8c5e63
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2023-08-02 18:59:14 +0300

    Refactor Initial Walkthrough
    Use a contextless spinner to avoid UI locks(via flutter_easyloading plugin)
    
    Added a "Sign into Google Drive failed." flushbar
    
    BackupProvider settings are always updated.
    
    Update lastBackupTime of BackupState with modified time of restored snapshot

--- lib/routes/initial_walkthrough/initial_walkthrough.dart
+++ lib/routes/initial_walkthrough/initial_walkthrough.dart
@@ -242,7 +242,6 @@ class _InitialWalkthroughPageState extends State<InitialWalkthroughPage>
             backupSettings: backupSettings,
             backupProviders: backupProviders,
             onProviderSelected: (_) {
-              // TODO add translation
               EasyLoading.show(
                 indicator: const LoaderIndicator(
                   message: 'Loading Backups',
