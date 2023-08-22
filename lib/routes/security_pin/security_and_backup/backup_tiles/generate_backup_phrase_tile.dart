a755ba94a085f7cd98bb73336d26028f402b3572
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2023-08-23 02:22:43 +0300

    Get BackupSettings on top level(SecurityAndBackupPage)

--- lib/routes/security_pin/security_and_backup/backup_tiles/generate_backup_phrase_tile.dart
+++ lib/routes/security_pin/security_and_backup/backup_tiles/generate_backup_phrase_tile.dart
@@ -1,5 +1,4 @@
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:breez/bloc/backup/backup_bloc.dart';
 import 'package:breez/bloc/backup/backup_model.dart';
 import 'package:breez/routes/podcast/theme.dart';
 import 'package:breez/routes/security_pin/backup_phrase/backup_phrase_confirmation_page.dart';
@@ -11,12 +10,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class GenerateBackupPhraseTile extends StatefulWidget {
-  final BackupBloc backupBloc;
+  final BackupSettings backupSettings;
   final AutoSizeGroup autoSizeGroup;
   final Future Function(BackupSettings backupSettings) backupNow;
 
   const GenerateBackupPhraseTile({
-    @required this.backupBloc,
+    @required this.backupSettings,
     @required this.autoSizeGroup,
     @required this.backupNow,
   });
@@ -30,59 +29,47 @@ class _GenerateBackupPhraseTileState extends State<GenerateBackupPhraseTile> {
   @override
   Widget build(BuildContext context) {
     final texts = context.texts();
-    return StreamBuilder<BackupSettings>(
-      stream: widget.backupBloc.backupSettingsStream,
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return Container();
-        }
 
-        final backupSettings = snapshot.data;
-
-        return SimpleSwitch(
-          text: texts.security_and_backup_encrypt,
-          switchValue: backupSettings.backupKeyType == BackupKeyType.PHRASE,
-          group: widget.autoSizeGroup,
-          onChanged: (bool value) async {
-            if (mounted) {
-              if (value) {
-                Navigator.push(
+    return SimpleSwitch(
+      text: texts.security_and_backup_encrypt,
+      switchValue: widget.backupSettings.backupKeyType == BackupKeyType.PHRASE,
+      group: widget.autoSizeGroup,
+      onChanged: (bool value) async {
+        if (mounted) {
+          if (value) {
+            Navigator.push(
+              context,
+              FadeInRoute(
+                builder: (BuildContext context) => withBreezTheme(
                   context,
-                  FadeInRoute(
-                    builder: (BuildContext context) => withBreezTheme(
-                      context,
-                      BackupPhraseGeneratorConfirmationPage(),
-                    ),
-                  ),
-                );
-              } else {
-                showDialog(
-                  context: context,
-                  barrierDismissible: false,
-                  builder: (BuildContext context) {
-                    return BackupPhraseWarningDialog();
-                  },
-                ).then(
-                  (approved) async {
-                    if (approved) {
-                      await _disableEncryption(backupSettings);
-                    }
-                  },
-                );
-              }
-            }
-          },
-        );
+                  BackupPhraseGeneratorConfirmationPage(),
+                ),
+              ),
+            );
+          } else {
+            showDialog(
+              context: context,
+              barrierDismissible: false,
+              builder: (BuildContext context) {
+                return BackupPhraseWarningDialog();
+              },
+            ).then(
+              (approved) async {
+                if (approved) await _disableEncryption();
+              },
+            );
+          }
+        }
       },
     );
   }
 
-  Future<void> _disableEncryption(BackupSettings backupSettings) async {
+  Future<void> _disableEncryption() async {
     try {
       EasyLoading.show();
 
       await widget.backupNow(
-        backupSettings.copyWith(keyType: BackupKeyType.NONE),
+        widget.backupSettings.copyWith(keyType: BackupKeyType.NONE),
       );
     } finally {
       EasyLoading.dismiss();
