085f131d7b7bb1d0987340a956a10bc57a7ee5a1
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2023-08-23 02:22:43 +0300

    Get BackupSettings on top level(SecurityAndBackupPage)

--- lib/routes/security_pin/security_and_backup/backup_tiles/backup_provider_tile.dart
+++ lib/routes/security_pin/security_and_backup/backup_tiles/backup_provider_tile.dart
@@ -1,5 +1,4 @@
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:breez/bloc/backup/backup_bloc.dart';
 import 'package:breez/bloc/backup/backup_model.dart';
 import 'package:breez/theme_data.dart' as theme;
 import 'package:breez/utils/min_font_size.dart';
@@ -8,16 +7,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class BackupProviderTile extends StatefulWidget {
-  final BackupBloc backupBloc;
+  final BackupSettings backupSettings;
   final AutoSizeGroup autoSizeGroup;
   final Future Function(
-    BackupSettings backupSettings, {
     BackupProvider backupProvider,
-  }) enterRemoteServerCredentials;
+  ) enterRemoteServerCredentials;
   final Future Function(BackupSettings backupSettings) backupNow;
 
   const BackupProviderTile({
-    @required this.backupBloc,
+    @required this.backupSettings,
     @required this.autoSizeGroup,
     @required this.enterRemoteServerCredentials,
     @required this.backupNow,
@@ -31,56 +29,45 @@ class _BackupProviderTileState extends State<BackupProviderTile> {
   @override
   Widget build(BuildContext context) {
     final texts = context.texts();
-    return StreamBuilder<BackupSettings>(
-      stream: widget.backupBloc.backupSettingsStream,
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return Container();
-        }
 
-        final backupSettings = snapshot.data;
-
-        return ListTile(
-          title: AutoSizeText(
-            texts.security_and_backup_store_location,
-            style: const TextStyle(color: Colors.white),
-            maxLines: 1,
-            minFontSize: MinFontSize(context).minFontSize,
-            stepGranularity: 0.1,
-            group: widget.autoSizeGroup,
-          ),
-          trailing: DropdownButtonHideUnderline(
-            child: DropdownButton<BackupProvider>(
-              iconEnabledColor: Colors.white,
-              value: backupSettings.backupProvider,
-              isDense: true,
-              onChanged: (BackupProvider selectedProvider) async {
-                await _updateBackupProvider(selectedProvider, backupSettings);
-              },
-              items: BackupSettings.availableBackupProviders().map(
-                (provider) {
-                  return DropdownMenuItem(
-                    value: provider,
-                    child: AutoSizeText(
-                      provider.displayName,
-                      style: theme.FieldTextStyle.textStyle,
-                      maxLines: 1,
-                      minFontSize: MinFontSize(context).minFontSize,
-                      stepGranularity: 0.1,
-                    ),
-                  );
-                },
-              ).toList(),
-            ),
-          ),
-        );
-      },
+    return ListTile(
+      title: AutoSizeText(
+        texts.security_and_backup_store_location,
+        style: const TextStyle(color: Colors.white),
+        maxLines: 1,
+        minFontSize: MinFontSize(context).minFontSize,
+        stepGranularity: 0.1,
+        group: widget.autoSizeGroup,
+      ),
+      trailing: DropdownButtonHideUnderline(
+        child: DropdownButton<BackupProvider>(
+          iconEnabledColor: Colors.white,
+          value: widget.backupSettings.backupProvider,
+          isDense: true,
+          onChanged: (BackupProvider selectedProvider) async {
+            await _updateBackupProvider(selectedProvider);
+          },
+          items: BackupSettings.availableBackupProviders().map(
+            (provider) {
+              return DropdownMenuItem(
+                value: provider,
+                child: AutoSizeText(
+                  provider.displayName,
+                  style: theme.FieldTextStyle.textStyle,
+                  maxLines: 1,
+                  minFontSize: MinFontSize(context).minFontSize,
+                  stepGranularity: 0.1,
+                ),
+              );
+            },
+          ).toList(),
+        ),
+      ),
     );
   }
 
   Future<void> _updateBackupProvider(
     BackupProvider selectedProvider,
-    BackupSettings backupSettings,
   ) async {
     try {
       EasyLoading.show();
@@ -89,15 +76,10 @@ class _BackupProviderTileState extends State<BackupProviderTile> {
           BackupSettings.remoteServerBackupProvider().name) {
         EasyLoading.dismiss();
 
-        await widget.enterRemoteServerCredentials(
-          backupSettings,
-          backupProvider: selectedProvider,
-        );
+        await widget.enterRemoteServerCredentials(selectedProvider);
       } else {
         await widget.backupNow(
-          backupSettings.copyWith(
-            backupProvider: selectedProvider,
-          ),
+          widget.backupSettings.copyWith(backupProvider: selectedProvider),
         );
       }
     } finally {
