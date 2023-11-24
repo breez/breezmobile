547391f713ed1003952814c54fe435555d9e3d05
Peter <peter@talesofapleb.com>
2023-11-24 19:40:36 +0000

    Add spend code field to the sweep slot page

--- lib/routes/satscard_balance/sweep_slot_page.dart
+++ lib/routes/satscard_balance/sweep_slot_page.dart
@@ -1,10 +1,15 @@
+import 'dart:collection';
+import 'dart:io';
+
 import 'package:breez/bloc/account/account_bloc.dart';
 import 'package:breez/bloc/account/account_model.dart';
 import 'package:breez/bloc/blocs_provider.dart';
 import 'package:breez/bloc/lsp/lsp_bloc.dart';
 import 'package:breez/bloc/lsp/lsp_model.dart';
 import 'package:breez/bloc/satscard/satscard_bloc.dart';
+import 'package:breez/theme_data.dart' as theme;
 import 'package:breez/widgets/back_button.dart' as backBtn;
+import 'package:breez/widgets/satscard/spend_code_field.dart';
 import 'package:breez/widgets/single_button_bottom_bar.dart';
 import 'package:breez_translations/breez_translations_locales.dart';
 import 'package:cktap_protocol/cktapcard.dart';
@@ -23,6 +28,12 @@ class SweepSlotPage extends StatefulWidget {
 }
 
 class SweepSlotPageState extends State<SweepSlotPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _spendCodeController = TextEditingController();
+  final _spendCodeFocusNode = FocusNode();
+  final _incorrectCodes =
+      HashSet<String>(equals: (a, b) => a == b, hashCode: (a) => a.hashCode);
+
   @override
   Widget build(BuildContext context) {
     final accountBloc = AppBlocsProvider.of<AccountBloc>(context);
@@ -37,12 +48,18 @@ class SweepSlotPageState extends State<SweepSlotPage> {
 
             return Scaffold(
               bottomNavigationBar: Padding(
-                padding: const EdgeInsets.only(top: 10),
+                padding: EdgeInsets.only(
+                  bottom: Platform.isIOS && _spendCodeFocusNode.hasFocus
+                      ? 40.0
+                      : 0.0,
+                ),
                 child: SingleButtonBottomBar(
                   stickToBottom: true,
                   text: texts.satscard_sweep_button_label,
                   onPressed: () {
-                    // TODO: Add SweepSatscard action and open the NFC dialog
+                    if (_formKey.currentState.validate()) {
+                      // TODO: Add SweepSatscard action and open the NFC dialog
+                    }
                   },
                 ),
               ),
@@ -50,11 +67,29 @@ class SweepSlotPageState extends State<SweepSlotPage> {
                 leading: backBtn.BackButton(onPressed: widget.onBack),
                 title: Text(texts.satscard_sweep_title(widget._slot.index + 1)),
               ),
-              body: Scrollbar(
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: const [SizedBox.shrink()],
+              body: Form(
+                key: _formKey,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 40.0),
+                  child: Scrollbar(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          SpendCodeFormField(
+                              context: context,
+                              texts: texts,
+                              focusNode: _spendCodeFocusNode,
+                              controller: _spendCodeController,
+                              style: theme.FieldTextStyle.textStyle,
+                              validatorFn: (code) => _incorrectCodes
+                                      .contains(code)
+                                  ? texts
+                                      .satscard_spend_code_incorrect_code_hint
+                                  : null),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
               ),
