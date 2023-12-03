7d8e5ead6011e84e4e1030609f9c59203e9a0c68
Peter <peter@talesofapleb.com>
2023-12-03 22:54:04 +0000

    Rework BroadcastSlotSweepTransaction
    - Logic should be much clearer
    - Added the transaction ID
    - Auto-close on success

--- lib/routes/satscard_balance/broadcast_slot_sweep_transaction.dart
+++ lib/routes/satscard_balance/broadcast_slot_sweep_transaction.dart
@@ -7,7 +7,10 @@ import 'package:breez/bloc/satscard/satscard_actions.dart';
 import 'package:breez/bloc/satscard/satscard_bloc.dart';
 import 'package:breez/routes/satscard_balance/satscard_balance_page.dart';
 import 'package:breez/services/breezlib/data/messages.pb.dart';
+import 'package:breez/services/injector.dart';
 import 'package:breez/widgets/back_button.dart' as backBtn;
+import 'package:breez/widgets/flushbar.dart';
+import 'package:breez/widgets/link_launcher.dart';
 import 'package:breez/widgets/single_button_bottom_bar.dart';
 import 'package:breez_translations/breez_translations_locales.dart';
 import 'package:breez_translations/generated/breez_translations.dart';
@@ -21,11 +24,11 @@ class BroadcastSlotSweepTransactionPage extends StatefulWidget {
   final RawSlotSweepTransaction Function() getTransaction;
 
   const BroadcastSlotSweepTransactionPage(
-      {this.onBack,
-      this.onDone,
-      this.getAddressInfo,
-      this.getPrivateKey,
-      this.getTransaction});
+      {@required this.onBack,
+      @required this.onDone,
+      @required this.getAddressInfo,
+      @required this.getPrivateKey,
+      @required this.getTransaction});
 
   @override
   State<StatefulWidget> createState() =>
@@ -35,89 +38,131 @@ class BroadcastSlotSweepTransactionPage extends StatefulWidget {
 class BroadcastSlotSweepTransactionPageState
     extends State<BroadcastSlotSweepTransactionPage> {
   TransactionDetails _signedTransaction;
-  String _recentError = "";
-
-  void _setError(String errorMessage) {
-    if (context.mounted) {
-      setState(() => _recentError = errorMessage);
-    }
-  }
+  Future _future;
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-
-    _requestSignTransaction();
+    _broadcastTransaction(context);
   }
 
   @override
   Widget build(BuildContext context) {
-    final themeData = Theme.of(context);
-    final texts = context.texts();
-    final showRetryBroadcastButton =
-        _recentError.isNotEmpty && _signedTransaction != null;
-
-    return Scaffold(
-      bottomNavigationBar: !showRetryBroadcastButton
-          ? null
-          : Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: SingleButtonBottomBar(
-                stickToBottom: true,
-                text: texts.satscard_balance_button_retry_label,
-                onPressed: () => _requestBroadcastTransaction(),
+    return FutureBuilder(
+        future: _future,
+        builder: (context, snapshot) {
+          final themeData = Theme.of(context);
+          final texts = context.texts();
+          final showError = snapshot.hasError;
+          final showDone = !showError && snapshot.hasData;
+          return Scaffold(
+            bottomNavigationBar:
+                !snapshot.hasError || _signedTransaction == null
+                    ? null
+                    : Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: SingleButtonBottomBar(
+                          stickToBottom: true,
+                          text: texts.satscard_balance_button_retry_label,
+                          onPressed: () => _broadcastTransaction(context),
+                        ),
+                      ),
+            appBar: AppBar(
+              title: Text(
+                texts.satscard_broadcast_title,
+                style: themeData.appBarTheme.titleTextStyle,
+              ),
+              leading: backBtn.BackButton(
+                onPressed: showDone ? widget.onDone : widget.onBack,
               ),
             ),
-      appBar: AppBar(
-        leading: backBtn.BackButton(
-          onPressed: widget.onBack,
-        ),
-      ),
-      body: _recentError.isNotEmpty
-          ? buildErrorBody(themeData, _recentError)
-          : buildLoaderBody(themeData, _getLoaderText(texts)),
-    );
-  }
-
-  void _requestSignTransaction() {
-    _setError("");
-
-    final bloc = AppBlocsProvider.of<SatscardBloc>(context);
-    final addressInfo = widget.getAddressInfo();
-    final privateKey = widget.getPrivateKey();
-    final transaction = widget.getTransaction();
-    final action =
-        SignSlotSweepTransaction(addressInfo, transaction, privateKey);
-    bloc.actionsSink.add(action);
-    action.future.then((response) {
-      if (context.mounted) {
-        setState(() {
-          _signedTransaction = response as TransactionDetails;
-          _requestBroadcastTransaction();
+            body: _buildBody(themeData, texts, snapshot, showError, showDone),
+          );
         });
-      }
-    }).catchError(
-      (e) => _setError(
-          context.texts().satscard_broadcast_error_signing(e.toString())),
+  }
+
+  Widget _buildBody(
+    ThemeData themeData,
+    BreezTranslations texts,
+    AsyncSnapshot<String> snapshot,
+    bool showError,
+    bool showDone,
+  ) {
+    if (showError) {
+      return buildErrorBody(themeData, _getErrorText(texts, snapshot.error));
+    } else if (!showDone) {
+      return buildLoaderBody(themeData, _getLoaderText(texts));
+    }
+
+    final txId = snapshot.data;
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        LinkLauncher(
+          linkName: txId,
+          linkAddress: "https://blockstream.info/tx/$txId",
+          onCopy: () {
+            ServiceInjector().device.setClipboardText(txId);
+            showFlushbar(
+              context,
+              message: texts.add_funds_transaction_id_copied,
+              duration: const Duration(seconds: 3),
+            );
+          },
+        ),
+      ],
     );
   }
 
-  _requestBroadcastTransaction() {
-    _setError("");
-
-    final bloc = AppBlocsProvider.of<AccountBloc>(context);
-    final action = PublishTransaction(_signedTransaction.tx);
-    bloc.userActionsSink.add(action);
-    action.future.then((_) {
-      widget.onDone();
-    }).catchError((e) =>
-        _setError(context.texts().satscard_broadcast_error_broadcasting(e)));
+  String _getErrorText(BreezTranslations texts, Object error) {
+    return _signedTransaction == null
+        ? texts.satscard_broadcast_error_signing(error)
+        : texts.satscard_broadcast_error_broadcasting(error);
   }
 
   String _getLoaderText(BreezTranslations texts) {
-    if (_signedTransaction == null) {
-      return texts.satscard_broadcast_signing_label;
+    return _signedTransaction == null
+        ? texts.satscard_broadcast_signing_label
+        : texts.satscard_broadcast_broadcasting_label;
+  }
+
+  void _broadcastTransaction(BuildContext context) {
+    if (!context.mounted) {
+      return;
     }
-    return texts.satscard_broadcast_broadcasting_label;
+    setState(() {
+      _future = Future.sync(() {
+        // Sign the selected transaction if we haven't already
+        if (_signedTransaction == null) {
+          final satscardBloc = AppBlocsProvider.of<SatscardBloc>(context);
+          final info = widget.getAddressInfo();
+          final tx = widget.getTransaction();
+          final key = widget.getPrivateKey();
+          final action = SignSlotSweepTransaction(info, tx, key);
+          satscardBloc.actionsSink.add(action);
+          return action.future.then((result) {
+            if (context.mounted) {
+              setState(() {
+                _signedTransaction = result as TransactionDetails;
+              });
+            }
+          });
+        }
+      }).then((_) {
+        final accountBloc = AppBlocsProvider.of<AccountBloc>(context);
+        final action = PublishTransaction(_signedTransaction.tx);
+        accountBloc.userActionsSink.add(action);
+        return action.future;
+      }).then((_) {
+        // Close the page automatically after a successful broadcast
+        Future.delayed(const Duration(seconds: 5), () {
+          if (context.mounted) {
+            widget.onDone();
+          }
+        });
+        return _signedTransaction.txHash;
+      });
+    });
   }
 }
