73a47cf8288921c1f077037891798e259558ee52
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-05-15 15:56:04 +0300

    Address theme issues on restore flow

--- lib/widgets/single_button_bottom_bar.dart
+++ lib/widgets/single_button_bottom_bar.dart
@@ -1,3 +1,4 @@
+import 'package:breez/theme_data.dart';
 import 'package:breez/widgets/designsystem/button/action_button.dart';
 import 'package:breez/widgets/designsystem/variant.dart';
 import 'package:flutter/material.dart';
@@ -6,33 +7,41 @@ class SingleButtonBottomBar extends StatelessWidget {
   final VoidCallback onPressed;
   final String text;
   final bool stickToBottom;
+  final bool isRestoreFlow;
 
   const SingleButtonBottomBar({
     this.text,
     this.onPressed,
     this.stickToBottom = false,
+    this.isRestoreFlow = false,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-        bottom: stickToBottom ? MediaQuery.of(context).viewInsets.bottom + 40.0 : 40.0,
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          ConstrainedBox(
-            constraints: const BoxConstraints(
-              minHeight: 48.0,
-              minWidth: 168.0,
+    final themeData = isRestoreFlow ? blueTheme : Theme.of(context);
+
+    return Theme(
+      data: themeData,
+      child: Padding(
+        padding: EdgeInsets.only(
+          bottom: stickToBottom ? MediaQuery.of(context).viewInsets.bottom + 40.0 : 40.0,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ConstrainedBox(
+              constraints: const BoxConstraints(
+                minHeight: 48.0,
+                minWidth: 168.0,
+              ),
+              child: SubmitButton(
+                text,
+                onPressed,
+                isRestoreFlow: isRestoreFlow,
+              ),
             ),
-            child: SubmitButton(
-              text,
-              onPressed,
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -41,25 +50,32 @@ class SingleButtonBottomBar extends StatelessWidget {
 class SubmitButton extends StatelessWidget {
   final VoidCallback onPressed;
   final String text;
+  final bool isRestoreFlow;
 
   const SubmitButton(
     this.text,
-    this.onPressed,
-  );
+    this.onPressed, {
+    this.isRestoreFlow = false,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: const BoxConstraints(
-        minHeight: 48.0,
-        minWidth: 168.0,
-      ),
-      child: ActionButton(
-        text: text,
-        onPressed: onPressed,
-        enabled: onPressed != null ?? true,
-        fill: true,
-        variant: Variant.fab,
+    final themeData = isRestoreFlow ? blueTheme : Theme.of(context);
+    return Theme(
+      data: themeData,
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(
+          minHeight: 48.0,
+          minWidth: 168.0,
+        ),
+        child: ActionButton(
+          text: text,
+          onPressed: onPressed,
+          enabled: onPressed != null ?? true,
+          fill: true,
+          variant: Variant.fab,
+          isRestoreFlow: isRestoreFlow,
+        ),
       ),
     );
   }
