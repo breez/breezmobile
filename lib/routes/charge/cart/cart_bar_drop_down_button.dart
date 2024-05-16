660ab480bdf097d322d2b784ed46bcc3116f6df1
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-05-16 16:42:18 +0300

    Only update POS currency list if there's a change

--- lib/routes/charge/cart/cart_bar_drop_down_button.dart
+++ lib/routes/charge/cart/cart_bar_drop_down_button.dart
@@ -3,12 +3,13 @@ import 'package:breez/bloc/user_profile/currency.dart';
 import 'package:breez/routes/charge/cart/cart_bar_drop_down_item.dart';
 import 'package:breez/routes/charge/currency_wrapper.dart';
 import 'package:breez/theme_data.dart' as theme;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
 
 final _log = Logger("CartBarDropDownButton");
 
-class CartBarDropDownButton extends StatelessWidget {
+class CartBarDropDownButton extends StatefulWidget {
   final String value;
   final AccountModel accountModel;
   final ValueChanged<String> onChanged;
@@ -21,49 +22,74 @@ class CartBarDropDownButton extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<CartBarDropDownButton> createState() => _CartBarDropDownButtonState();
+}
+
+class _CartBarDropDownButtonState extends State<CartBarDropDownButton> {
+  List<CurrencyWrapper> currencyList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _updateCurrencyList(_generateCurrencyList());
+  }
+
+  @override
+  void didUpdateWidget(CartBarDropDownButton oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    List<CurrencyWrapper> newCurrencyList = _generateCurrencyList();
+    if (_isCurrencyListDifferent(currencyList, newCurrencyList)) {
+      _updateCurrencyList(newCurrencyList);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
 
     return DropdownButtonHideUnderline(
-      key: key,
+      key: widget.key,
       child: ButtonTheme(
         alignedDropdown: true,
         child: DropdownButton(
-          onChanged: onChanged,
+          onChanged: widget.onChanged,
           iconEnabledColor: textTheme.headlineSmall.color,
-          value: value,
+          value: widget.value,
           style: theme.invoiceAmountStyle.copyWith(
             color: textTheme.headlineSmall.color,
           ),
-          items: _items(context, accountModel),
+          items: currencyList.map((currency) => CartBarDropDownItem(context, currency)).toList(),
         ),
       ),
     );
   }
 
-  List<DropdownMenuItem<String>> _items(
-    BuildContext context,
-    AccountModel accountModel,
-  ) {
-    _log.info("Building the cart drop down items");
-    List<CurrencyWrapper> currencies = [];
-    currencies.addAll(
-      Currency.currencies.map((c) => CurrencyWrapper.fromBTC(c)),
+  List<CurrencyWrapper> _generateCurrencyList() {
+    List<CurrencyWrapper> newCurrencyList = [
+      ...Currency.currencies.map((c) => CurrencyWrapper.fromBTC(c)),
+      ...widget.accountModel.preferredFiatConversionList.map((f) => CurrencyWrapper.fromFiat(f)),
+    ];
+    return newCurrencyList;
+  }
+
+  bool _isCurrencyListDifferent(List<CurrencyWrapper> oldList, List<CurrencyWrapper> newList) {
+    // We compare currency names instead of the entire CurrencyWrapper list
+    // because we're interested in changes in the currency selection,
+    // not in their properties(i.e. changes in exchange rates).
+    return !listEquals(
+      oldList.map((e) => e.shortName).toList(),
+      newList.map((e) => e.shortName).toList(),
     );
-    _log.info('Currencies after first addition: [${_fold(currencies)}]');
-    currencies.addAll(
-      accountModel.preferredFiatConversionList.map((f) => CurrencyWrapper.fromFiat(f)),
-    );
-    _log.info('Currencies after second addition: [${_fold(currencies)}]');
-    final dropDownItems = currencies.map(
-      (currency) => CartBarDropDownItem(context, currency),
-    );
-    _log.info(
-        'Drop down items: [${dropDownItems.fold("", (p, e) => p.isEmpty ? e.value : "$p, ${e.value}")}]');
-    return dropDownItems.toList();
+  }
+
+  void _updateCurrencyList(List<CurrencyWrapper> newCurrencyList) {
+    _log.info('POS Currency list: [${_fold(newCurrencyList)}]');
+    setState(() {
+      currencyList = newCurrencyList;
+    });
   }
 
   String _fold(List<CurrencyWrapper> currencies) {
-    return currencies.fold("", (p, e) => p.isEmpty ? e.shortName : "$p, ${e.shortName}");
+    return currencies.map((e) => e.shortName).join(", ");
   }
 }
